ab95357c62647c4a92ad01b781c73bc2b9226cea
duesex <duesex@localhost>
2004-05-18 03:39:47 +0000

    Added global defines and included them in the prefix header.

---
+++ GrowlDefines.h
@@ -0,0 +1,15 @@
+//
+//  GrowlDefines.h
+//  Growl
+//
+//  Created by Karl Adam on Mon May 17 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+// User Info Keys
+#define GROWL_APP_NAME					@"ApplicationName"
+#define GROWL_NOTIFICATIONS_DEFAULT		@"DefaultNotifications"
+#define GROWL_NOTIFICATIONS_ALL			@"AllNotifications"
+
+// Notifications
+#define GROWL_APP_REGISTRATION			@"GrowlApplicationRegistrationNotification"
