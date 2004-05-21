fbb8d3485e42fe1e15daf8ae53659f75a9f6ed44
duesex <duesex@localhost>
2004-05-21 02:54:13 +0000

    actually starts GrowlHelperApp now, now to stop it and make the rest of the buttons actually do stuff.

--- GrowlPref.h
+++ GrowlPref.h
@@ -5,14 +5,14 @@
 //  Created by Karl Adam on Wed Apr 21 2004.
 //
 
+#import <Cocoa/Cocoa.h>
 #import <PreferencePanes/PreferencePanes.h>
 
-
-@interface GrowlPref : NSPreferencePane 
-{
-
+@interface GrowlPref : NSPreferencePane {
+    IBOutlet NSButton *_startGrowlButton;
+    IBOutlet NSButton *_startGrowlLoginButton;
 }
 
-- (void) mainViewDidLoad;
-
+- (IBAction)startGrowl:(id)sender;
+- (IBAction)startGrowlAtLogin:(id)sender;
 @end
--- GrowlPref.m
+++ GrowlPref.m
@@ -16,10 +16,23 @@
 
 - (IBAction) startGrowl:(id) sender {
 	NSLog( @"start Growl" );	
+	NSString *helperPath = [[[self bundle] resourcePath] stringByAppendingString:@"/GrowlHelperApp.app"];
+	NSLog( @"tried to run %@", helperPath);
+	
+	if ( [_startGrowlButton state] == NSOnState ) {
+		if ( ! [[NSWorkspace sharedWorkspace] launchApplication:helperPath] ) {
+			[_startGrowlButton setState:NSOffState];
+		}		
+	} else {
+		NSLog( @"stop GrowlHelperApp somehow" );
+		//insert code here
+	}
+	
 }
 
 - (IBAction) startGrowlAtLogin:(id) sender {
 	NSLog( @"start Growl At Login" );
+	//insert code here
 }
 
 @end
