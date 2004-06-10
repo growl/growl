c7e8a67e56cc2642e9ae4002f284376a394956b7
hanji <hanji@localhost>
2004-06-10 17:14:04 +0000

    Added a simple perl script to generate a GrowlDefinesCarbon.h header from GrowlDefines.h for people wanting to use Growl with Carbon.

---
+++ GenCarbonHeader.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use POSIX;
+
+#Translate the Cocoa GrowlDefines.h file into one suitable for
+#use in Carbon apps, by
+#(1) Stripping the protocol information from the file, and
+#(2) Converting all @"String" forms into CFSTR("String")
+
+my $infile = shift || "GrowlDefines.h";
+my $outfile = shift || "GrowlDefinesCarbon.h";
+my $line;
+
+open(COCOA, "<", $infile) or die("Couldn't open $infile: $!");
+open(CARBON, ">", $outfile);
+
+my $date = POSIX::strftime("%a %b %Y", localtime);
+print CARBON <<ENDOFHEADER;
+//
+// $outfile
+//
+// Automatically generated from $infile on $date by GenCarbonHeader.pl
+//
+
+ENDOFHEADER
+
+while(<COCOA> =~ m{^//}) {}	#Strip the initial comment header
+
+#Loop until we hit the protocol declarations
+while(($line = <COCOA>) !~ m{^@})
+{
+	$line =~ s/@"(.*)"/CFSTR("$1")/g;
+	print CARBON $line;
+}
+
+close(CARBON);
+close(COCOA);
