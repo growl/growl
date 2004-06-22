5098e0360d4adb164f62072fda2502c2e8e18ecd
hanji <hanji@localhost>
2004-06-22 13:16:17 +0000

    Moving GrowlTunes, and cleaning it up, part 2.

---
+++ Examples/GrowlTunes/GrowlTunesController.h
@@ -0,0 +1,40 @@
+//
+//  GrowlTunesController.h
+//  GrowlTunes
+//
+//  Created by Nelson Elhage on Mon Jun 21 2004.
+//  Copyright (c) 2004 Nelson Elhage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define ITUNES_TRACK_CHANGED	@"iTunes-ChangedTracks"
+#define ITUNES_PAUSED			@"iTunes-Paused"
+#define ITUNES_STOPPED			@"iTunes-Stopped"
+#define ITUNES_PLAYING			@"iTunes-Playing"
+
+#define POLL_INTERVAL			3
+
+typedef enum {
+	itPLAYING,
+	itPAUSED,
+	itSTOPPED,
+	itUNKNOWN
+} iTunesState;
+
+@interface GrowlTunesController : NSObject {
+	NSTimer				* pollTimer;
+	NSAppleScript		* pollScript;
+	
+	NSAppleScript		* getTrackScript;
+	NSAppleScript		* getArtistScript;
+	NSAppleScript		* getArtworkScript;
+	
+	iTunesState			  state;
+	int					  trackID;		//The "database ID" of the last-polled track in iTunes, -1 for none
+}
+
+- (void)registerGrowl:(void *)context;
+- (void)poll: (NSTimer *)timer;
+
+@end
