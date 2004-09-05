661e154a6f8861be594991f90369ddaeccb40aff
toby <toby@localhost>
2004-09-05 22:31:40 +0000

    Make this work regardless of cwd.

--- Bindings/tcl/example.tcl
+++ Bindings/tcl/example.tcl
@@ -1,7 +1,7 @@
-#!/usr/bin/env tclsh
 package require growl
 
-growl register ExampleTclApp "warning error" pwrdLogo75.gif
+set curdir [file dirname [info script]]
+growl register ExampleTclApp "warning error" ${curdir}/pwrdLogo75.gif
 growl post warning "TclGrowl launched" "Hi there!"
 after 10000
-growl post error "TclGrowl quitting" "Bye!" ../../images/icons/growl-icon-(png).png
+growl post error "TclGrowl quitting" "Bye!" ${curdir}/../../images/icons/growl-icon-(png).png
