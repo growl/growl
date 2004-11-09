d55dc1bb9b008b9e772d3238594471cacc2ffabb
Ingmar Stein <IngmarStein@users.noreply.github.com>
2004-11-09 07:22:42 +0000

    Oops: forgot two file last time

---
+++ Bindings/applescript/GrowlRegisterScriptCommand.m
@@ -0,0 +1,76 @@
+//
+//  GrowlRegisterScriptCommand.m
+//  Growl
+//
+//  Created by Ingmar Stein on Tue Nov 09 2004.
+//  Copyright (c) 2004 Ingmar Stein. All rights reserved.
+//
+
+#import "GrowlRegisterScriptCommand.h"
+#import "GrowlApplicationBridge.h"
+#import "GrowlController.h"
+#import "NSGrowlAdditions.h"
+
+#define KEY_APP_NAME					@"asApplication"
+#define KEY_NOTIFICATIONS_ALL			@"allNotifications"
+#define KEY_NOTIFICATIONS_DEFAULT		@"defaultNotifications"
+#define KEY_ICON_APP_NAME				@"iconOfApplication"
+
+#define ERROR_EXCEPTION						1
+
+@implementation GrowlRegisterScriptCommand
+
+-(id)performDefaultImplementation
+{
+	NSDictionary* args = [self evaluatedArguments];
+
+	// should validate params better!
+	NSString *appName = [args valueForKey:KEY_APP_NAME];
+	NSArray *allNotifications = [args valueForKey:KEY_NOTIFICATIONS_ALL];
+	NSArray *defaultNotifications = [args valueForKey:KEY_NOTIFICATIONS_DEFAULT];
+	NSString *iconOfApplication = [args valueForKey:KEY_ICON_APP_NAME];
+
+	NSMutableDictionary* registerDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+		appName, GROWL_APP_NAME,
+		allNotifications, GROWL_NOTIFICATIONS_ALL,
+		defaultNotifications, GROWL_NOTIFICATIONS_DEFAULT,
+		nil];
+
+	NS_DURING
+		NSImage* icon = nil;
+		if (iconOfApplication != nil) {
+			icon = [[NSWorkspace sharedWorkspace] iconForApplication:iconOfApplication];
+			if (icon != nil) {
+				[icon setSize:NSMakeSize(128.,128.)];
+				[registerDict setObject:[icon TIFFRepresentation] forKey:GROWL_APP_ICON];
+			}
+		}
+
+		[[GrowlController singleton] _registerApplicationWithDictionary:registerDict];
+	NS_HANDLER
+		NSLog (@"error processing AppleScript request: %@", localException);
+		[self setError:ERROR_EXCEPTION failure:localException];
+	NS_ENDHANDLER
+
+	return nil;
+}
+
+- (void) setError:(int) errorCode
+{
+	[self setError:errorCode failure:nil];
+}
+
+- (void) setError:(int) errorCode failure:(id) failure
+{
+	[self setScriptErrorNumber:errorCode];
+	NSString* str = nil;
+	switch (errorCode) {
+		case ERROR_EXCEPTION:
+			str = [NSString stringWithFormat:@"Exception raised while processing: %@", failure];
+			break;
+	}
+	if (str != nil)
+		[self setScriptErrorString:str];
+}
+
+@end
