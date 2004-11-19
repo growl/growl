b03496e3a7673a4c6b16be116b15d22acd128145
Ingmar Stein <IngmarStein@users.noreply.github.com>
2004-11-19 19:01:41 +0000

    Added format for UDP registrations, not implemented yet.

--- GrowlUDPServer.h
+++ GrowlUDPServer.h
@@ -11,17 +11,59 @@
 
 #define GROWL_UDP_PORT 9887
 
+#define GROWL_TYPE_REGISTRATION	0
+#define GROWL_TYPE_NOTIFICATION	1
+
+/*
+ * This struct is common to all incoming network packets and identifies
+ * the type of the packet.
+ */
+struct GrowlNetworkPacket {
+	unsigned char type;
+};
+
+/*
+ * A registration packet.
+ */
+struct GrowlNetworkRegistration {
+	struct GrowlNetworkPacket common;
+	struct GrowlNetworkRegistrationFlags {
+		unsigned reserved: 31;
+		unsigned hasIcon:  1;
+	} flags; //size = 32 (31 + 1)
+	unsigned int appNameLen;
+	unsigned int numAllNotifications;
+	unsigned int numDefaultNotifications;
+	unsigned int appIconLen;
+	/*
+	 * Variable sized. Format:
+	 * <application name><all notifications><default notifications>[application icon]
+	 */
+	unsigned char data[];
+};
+
+/**
+ * A notification packet.
+ */
 struct GrowlNetworkNotification {
-	struct {
-		unsigned reserved: 28;
+	struct GrowlNetworkPacket common;
+	struct GrowlNetworkNotificationFlags {
+		unsigned reserved: 25;
+		unsigned hasIcon:  1;
+		unsigned iconType: 2; // 0 = URL, 1 = icon of application, 2=icon of file, 3=image data
 		signed   priority: 3;
 		unsigned sticky:   1;
-	} flags; //size = 32 (28 + 3 + 1)
-	unsigned nameLen;
-	unsigned titleLen;
-	unsigned descriptionLen;
-	unsigned appNameLen;
-	unsigned char data[];	/* variable sized */
+	} flags; //size = 32 (25 + 1 + 2 + 3 + 1)
+	unsigned int nameLen;
+	unsigned int titleLen;
+	unsigned int descriptionLen;
+	unsigned int appNameLen;
+	unsigned int iconLen;
+	/*
+	 * Variable sized. Format:
+	 * <notification name><title><description><application name>[icon]
+	 */
+	unsigned char data[];
 };
 
 @interface GrowlUDPServer : NSObject {
--- GrowlUDPServer.m
+++ GrowlUDPServer.m
@@ -9,6 +9,7 @@
 
 #import "GrowlUDPServer.h"
 #import "GrowlController.h"
+#import "NSGrowlAdditions.h"
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -55,7 +56,9 @@
 	char *title;
 	char *description;
 	char *applicationName;
-	unsigned notificationNameLen, titleLen, descriptionLen, priority, applicationNameLen;
+	char *icon;
+	unsigned int notificationNameLen, titleLen, descriptionLen, priority, applicationNameLen;
+	unsigned int iconLen, length;
 	BOOL isSticky;
 
 	NSDictionary *userInfo = [aNotification userInfo];
@@ -63,35 +66,62 @@
 	
 	if ( ![error intValue] ) {
 		NSData *data = (NSData *)[userInfo objectForKey:@"NSFileHandleNotificationDataItem"];
-		if ( [data length] >= sizeof(struct GrowlNetworkNotification) ) {
-			struct GrowlNetworkNotification *nn = (struct GrowlNetworkNotification *)[data bytes];
-			
-			priority = nn->flags.priority;
-			isSticky = nn->flags.sticky;
-			notificationName = nn->data;
-			notificationNameLen = ntohl( nn->nameLen );
-			title = notificationName + notificationNameLen;
-			titleLen = ntohl( nn->titleLen );
-			description = title + titleLen;
-			descriptionLen = ntohl( nn->descriptionLen );
-			applicationName = description + descriptionLen;
-			applicationNameLen = ntohl( nn->appNameLen );
+		length = [data length];
+		if ( length >= sizeof(struct GrowlNetworkPacket) ) {
+			struct GrowlNetworkPacket *packet = (struct GrowlNetworkPacket *)[data bytes];
+			switch( packet->type ) {
+				case GROWL_TYPE_REGISTRATION:
+					if ( length >= sizeof(struct GrowlNetworkRegistration) ) {
+						BOOL enabled = [[[GrowlPreferences preferences] objectForKey:GrowlRemoteRegistrationKey] boolValue];
+						if ( enabled ) {
+							struct GrowlNetworkRegistration *nr = (struct GrowlNetworkRegistration *)packet;
+							// TODO
+						}
+					} else {
+						NSLog( @"GrowlUDPServer: received runt registration packet." );
+					}
+					break;
+				case GROWL_TYPE_NOTIFICATION:
+					if ( length >= sizeof(struct GrowlNetworkNotification) ) {
+						struct GrowlNetworkNotification *nn = (struct GrowlNetworkNotification *)packet;
 
-			if( [data length] >= sizeof(struct GrowlNetworkNotification) + notificationNameLen
-				+ titleLen + descriptionLen + applicationNameLen ) {
-				NSDictionary *notificationInfo;
-				notificationInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-					[NSString stringWithUTF8String:notificationName length:notificationNameLen], GROWL_NOTIFICATION_NAME,
-					[NSString stringWithUTF8String:applicationName length:applicationNameLen], GROWL_APP_NAME,
-					[NSString stringWithUTF8String:title length:titleLen], GROWL_NOTIFICATION_TITLE,
-					[NSString stringWithUTF8String:description length:descriptionLen], GROWL_NOTIFICATION_DESCRIPTION,
-					[NSNumber numberWithInt:priority], GROWL_NOTIFICATION_PRIORITY,
-					[NSNumber numberWithBool:isSticky], GROWL_NOTIFICATION_STICKY,
-					nil];
+						priority = nn->flags.priority;
+						isSticky = nn->flags.sticky;
+						notificationName = nn->data;
+						notificationNameLen = ntohl( nn->nameLen );
+						title = notificationName + notificationNameLen;
+						titleLen = ntohl( nn->titleLen );
+						description = title + titleLen;
+						descriptionLen = ntohl( nn->descriptionLen );
+						applicationName = description + descriptionLen;
+						applicationNameLen = ntohl( nn->appNameLen );
+						icon = applicationName + applicationNameLen;
+						iconLen = ntohl( nn->iconLen );
+						// TODO: icon
+
+						if( length >= sizeof(struct GrowlNetworkNotification) + notificationNameLen
+								+ titleLen + descriptionLen + applicationNameLen ) {
+							NSDictionary *notificationInfo;
+							notificationInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+								[NSString stringWithUTF8String:notificationName length:notificationNameLen], GROWL_NOTIFICATION_NAME,
+								[NSString stringWithUTF8String:applicationName length:applicationNameLen], GROWL_APP_NAME,
+								[NSString stringWithUTF8String:title length:titleLen], GROWL_NOTIFICATION_TITLE,
+								[NSString stringWithUTF8String:description length:descriptionLen], GROWL_NOTIFICATION_DESCRIPTION,
+								[NSNumber numberWithInt:priority], GROWL_NOTIFICATION_PRIORITY,
+								[NSNumber numberWithBool:isSticky], GROWL_NOTIFICATION_STICKY,
+								nil];
 				
-				[[GrowlController singleton] dispatchNotificationWithDictionary:notificationInfo];
-			} else {
-				NSLog( @"GrowlUDPServer: received runt packet." );
+							[[GrowlController singleton] dispatchNotificationWithDictionary:notificationInfo];
+						} else {
+							NSLog( @"GrowlUDPServer: received invalid notification packet." );
+						}
+					} else {
+						NSLog( @"GrowlUDPServer: received runt notification packet." );
+					}
+					break;
+				default:
+					NSLog( @"GrowlUDPServer: received packet of invalid type." );
+					break;
 			}
 		} else {
 			NSLog( @"GrowlUDPServer: received runt packet." );
