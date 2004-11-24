623f82d7bb69e9dc575a2a343b7c95cf5e495f95
hanji <hanji@localhost>
2004-11-24 03:22:33 +0000

    Pudge <Chris Nandor>'s patch to Mac::Growl to add image support, and to add support for builds of perl without PerlObjCBridge installed.

--- Bindings/perl/Mac-Growl/lib/Mac/Growl.pm
+++ Bindings/perl/Mac-Growl/lib/Mac/Growl.pm
@@ -4,28 +4,86 @@ use 5.008001;
 use strict;
 use warnings;
 
-require Exporter;
+our $VERSION = '0.60';
 
-use Foundation;
+use base 'Exporter';
+our @EXPORT = qw();
+our @EXPORT_OK = qw(RegisterNotifications PostNotification);
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
-our @ISA = qw(Exporter);
+#### we have various options here ...
+#### in declining order of preference, we pick an implementation to use,
+#### and stick with it.
+our($base, $glue, $helper);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
+# we could use gotos or something, but this is the most efficient and reliable
+# way; we could also try to put all the implementations in one function, but
+# that's just messy, and I'd rather have it be messy here than there. -- pudge
+sub _Define_Subs {
+	no warnings 'redefine';
 
-# This allows declaration	use Mac::Growl ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-RegisterNotification PostNotifications
-) ] );
+	sub RegisterNotifications($$$;$);
+	sub PostNotification($$$$;$$);
 
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+	sub Foundation_RegisterNotifications($$$;$);
+	sub Foundation_PostNotification($$$$;$$);
 
-our @EXPORT = qw(
-	
-);
+	sub Glue_RegisterNotifications($$$;$);
+	sub Glue_PostNotification($$$$;$$);
+
+	sub AppleScript_RegisterNotifications($$$;$);
+	sub AppleScript_PostNotification($$$$;$$);
+
+	if ($base eq 'Foundation') {
+		*RegisterNotifications = *Foundation_RegisterNotifications{CODE};
+		*PostNotification      = *Foundation_PostNotification{CODE};
+	} elsif ($base eq 'Mac::Glue') {
+		*PostNotification      = *Glue_PostNotification{CODE};
+		*RegisterNotifications = *Glue_RegisterNotifications{CODE};
+	} else { # AppleScript
+		*PostNotification      = *AppleScript_PostNotification{CODE};
+		*RegisterNotifications = *AppleScript_RegisterNotifications{CODE};
+	}
+}
+
+sub BEGIN {
+	$helper = 'GrowlHelperApp';
+
+# Foundation not currently working properly, see TODO
+#	eval 'require Foundation';
+#	$base = 'Foundation' unless $@;
+
+	if (!$base) {
+		eval 'require Mac::Glue';
+		unless ($@) {
+			eval { $glue = Mac::Glue->new($helper) };
+			$base = 'Mac::Glue' if $glue;
+		}
+	}
+
+	for my $applescript (qw(Mac::OSA::Simple MacPerl Mac::AppleScript)) {
+		if (!$base) {
+			eval "require $applescript";
+			$base = $applescript unless $@;
+		}
+	}
+
+	if (!$base) {
+		chomp(my $res = `osascript -e1`);
+		$base = 'osascript' if $res eq 1;
+	}
+
+	if (!$base) {
+		die "No way to invoke Growl, please see `perldoc Mac::Growl`";
+	}
+
+#	warn "Using $base for " . __PACKAGE__, "\n";
+
+	_Define_Subs();
+}
+
+#################################
+### Foundation implementation ###
 
 use constant GROWL_APP_NAME						=> "ApplicationName";
 use constant GROWL_APP_ICON						=> "ApplicationIcon";
@@ -47,90 +105,299 @@ use constant GROWL_NOTIFICATION					=> "GrowlNotification";
 use constant GROWL_PING							=> "Honey, Mind Taking Out The Trash";
 use constant GROWL_PONG							=> "What Do You Want From Me, Woman";
 
-our $VERSION = '0.25';
-
-sub RegisterNotifications($$$)
+sub Foundation_RegisterNotifications($$$;$)
 {
-	my $appName = $_[0];
-	my @allNotes = @{$_[1]};
-	my @defaultNotes = @{$_[2]};
+	my($appName, $allNotes, $defaultNotes, $iconOfApp) = @_;
 	
 	my $appString = NSString->stringWithCString_($appName);
 	my $notesArray = NSMutableArray->array();
 	my $defaultArray = NSMutableArray->array();
-	
-	my $note;
-	
-	$notesArray->addObject_($note) while($note = shift @allNotes);
-		
-	$defaultArray->addObject_($note)	 while($note = shift @defaultNotes);
-		
+
+	# can't do this, because NSImage not available in Foundation
+	if ($iconOfApp) {
+	}
+
+	$notesArray->addObject_($_)   for @$allNotes;
+	$defaultArray->addObject_($_) for @$defaultNotes;
+
 	my $regDict = NSMutableDictionary->dictionary();
 	$regDict->setObject_forKey_($appName,GROWL_APP_NAME);
 	$regDict->setObject_forKey_($notesArray,GROWL_NOTIFICATIONS_ALL);
 	$regDict->setObject_forKey_($defaultArray,GROWL_NOTIFICATIONS_DEFAULT);
-	
+
 	NSDistributedNotificationCenter->defaultCenter()->postNotificationName_object_userInfo_(
-				GROWL_APP_REGISTRATION,
-				undef,
-				$regDict);			
+		GROWL_APP_REGISTRATION,
+		undef,
+		$regDict
+	);
 }
 
-sub PostNotification($$$$;$)
+sub Foundation_PostNotification($$$$;$$)
 {
-	my ($appName, $noteName, $noteTitle, $noteDescription, $sticky) = @_;
-	$sticky = $sticky?1:0;
-	
+	my($appName, $noteName, $noteTitle, $noteDescription, $sticky, $image) = @_;
+	$sticky = $sticky ? 1 : 0;
+
 	my $noteDict = NSMutableDictionary->dictionary();
 	$noteDict->setObject_forKey_($noteName,GROWL_NOTIFICATION_NAME);
 	$noteDict->setObject_forKey_($appName,GROWL_APP_NAME);
 	$noteDict->setObject_forKey_($noteTitle,GROWL_NOTIFICATION_TITLE);
 	$noteDict->setObject_forKey_($noteDescription,GROWL_NOTIFICATION_DESCRIPTION);
 	$noteDict->setObject_forKey_(NSNumber->numberWithBool_($sticky),GROWL_NOTIFICATION_STICKY);
-	
+
+	# can't do this, because NSImage not available in Foundation
+	if ($image) {
+	}
+
 	NSDistributedNotificationCenter->defaultCenter()->postNotificationName_object_userInfo_(
-				GROWL_NOTIFICATION,
-				undef,
-				$noteDict);	
+		GROWL_NOTIFICATION,
+		undef,
+		$noteDict
+	);
 }
 
-# Preloaded methods go here.
+
+################################
+### Mac::Glue implementation ###
+
+sub Glue_RegisterNotifications($$$;$)
+{
+	my($appName, $allNotes, $defaultNotes, $iconOfApp) = @_;
+
+	for my $notes ($allNotes, $defaultNotes) {
+		$notes = [ map {
+			Mac::Glue::param_type(Mac::AppleEvents::typeChar(), $_)
+		} @$notes ];
+	}
+
+	$glue->register(
+		as_application			=> $appName,
+		all_notifications		=> $allNotes,
+		default_notifications	=> $defaultNotes,
+		icon_of_application		=> $iconOfApp,
+	);
+}
+
+sub Glue_PostNotification($$$$;$$)
+{
+	my($appName, $noteName, $noteTitle, $noteDescription, $sticky, $image) = @_;
+	$sticky = $sticky ? 1 : 0;
+
+	my %params = (
+		application_name	=> $appName,
+		with_name			=> $noteName,
+		title				=> $noteTitle,
+		description			=> $noteDescription,
+		sticky				=> $sticky
+	);
+
+	my $image_url = _Fix_Image_Path($image);
+	$params{image_from_url} = $image_url if $image_url;
+
+	$glue->notify(%params);
+}
+
+
+##################################
+### AppleScript implementation ###
+
+sub _Fix_AppleScript_String(\$);
+
+sub AppleScript_RegisterNotifications($$$;$)
+{
+	my($appName, $allNotes, $defaultNotes, $iconOfApp) = @_;
+
+	# protect quotes and slashes
+	for ($appName, $iconOfApp) {
+		next unless defined;
+		_Fix_AppleScript_String($_);
+	}
+
+	for my $list ($allNotes, $defaultNotes) {
+		_Fix_AppleScript_String($_) for @$list;
+		my $string = join('","', @$list);
+		$list = $string;
+	}
+
+	my $script = qq'tell application "$helper" to register ' .
+		qq'as application "$appName" ' .
+		qq'all notifications ["$allNotes"] default notifications ["$defaultNotes"]';
+	$script .= qq' icon of application "$iconOfApp"' if $iconOfApp;
+
+	_Execute_AppleScript($script);
+}
+
+sub AppleScript_PostNotification($$$$;$$)
+{
+	my($appName, $noteName, $noteTitle, $noteDescription, $sticky, $image) = @_;
+	$sticky = $sticky ? 1 : 0;
+
+	# protect quotes and slashes
+	for ($appName, $noteName, $noteTitle, $noteDescription) {
+		next unless defined;
+		_Fix_AppleScript_String($_);
+	}
+
+	my $script = qq'tell application "$helper" to notify ' .
+		qq'application name "$appName" with name "$noteName" ' .
+		qq'title "$noteTitle" description "$noteDescription"';
+	$script .= ' sticky true' if $sticky;
+
+	my $image_url = _Fix_Image_Path($image);
+	$script .= qq' image from url "$image_url"' if $image_url;
+
+	_Execute_AppleScript($script);
+}
+
+sub _Fix_AppleScript_String(\$)
+{
+	my($string) = @_;
+	$$string =~ s/\\/\\\\/g;
+	$$string =~ s/"/\\"/g;
+}
+
+sub _Execute_AppleScript
+{
+	my($script, $return) = @_;
+	my $reply;
+
+	# warn $script, "\n";
+
+	if ($base eq 'Mac::OSA::Simple')
+	{
+		$reply = Mac::OSA::Simple::applescript($script);
+	}
+
+	elsif ($base eq 'MacPerl')
+	{
+		$reply = MacPerl::DoAppleScript($script);
+	}
+
+	elsif ($base eq 'Mac::AppleScript')
+	{
+		$reply = Mac::AppleScript::RunAppleScript($script);
+	}
+
+	elsif ($base eq 'osascript')
+	{
+		if ($return) {
+			$script =~ s/\\/\\\\/g;
+			$script =~ s/'/'\''/g;
+			chomp($reply = `osascript -ss -e '$script' 2>/dev/null`);
+		} else {
+			system('osascript', '-e', $script);
+		}
+	}
+
+	if ($return) {
+		$reply =~ s/^"(.+)"$/$1/;
+		return $reply;
+	}
+}
+
+
+#######################
+### Misc. Functions ###
+
+{
+my $uri_file;
+
+sub _Fix_Image_Path
+{
+	require File::Spec;
+	unless (defined $uri_file) {
+		eval 'require URI::file';
+		$uri_file = $@ ? 0 : 1;
+	}
+
+	my($image) = @_;
+	my $path;
+
+	if (defined $image && length $image) {
+		$path = File::Spec->rel2abs($image);
+		if (-e $path) {
+			if ($uri_file) {
+				my $uri = URI::file->new($path);
+				return $uri->as_string if $uri;
+			} else {
+				# URI::file will be available if Mac::Glue
+				# is, so this only needs to be implemented
+				# in AppleScript; yes, it's a hack, but
+				# it's better to work out of the box than
+				# to not
+				my $script = <<EOT;
+tell application "Finder"
+   set thisfile to POSIX file "$path"
+   set thisdoc to document file thisfile
+   return URL of thisdoc
+end tell
+EOT
+				my $reply = _Execute_AppleScript($script, 1);
+				# Growl being excessively anal
+				$reply =~ s|file://localhost/|file:///|;
+				return $reply;
+			}
+		}
+	}
+
+	return;
+}
+}
 
 1;
+
 __END__
 
 =head1 NAME
 
-Mac::Growl - Perl module for registering and sending Growl Notifications on OS X
+Mac::Growl - Perl module for registering and sending Growl Notifications on Mac OS X
 
 =head1 SYNOPSIS
 
-  use Mac::Growl qw{RegisterNotifications, PostNotification};
+  use Mac::Growl ':all';
 
-  RegisterNotifications("MyPerlApp", \@allNotifications, \@defaultNotifications);
-  PostNotification("MyPerlApp", $notificationName, $notificationTitle, $notificationDescription[, $sticky]);
+  RegisterNotifications("MyPerlApp", \@allNotifications,
+    \@defaultNotifications[, $iconOfApp]);
+
+  PostNotification("MyPerlApp", $notificationName, $notificationTitle,
+    $notificationDescription[, $sticky, $image_path]);
 
 =head1 DESCRIPTION
 
-Mac::Growl provides a simple notification for perl apps to register themselves with and send notifications to the OS X notification application Growl.
-	
+Mac::Growl provides a simple notification for perl apps to register themselves with and send notifications to the Mac OS X notification application Growl.
+
 Mac::Growl defines two methods:
-	
+
 =over 4
 
-=item RegisterNotifications(appname, allNotifications, defaultNotifications);
+=item RegisterNotifications(appname, allNotifications, defaultNotifications[, iconOfApp]);
 
-RegisterNotifications takes the name of the application sending notifications, as well as a reference to a list of all notifications the app sends out, and a reference to an array of all the notifications to be enabled by default.
+RegisterNotifications takes the name of the application sending notifications, as well as a reference to a list of all notifications the app sends out, and a reference to an array of all the notifications to be enabled by default.  Also, optionally accepts the name of an application whose icon to use by default.
 
-=item PostNotification(appname, name, title, description[, sticky]);
+=item PostNotification(appname, name, title, description[, sticky, image_path]);
 
-PostNotification takes the name of the sending application (normally the same as passed to the Register call), the name of the notification (should be one of the allNotification list passed to Register), and a title and description to be displayed by Growl. Also, optionally accepts a "sticcky" flag, which, if true, will cause the notification to remain until dismissed, instead of timing out normally.
+PostNotification takes the name of the sending application (normally the same as passed to the Register call), the name of the notification (should be one of the allNotification list passed to Register), and a title and description to be displayed by Growl. Also, optionally accepts a "sticky" flag, which, if true, will cause the notification to remain until dismissed, instead of timing out normally; and an image path, a path to a file containing the image for the notification.
 
 =back
 
-=head2 EXPORT
+
+=head1 CAVEATS
+
+This module is designed to use L<Foundation>, a perl module included with Mac OS X that is probably only available if you are using the default system perl.  If Foundation is not available, this module will attempt to talk to Growl using Apple events instead of the Cocoa API.  (L<TODO>: currently, Foundation is disabled.)
+
+It tries various perl modules to accomplish this, in descending order of preference: L<Mac::Glue>*, L<Mac::OSA::Simple>, L<MacPerl> (which defines C<DoAppleScript>), and L<Mac::AppleScript>.  As a last resort, it will use C<osascript(1)>, a command line program that should be available on all Mac OS X machines.
+
+*In order to use Mac::Glue, you must run C<sudo gluemac GrowlHelper.app> (from the F</Library/PreferencePanes/Growl.prefPane/Contents/Resources/> directory) first on the command line, to create the glue file.
+
+
+=head1 TODO
+
+For the Foundation implementation, make the iconOfApp argument work in C<RegisterNotifications> and image argument work for C<PostNotification>.  Somebody else ... ?
+
+
+=head1 EXPORT
 
 None by default.
+
 
 =head1 SEE ALSO
 
@@ -138,9 +405,12 @@ http://growl.info
 
 #growl on irc.freenode.net
 
-=head1 AUTHOR
 
-Nelson Elhage, E<lt>nelhage@gmail.comE<gt>
+=head1 AUTHORS
+
+Nelson Elhage E<lt>nelhage@gmail.comE<gt>,
+Chris Nandor E<lt>pudge@perl.orgE<gt>
+
 
 =head1 COPYRIGHT AND LICENSE
 
--- Bindings/perl/Mac-Growl/t/Mac-Growl.t
+++ Bindings/perl/Mac-Growl/t/Mac-Growl.t
@@ -1,15 +1,69 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl Mac-Growl.t'
 
+use warnings;
+use strict;
+
 #########################
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 1;
+use Test::More tests => 11;
 BEGIN { use_ok('Mac::Growl') };
+
+use Mac::Growl ':all';
 
 #########################
 
-Mac::Growl::RegisterNotifications("PerlApp",["Perl Notification"],["Perl Notification"]);
-Mac::Growl::PostNotification("PerlApp","Perl Notification","Congratulations","Mac::Growl is working.");
-Mac::Growl::PostNotification("PerlApp","Perl Notification","If things are working...", "This should 'stick'",1);
+my $app    = 'PerlApp';
+my $as_app = 'Software Update';
+my @names  = ('a', 'b');
+my $image = '/Applications/Utilities/Installer.app/Contents/Resources/Caut.tiff';
+
+my %pkgs = map { $_ => 0 } qw(
+	Foundation
+	Mac::Glue
+	Mac::OSA::Simple
+	MacPerl
+	Mac::AppleScript
+);
+
+for my $pkg (keys %pkgs) {
+	eval "require $pkg";
+	$pkgs{$pkg} = $@ ? 0 : 1;
+}
+$pkgs{osascript} = 1;
+
+
+RegisterNotifications($app, \@names, [$names[0]], $as_app);
+pass("register $app");
+PostNotification($app, $names[0], 'Congratulations', 'Mac::Growl is working.');
+pass("notify");
+PostNotification($app, $names[0], 'If things are working...', 'This should "stick."', 1);
+pass("notify sticky");
+PostNotification($app, $names[1], 'You should not see this', 'Danger, Will Robinson!');
+pass("notify fail");
+
+my $old = $Mac::Growl::base;
+for my $pkg (sort keys %pkgs) {
+	SKIP: {
+		skip("Notify $pkg test", 1), next unless $pkgs{$pkg};
+		$Mac::Growl::base = $pkg;
+		Mac::Growl::_Define_Subs();
+
+		if ($pkg eq 'Mac::Glue') {
+			eval { $Mac::Growl::glue ||= Mac::Glue->new($Mac::Growl::helper) };
+			next if $@;
+		}
+
+		Mac::Growl::PostNotification(
+			$app, $names[0], "Testing $pkg", "Yay, $pkg works!", 0, $image
+		);
+		pass("notify $pkg");
+	}
+}
+
+$Mac::Growl::base = $old;
+Mac::Growl::_Define_Subs();
+
+__END__
