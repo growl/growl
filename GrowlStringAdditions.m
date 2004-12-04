11ece8873d104ef66c228f19f03ea58bb0675c5a
Peter Hosey <boredzo@users.noreply.github.com>
2004-12-04 10:29:50 +0000

    The Jaguar ellipsis code should be faster now, and it now has the Unicode nature (using U+2026 "HORIZONTAL ELLIPSIS" instead of '...'). We no longer use alloc + initWith... + autorelease when a simple fooWith... will do.

--- GrowlStringAdditions.m
+++ GrowlStringAdditions.m
@@ -8,8 +8,6 @@
 
 #import "GrowlStringAdditions.h"
 
-#define ELLIPSIS_STRING @"..."
-
 @implementation NSString (GrowlStringAdditions)
 
 - (void)drawWithEllipsisInRect:(NSRect)rect withAttributes:(NSDictionary *)attributes {
@@ -19,30 +17,43 @@
 		// use the built-in ellipsising system if possible
 		NSMutableParagraphStyle *ellipsisingStyle = [[[[NSParagraphStyle defaultParagraphStyle] mutableCopy] 
 			setLineBreakMode:NSLineBreakByTruncatingTail] autorelease];
-		NSMutableDictionary *md = [[[NSMutableDictionary alloc] initWithDictionary:attributes] autorelease];
+		NSMutableDictionary *md = [NSMutableDictionary dictionaryWithDictionary:attributes];
 		[md setObject:ellipsisingStyle forKey:NSParagraphStyleAttributeName];
 		[self drawInRect:rect withAttributes:md];
-		
 	} else {
 		// use our own ellipsising routine
 		NSSize mySize = [self sizeWithAttributes:attributes];
 		BOOL didTruncate = NO;
-		NSMutableString *newString = [[[NSMutableString alloc] initWithString:self] autorelease];
-		
+		NSMutableString *newString = [NSMutableString stringWithString:self];
+
+		unsigned length = [newString length];
+		NSRange range = {
+			.location = length,
+			.length   = 1U,
+		};
+
 		// while we don't fit (allowing room for the ellipsis), chop off the last character
 		while (mySize.width > (NSWidth(rect) - mySize.height)) {
+			if(!length) break;
+			range.location = --length;
+
 			didTruncate = YES;
-			[newString deleteCharactersInRange:NSMakeRange([newString length] - 1, 1)];
+			[newString deleteCharactersInRange:range];
 			mySize = [newString sizeWithAttributes:attributes];
 		}
 		
 		if (didTruncate) {
 			// drop any trailing spaces, it looks odd if we put an ellipsis after a space
-			while ([newString characterAtIndex:[newString length] - 1] == ' ') {
-				[newString deleteCharactersInRange:NSMakeRange([newString length] - 1, 1)];
+			while (length && ([newString characterAtIndex:range.location] == ' ')) {
+				range.location = --length;
+				[newString deleteCharactersInRange:range];
 			}
-			// add the ellipsis itself to indicate that there's missing text
-			[newString appendString:ELLIPSIS_STRING];
+
+			if(length) {
+				// add the ellipsis itself to indicate that there's missing text
+				static const char ellipsisUTF8[] = "\xE2\x80\xA6";
+				[newString appendString:[NSString stringWithUTF8String:ellipsisUTF8]];
+			}
 		}
 		
 		// draw the string in the supplied rect with the supplied attributes
