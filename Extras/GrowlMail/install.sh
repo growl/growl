fb4fb1015720cacd2fb04a363df5f31ecb1d40b4
toby <toby@localhost>
2005-01-05 06:10:58 +0000

    Massive rewrite. - Executes the various steps in a more logical fashion. - Does a clean build (by nuking the build directory). - Can be run from anywhere, does not use $PWD anymore. - Properly checks for an existing GrowlMail installation and moves it to the Trash. - Exits correctly when commands fail. - Quotes everything properly. - Uses -buildstyle Deployment. - Probably other stuff I forgot to mention.

--- Extras/GrowlMail/install.sh
+++ Extras/GrowlMail/install.sh
@@ -1,50 +1,53 @@
 #!/bin/bash
-# Script for installing GrowlMail
-# This will install GrowlMail, quit Mail, enable plug-ins in Mail, and relaunch Mail. It will move an old GrowlMail (if present) to the trash.
-# It uses the home folder as the destination by default, though this can be changed.
+#
+# install.sh - Script for installing GrowlMail.
+#
+# - Build GrowlMail
+# - Possibly move old GrowlMail to the Trash.
+# - Install new GrowlMail.
+# - Enable the plugin in Mail.
+# - Relaunch Mail.
 #
 
-SRC="$PWD/build"
-DEST="$HOME/Library/Mail/Bundles"
+GROWLMAIL="`dirname $0`"
+BUILD="$GROWLMAIL/build"
+SRC="$BUILD/GrowlMail.mailbundle"
 
-echo SRC="$SRC"
-echo DEST="$DEST"
+BUNDLES="$HOME/Library/Mail/Bundles"
+DEST="$BUNDLES/GrowlMail.mailbundle"
+
+echo "--> Building GrowlMail."
+echo "rm -r \"$BUILD\""
+rm -fr "$BUILD"
+echo "cd \"$GROWLMAIL\" && xcodebuild -buildstyle Deployment"
+(cd "$GROWLMAIL" && xcodebuild -buildstyle Deployment) || exit 1
 
 if test -e "$DEST"; then
-	echo "$0: GrowlMail exists, moving to trash"
-	echo "ditto --rsrc \"$SRC/GrowlMail.mailbundle\" \"$HOME/.Trash\""
-	ditto --rsrc "$SRC/GrowlMail.mailbundle" "$HOME/.Trash"
-	if test ! -d "$SRC/GrowlMail.mailbundle"; then
-		echo "rm \"$SRC/GrowlMail.mailbundle\""
-		rm "$SRC/GrowlMail.mailbundle"
-	fi
-else
-	echo "$0: GrowlMail is not there, making Bundles folder if necessary"
-	echo "mkdir \"$HOME/Library/Mail/Bundles\""
-	mkdir "$HOME/Library/Mail/Bundles"
-	if test ! -?; then
-		echo "$0: Could not create Bundles folder - try repairing your permissions"
-	fi
-fi
-if test ! -e "$SRC/GrowlMail.mailbundle"; then
-	echo xcodebuild
-	xcodebuild
-else
-	true
-fi
-if test -?; then
-	echo "ditto --rsrc \"$SRC/GrowlMail.mailbundle\" \"$DEST/GrowlMail.mailbundle\""
-	ditto --rsrc "$SRC/GrowlMail.mailbundle" "$DEST/GrowlMail.mailbundle"
+	echo "--> GrowlMail exists, moving to Trash."
+	echo "ditto -v --rsrc \"$DEST\" \"$HOME/.Trash/GrowlMail.mailbundle\""
+	ditto -v --rsrc "$DEST" "$HOME/.Trash/GrowlMail.mailbundle" || exit 1
+	echo "rm -r \"$DEST\""
+	rm -fr "$DEST"
 fi
 
-echo "$0: Enabling plug-ins in Mail (if they are already enabled, this will have no effect)"
+echo "--> Creating Bundles folder."
+echo "mkdir -p \"$BUNDLES\""
+mkdir -p "$BUNDLES" || exit 1
+
+echo "--> Installing GrowlMail."
+echo "ditto -v --rsrc \"$SRC\" \"$DEST\""
+ditto -v --rsrc "$SRC" "$DEST" || exit 1
+
+echo "--> Enabling plug-ins in Mail (if they are already enabled, this will have no effect)."
 echo defaults write com.apple.mail EnableBundles -bool YES
 defaults write com.apple.mail EnableBundles -bool YES
 echo defaults write com.apple.mail BundleCompatibilityVersion -int 1
 defaults write com.apple.mail BundleCompatibilityVersion -int 1
 
-echo "$0: Relaunching Mail if necessary (if Mail is not already running, this will do nothing)"
-if killall -s Mail 2>/dev/null; then
+if killall -s Mail 1>/dev/null 2>/dev/null; then
+	echo "--> Relaunching Mail."
 	echo "(osascript -l AppleScript -e 'quit application \"Mail\"') && open -a Mail"
 	(osascript -l AppleScript -e 'quit application "Mail"') && open -a Mail
 fi
+
+exit 0
