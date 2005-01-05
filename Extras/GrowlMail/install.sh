d1b0dcfd620e3515cb68d79e1ce992b55df04499
aranor <aranor@localhost>
2005-01-05 07:22:35 +0000

    Clean up and enhance GrowlMail installation script: - Quit Mail.app before setting defaults - Move old installation to trash rather than copying it - Clean up some of the echoed messages - Fix some quotes - Don't hardcode ditto and mv when used - set variables to them up top

--- Extras/GrowlMail/install.sh
+++ Extras/GrowlMail/install.sh
@@ -16,38 +16,49 @@ SRC="$BUILD/GrowlMail.mailbundle"
 BUNDLES="$HOME/Library/Mail/Bundles"
 DEST="$BUNDLES/GrowlMail.mailbundle"
 
-echo "--> Building GrowlMail."
-echo "rm -r \"$BUILD\""
-rm -fr "$BUILD"
+MV="/bin/mv"
+CP="/usr/bin/ditto -v --rsrc"
+
+if [[ -e "$BUILD" ]]; then
+	echo "--> Removing old build"
+	echo "rm -rf \"$BUILD\""
+	rm -fr "$BUILD"
+fi
+echo "--> Building GrowlMail"
 echo "cd \"$GROWLMAIL\" && xcodebuild -buildstyle Deployment \"SYMROOT=$BUILD\" \"OBJROOT=$BUILD\""
 (cd "$GROWLMAIL" && xcodebuild -buildstyle Deployment build "SYMROOT=$BUILD" "OBJROOT=$BUILD") || exit 1
 
-if test -e "$DEST"; then
+if [[ -e "$DEST" ]]; then
 	echo "--> GrowlMail exists, moving to Trash."
-	echo "ditto -v --rsrc \"$DEST\" \"$HOME/.Trash/GrowlMail.mailbundle\""
-	ditto -v --rsrc "$DEST" "$HOME/.Trash/GrowlMail.mailbundle" || exit 1
-	echo "rm -r \"$DEST\""
-	rm -fr "$DEST"
+	echo "$MV \"$DEST\" \"$HOME/.Trash/GrowlMail.mailbundle\""
+	$MV "$DEST" "$HOME/.Trash/GrowlMail.mailbundle" || exit 1
 fi
 
-echo "--> Creating Bundles folder."
+echo "--> Creating Bundles folder"
 echo "mkdir -p \"$BUNDLES\""
 mkdir -p "$BUNDLES" || exit 1
 
-echo "--> Installing GrowlMail."
-echo "ditto -v --rsrc \"$SRC\" \"$DEST\""
-ditto -v --rsrc "$SRC" "$DEST" || exit 1
+echo "--> Installing GrowlMail"
+echo "$CP \"$SRC\" \"$DEST\""
+$CP "$SRC" "$DEST" || exit 1
 
-echo "--> Enabling plug-ins in Mail (if they are already enabled, this will have no effect)."
-echo defaults write com.apple.mail EnableBundles -bool YES
+if killall -s Mail >/dev/null 2>/dev/null; then
+	echo "--> Quitting Mail"
+	echo "osascript -l AppleScript -e 'quit application \"Mail\"'"
+	osascript -l AppleScript -e 'quit application "Mail"'
+	MAIL_RUNNING=-YES-
+fi
+
+echo "--> Enabling plug-ins in Mail (if they are already enabled, this will have no effect)"
+echo "defaults write com.apple.mail EnableBundles -bool YES"
 defaults write com.apple.mail EnableBundles -bool YES
-echo defaults write com.apple.mail BundleCompatibilityVersion -int 1
+echo "defaults write com.apple.mail BundleCompatibilityVersion -int 1"
 defaults write com.apple.mail BundleCompatibilityVersion -int 1
 
-if killall -s Mail 1>/dev/null 2>/dev/null; then
-	echo "--> Relaunching Mail."
-	echo "(osascript -l AppleScript -e 'quit application \"Mail\"') && open -a Mail"
-	(osascript -l AppleScript -e 'quit application "Mail"') && open -a Mail
+if [[ ${MAIL_RUNNING:--NO-} == -YES- ]]; then
+	echo "--> Relaunching Mail"
+	echo "open -a Mail"
+	open -a Mail
 fi
 
 exit 0
