567fb25ced5bafc1f069a67d5488fba1fcf0b26e
aranor <aranor@localhost>
2005-01-05 10:26:03 +0000

    Major re-write of install.sh: - Refactor everything out into individual functions - Add helper functions for displaying stages and displaying/running commands in one step - Add usage information (with -h flag) - Add verbose flag (with -v) to show the commands that are executed - without verbose, just the stages are shown - Add targets clean/build/install/uninstall - install will call build (but not clean) before running - Add test so bundle-enabling defaults are not written if they already exist

--- Extras/GrowlMail/install.sh
+++ Extras/GrowlMail/install.sh
@@ -9,8 +9,9 @@
 # - Relaunch Mail.
 #
 
-GROWLMAIL="`dirname $0`"
-BUILD="$GROWLMAIL/build"
+NAME="GrowlMail"
+CURDIR="`dirname $0`"
+BUILD="$CURDIR/build"
 SRC="$BUILD/GrowlMail.mailbundle"
 
 BUNDLES="$HOME/Library/Mail/Bundles"
@@ -19,46 +20,139 @@ DEST="$BUNDLES/GrowlMail.mailbundle"
 MV="/bin/mv"
 CP="/usr/bin/ditto -v --rsrc"
 
-if [[ -e "$BUILD" ]]; then
-	echo "--> Removing old build"
-	echo "rm -rf \"$BUILD\""
-	rm -fr "$BUILD"
+UI_ECHO_PREFIX="-->"
+
+function run ()
+{
+	if [[ ${VERBOSE:--NO-} == '-YES-' ]]; then
+		echo "$*"
+		eval $*
+	else
+		exec 5>&1 6>&2 1>/dev/null 2>/dev/null
+		eval $*
+		exec 1>&5 2>&6
+	fi
+}
+
+function ui_echo ()
+{
+	echo $UI_ECHO_PREFIX $@
+}
+
+function stopMail ()
+{
+	if killall -s Mail >/dev/null 2>/dev/null; then
+		ui_echo "Quitting Mail"
+		run "osascript -l AppleScript -e 'quit application \"Mail\"'"
+		SHOULD_RESTART_MAIL=-YES-
+	fi
+}
+
+function startMail ()
+{
+	if [[ ${SHOULD_RESTART_MAIL:--NO-} == -YES- ]]; then
+		ui_echo "Relaunching Mail"
+		run "open -a Mail"
+	fi
+}
+
+function cleanGrowlMail ()
+{
+	if [[ -e "$BUILD" ]]; then
+		ui_echo "Removing old build"
+		run "rm -rf \"$BUILD\""
+	fi
+}
+
+function buildGrowlMail ()
+{
+	ui_echo "Building GrowlMail"
+	run "cd \"$CURDIR\" && xcodebuild -buildstyle Deployment build \"SYMROOT=$BUILD\" \"OBJROOT=$BUILD\"" || exit 1
+}
+
+function installGrowlMail ()
+{
+	if [[ ! -d "$BUNDLES" ]]; then
+		ui_echo "Creating Bundles folder"
+		run "mkdir -p \"$BUNDLES\"" || exit 1
+	fi
+
+	ui_echo "Installing GrowlMail"
+	run "$CP \"$SRC\" \"$DEST\"" || exit 1
+
+	local ENABLE_BUNDLES HAS_ENABLE_BUNDLES \
+		  HAS_BUNDLE_COMPATIBILITY_VERSION BUNDLE_COMPATIBILITY_VERSION
+	exec 5>&1 6>&2 1>/dev/null 2>/dev/null
+	ENABLE_BUNDLES=`defaults read com.apple.mail EnableBundles`
+	HAS_ENABLE_BUNDLES=$?
+	BUNDLE_COMPATIBILITY_VERSION=`defaults read com.apple.mail BundleCompatibilityVersion`
+	HAS_BUNDLE_COMPATIBILITY_VERSION=$?
+	exec 1>&5 2>&6
+	
+	if [[ ! ( $HAS_ENABLE_BUNDLES && $ENABLE_BUNDLES == 1 &&
+			  $HAS_BUNDLE_COMPATIBILITY_VERSION && $BUNDLE_COMPATIBILITY_VERSION == 1 ) ]]; then
+		ui_echo "Enabling plug-ins in Mail"
+		run "defaults write com.apple.mail EnableBundles -bool YES"
+		run "defaults write com.apple.mail BundleCompatibilityVersion -int 1"
+	fi
+}
+
+function uninstallGrowlMail ()
+{
+	if [[ -e "$DEST" ]]; then
+		ui_echo "Moving existing GrowlMail installation to Trash"
+		mkdir -p "$HOME/.Trash"		# Just in case
+		local BASE HEAD INC
+		BASE=`basename $DEST`
+		HEAD=$BASE
+		INC=0
+		while [[ -e "$HOME/.Trash/$HEAD" ]]; do
+			let INC=$INC+1
+			HEAD=$BASE.$INC
+		done
+		run "$MV \"$DEST\" \"$HOME/.Trash/$HEAD\"" || exit 1
+	fi
+}
+
+function usage ()
+{
+	cat <<-EOF
+	usage: $0 [-h] [-v] action
+	
+	    action can be one of the following:
+	        clean:      cleans up after any builds
+	        build:      builds $NAME
+	        install:    builds and installs $NAME
+	        uninstall:  uninstalls $NAME
+	EOF
+	exit 1
+}
+
+while getopts vh FLAG; do
+	case $FLAG in
+		v)		VERBOSE=-YES-	;;
+		h|\?)	usage; exit 1	;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+
+if [[ $# -ne 1 ]]; then
+	usage
 fi
-echo "--> Building GrowlMail"
-echo "cd \"$GROWLMAIL\" && xcodebuild -buildstyle Deployment \"SYMROOT=$BUILD\" \"OBJROOT=$BUILD\""
-(cd "$GROWLMAIL" && xcodebuild -buildstyle Deployment build "SYMROOT=$BUILD" "OBJROOT=$BUILD") || exit 1
 
-if [[ -e "$DEST" ]]; then
-	echo "--> GrowlMail exists, moving to Trash."
-	echo "$MV \"$DEST\" \"$HOME/.Trash/GrowlMail.mailbundle\""
-	$MV "$DEST" "$HOME/.Trash/GrowlMail.mailbundle" || exit 1
-fi
-
-echo "--> Creating Bundles folder"
-echo "mkdir -p \"$BUNDLES\""
-mkdir -p "$BUNDLES" || exit 1
-
-echo "--> Installing GrowlMail"
-echo "$CP \"$SRC\" \"$DEST\""
-$CP "$SRC" "$DEST" || exit 1
-
-if killall -s Mail >/dev/null 2>/dev/null; then
-	echo "--> Quitting Mail"
-	echo "osascript -l AppleScript -e 'quit application \"Mail\"'"
-	osascript -l AppleScript -e 'quit application "Mail"'
-	MAIL_RUNNING=-YES-
-fi
-
-echo "--> Enabling plug-ins in Mail (if they are already enabled, this will have no effect)"
-echo "defaults write com.apple.mail EnableBundles -bool YES"
-defaults write com.apple.mail EnableBundles -bool YES
-echo "defaults write com.apple.mail BundleCompatibilityVersion -int 1"
-defaults write com.apple.mail BundleCompatibilityVersion -int 1
-
-if [[ ${MAIL_RUNNING:--NO-} == -YES- ]]; then
-	echo "--> Relaunching Mail"
-	echo "open -a Mail"
-	open -a Mail
-fi
+case $1 in
+	clean)		cleanGrowlMail		;;
+	build)		buildGrowlMail		;;
+	install)	buildGrowlMail		&&
+				uninstallGrowlMail	&&
+				stopMail			&&
+				installGrowlMail	&&
+				startMail			;;
+	uninstall)	uninstallGrowlMail	&&
+				stopMail			&&
+				startMail			;;
+	*)			usage				;;
+esac
 
 exit 0
