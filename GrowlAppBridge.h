4270a8ebe1cb42605c7e97e26de47262c541dde7
Peter Hosey <boredzo@users.noreply.github.com>
2005-01-18 23:15:40 +0000

    Replacements: s/app/application/i; s/Dict/Dictionary/; Added and reformatted some comments, too.

--- GrowlAppBridge.h
+++ GrowlAppBridge.h
@@ -1,5 +1,5 @@
 //
-//  GrowlAppBridge.h
+//  GrowlApplicationBridge.h
 //  Growl
 //
 //  Created by Evan Schoenberg on Wed Jun 16 2004.
@@ -17,7 +17,7 @@
 #import "GrowlDefines.h"
 
 //Forward declarations
-@protocol GrowlAppBridgeDelegate;
+@protocol GrowlApplicationBridgeDelegate;
 
 /*!
     @defined    GROWL_PREFPANE_BUNDLE_IDENTIFIER
@@ -34,20 +34,23 @@
 //Internal notification when the user chooses not to install (to avoid continuing to cache notifications awaiting installation)
 #define GROWL_USER_CHOSE_NOT_TO_INSTALL_NOTIFICATION @"User chose not to install"
 
+//------------------------------------------------------------------------------
+#pragma mark -
+
 /*!
-	@class      GrowlAppBridge
+	@class      GrowlApplicationBridge
 	@abstract   A class used to interface with Growl
 	@discussion This class provides a means to interface with Growl.
 	
 	Currently it provides a way to detect if Growl is installed and launch the GrowlHelperApp
 	if it's not already running.
  */
-@interface GrowlAppBridge : NSObject {
+@interface GrowlApplicationBridge : NSObject {
 
 }
 
 /*!
-@method isGrowlInstalled
+	@method isGrowlInstalled
 	@abstract Detects whether Growl is installed
 	@discussion Determies if the Growl prefpane and its helper app are installed
 	@result Returns YES if Growl is installed, NO otherwise
@@ -62,13 +65,15 @@
 */
 + (BOOL) isGrowlRunning;
 
-/* ***********************
-* This must be called before using GrowlAppBridge.  The methods in the GrowlAppBridgeDelegate are required;
-* other methods defined in the informal protocol are optional.
-* ***********************/
+/************************
+ *setGrowlDelegate: must be called before otherwise using GrowlApplicationBridge.
+ *The methods in the GrowlApplicationBridgeDelegate protocol are required;
+ * 	other methods defined in the informal protocol are optional.
+ ************************
+ */
 //XXX - Needs documentation
-+ (void) setGrowlDelegate:(NSObject<GrowlAppBridgeDelegate> *)inDelegate;
-+ (NSObject<GrowlAppBridgeDelegate> *) growlDelegate;
++ (void) setGrowlDelegate:(NSObject<GrowlApplicationBridgeDelegate> *)inDelegate;
++ (NSObject<GrowlApplicationBridgeDelegate> *) growlDelegate;
 
 //XXX - Needs documentation
 + (void) notifyWithTitle:(NSString *)title
@@ -80,26 +85,39 @@
 			clickContext:(id)clickContext;
 @end
 
-@interface GrowlAppBridge (GrowlInstallationPrompt_private)
+@interface GrowlApplicationBridge (GrowlInstallationPrompt_private)
+
 + (void) _userChoseNotToInstallGrowl;
+
 @end
 
-//XXX - Needs documentation
-@protocol GrowlAppBridgeDelegate
-//XXX - Needs documentation
-- (NSString *) growlAppName;
+//------------------------------------------------------------------------------
+#pragma mark -
 
 //XXX - Needs documentation
-- (NSDictionary *) growlRegistrationDict;
+@protocol GrowlApplicationBridgeDelegate
+
+//XXX - Needs documentation
+- (NSString *) applicationName;
+
+//XXX - Needs documentation
+- (NSDictionary *) growlRegistrationDictionary;
+
 @end
 
-//XXX - Needs documentation
-@interface NSObject (GrowlAppBridgeDelegate_InformalProtocol)
-/* The delegate may optionally return an NSData* object to use as the application icon;
-* if this is not implemented, the application's own icon is used */
-//XXX - Needs documentation
-- (NSData *) growlAppIconData;
+//------------------------------------------------------------------------------
+#pragma mark -
 
+//XXX - Needs documentation
+@interface NSObject (GrowlApplicationBridgeDelegate_InformalProtocol)
+
+/*The delegate may optionally return an NSData object to use as the application
+ *	icon; if this is not implemented, the application's own icon is used.
+ */
+//XXX - Needs documentation
+- (NSData *) applicationIconData;
+
+//called when GrowlHelperApp launches.
 //XXX - Needs documentation
 - (void) growlIsReady;
 
@@ -110,7 +128,8 @@
 @end
 
 //XXX - Needs documentation
-@interface NSObject (GrowlAppBridgeDelegate_Installation_InformalProtocol)
+@interface NSObject (GrowlApplicationBridgeDelegate_Installation_InformalProtocol)
+
 //XXX - Needs documentation
 - (NSString *)growlInstallationWindowTitle;
 
@@ -122,4 +141,5 @@
 
 //XXX - Needs documentation
 - (NSAttributedString *)growlUpdateInformation;
+
 @end
