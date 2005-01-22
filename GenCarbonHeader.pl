285806fa6fcfb394fa43a9ba52dbf858be99eda5
Peter Hosey <boredzo@users.noreply.github.com>
2005-01-22 05:40:07 +0000

    Tweaked Carbon-header generator to know about #include guards and translate those too.

--- GenCarbonHeader.pl
+++ GenCarbonHeader.pl
@@ -6,11 +6,18 @@ use POSIX;
 #Translate the Cocoa GrowlDefines.h file into one suitable for
 #use in Carbon apps, by
 #(1) Stripping the protocol information from the file, and
-#(2) Converting all @"String" forms into CFSTR("String")
+#(2) Changing imports to includes
+#(3) Replacing filenames in #imports/#includes and #include guards
+#(4) Converting all @"String" forms into CFSTR("String")
+#	(not in that order)
 
 my @infiles = ("GrowlDefines.h", "GrowlDefinesInternal.h");
 my %outfiles = ("GrowlDefinesInternal.h" => "GrowlDefinesInternalCarbon.h",
 		"GrowlDefines.h" => "GrowlDefinesCarbon.h");
+#include guards (needed for Carbon apps, since they don't generally use #import)
+my @infileguards = ("_GROWL_GROWLDEFINES_H", "_GROWL_GROWLDEFINESINTERNAL_H");
+my %outfileguards = ("_GROWL_GROWLDEFINESINTERNAL_H" => "_GROWL_GROWLDEFINESINTERNALCARBON_H",
+		"_GROWLDEFINES_H" => "_GROWLDEFINESCARBON_H");
 
 foreach my $infile (@infiles) {
 	my $outfile = $outfiles{$infile};
@@ -35,7 +42,11 @@ ENDOFHEADER
 	while(defined($line = <COCOA>) && ($line !~ m{/\* ---})) {
 		$line =~ s/@"([^"\r\n]*)"/CFSTR("$1")/g;
 		foreach my $header (@infiles) {
-			$line =~ s/#import "$header"/#import "$outfiles{$header}"/g;
+			$line =~ s/#(\s*)import <Growl\/$header>/#$1include <Growl\/$outfiles{$header}>/g;
+		}
+		$line =~ s/#(\s*)import <(.+)>/#$1include <$2>/g;
+		foreach my $guard (@infileguards) {
+			$line =~ s/$guard/$outfileguards{$guard}/;
 		}
 		print CARBON $line;
 	}
