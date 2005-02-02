df3788c330f73572e022a16f5d1d96fbf76e8c6e
Peter Hosey <boredzo@users.noreply.github.com>
2005-02-02 11:23:32 +0000

    Columnarisation and other whitespace additions. Added warning that 8-bit indices may not always be enough, citing the Zero-One-Infinity rule. (See the Jargon File.) Changed length from an unsigned to a size_t. Make sure default indices will fit in 8 bits. Skip those that don't, logging a message for each. Added support for indices (as NSNumbers) in the original defaults array. We no longer try to find these in the all array.

--- Source/GrowlUDPUtils.m
+++ Source/GrowlUDPUtils.m
@@ -1,4 +1,4 @@
-//
+﻿//
 //  GrowlUDPUtils.m
 //  Growl
 //
@@ -16,31 +16,33 @@
 	MD5_CTX ctx;
 	struct GrowlNetworkNotification *nn;
 	unsigned char *data;
-	unsigned length;
+	size_t length;
 	unsigned short notificationNameLen, titleLen, descriptionLen, applicationNameLen;
 
 	const char *notificationName = [[aNotification objectForKey:GROWL_NOTIFICATION_NAME] UTF8String];
-	const char *applicationName = [[aNotification objectForKey:GROWL_APP_NAME] UTF8String];
-	const char *title = [[aNotification objectForKey:GROWL_NOTIFICATION_TITLE] UTF8String];
-	const char *description = [[aNotification objectForKey:GROWL_NOTIFICATION_DESCRIPTION] UTF8String];
+	const char *applicationName  = [[aNotification objectForKey:GROWL_APP_NAME] UTF8String];
+	const char *title            = [[aNotification objectForKey:GROWL_NOTIFICATION_TITLE] UTF8String];
+	const char *description      = [[aNotification objectForKey:GROWL_NOTIFICATION_DESCRIPTION] UTF8String];
+	notificationNameLen = strlen( notificationName );
+	applicationNameLen  = strlen( applicationName );
+	titleLen            = strlen( title );
+	descriptionLen      = strlen( description );
+
 	NSNumber *priority = [aNotification objectForKey:GROWL_NOTIFICATION_PRIORITY];
 	NSNumber *isSticky = [aNotification objectForKey:GROWL_NOTIFICATION_STICKY];
-	notificationNameLen = strlen( notificationName );
-	applicationNameLen = strlen( applicationName );
-	titleLen = strlen( title );
-	descriptionLen = strlen( description );
+
 	length = sizeof(*nn) + notificationNameLen + applicationNameLen + titleLen + descriptionLen + MD5_DIGEST_LENGTH;
 
 	nn = (struct GrowlNetworkNotification *)malloc( length );
 	nn->common.version = GROWL_PROTOCOL_VERSION;
-	nn->common.type = GROWL_TYPE_NOTIFICATION;
+	nn->common.type    = GROWL_TYPE_NOTIFICATION;
 	nn->flags.reserved = 0;
 	nn->flags.priority = [priority intValue];
-	nn->flags.sticky = [isSticky boolValue];
-	nn->nameLen = htons( notificationNameLen );
-	nn->titleLen = htons( titleLen );
+	nn->flags.sticky   = [isSticky boolValue];
+	nn->nameLen        = htons( notificationNameLen );
+	nn->titleLen       = htons( titleLen );
 	nn->descriptionLen = htons( descriptionLen );
-	nn->appNameLen = htons( applicationNameLen );
+	nn->appNameLen     = htons( applicationNameLen );
 	data = nn->data;
 	memcpy( data, notificationName, notificationNameLen );
 	data += notificationNameLen;
@@ -64,49 +66,67 @@
 	return (unsigned char *)nn;
 }
 
+#pragma warning we need a way to handle the unlikely but fully-possible case wherein the dictionary contains more All notifications than the 8-bit Default indices can hold (Zero-One-Infinity) - first stage would be to try moving all the default notifications to the lower indices of the All array, second stage would be to create multiple packets
+
 + (unsigned char *) registrationToPacket:(NSDictionary *)aNotification password:(const char *)password packetSize:(unsigned int *)packetSize {
 	struct GrowlNetworkRegistration *nr;
 	unsigned char *data;
 	const char *notification;
-	unsigned i, length, size, notificationIndex;
+	unsigned i, size, notificationIndex;
+	size_t length;
 	unsigned short applicationNameLen;
 	unsigned numAllNotifications, numDefaultNotifications;
 	MD5_CTX ctx;
+	Class NSNumberClass = [NSNumber class];
 
-	const char *applicationName = [[aNotification objectForKey:GROWL_APP_NAME] UTF8String];
-	NSArray *allNotifications = [aNotification objectForKey:GROWL_NOTIFICATIONS_ALL];
+	const char *applicationName   = [[aNotification objectForKey:GROWL_APP_NAME] UTF8String];
+	NSArray *allNotifications     = [aNotification objectForKey:GROWL_NOTIFICATIONS_ALL];
 	NSArray *defaultNotifications = [aNotification objectForKey:GROWL_NOTIFICATIONS_DEFAULT];
-	applicationNameLen = strlen( applicationName );
-	numAllNotifications = [allNotifications count];
-	numDefaultNotifications = [defaultNotifications count];
+	applicationNameLen            = strlen( applicationName );
+	numAllNotifications           = [allNotifications count];
+	numDefaultNotifications       = [defaultNotifications count];
 
 	// compute packet size
 	length = sizeof(*nr) + applicationNameLen + MD5_DIGEST_LENGTH;
-	for ( i=0; i<numAllNotifications; ++i ) {
-		notification = [[allNotifications objectAtIndex:i] UTF8String];
-		length += sizeof(unsigned short) + strlen( notification );
+	for(i = 0; i < numAllNotifications; ++i) {
+		notification  = [[allNotifications objectAtIndex:i] UTF8String];
+		length       += sizeof(unsigned short) + strlen( notification );
 	}
 	size = numDefaultNotifications;
-	for ( i=0; i<numDefaultNotifications; ++i ) {
-		notificationIndex = [allNotifications indexOfObject:[defaultNotifications objectAtIndex:i]];
-		if( notificationIndex == NSNotFound ) {
-			--size;
-			NSLog( @"Warning: defaultNotifications is not a subset of allNotifications" );
+	for(i = 0; i < numDefaultNotifications; ++i) {
+		NSNumber *num = [defaultNotifications objectAtIndex:i];
+		if([num isKindOfClass:NSNumberClass]) {
+			notificationIndex = [num unsignedIntValue];
+			if(notificationIndex >= numAllNotifications) {
+				NSLog(@"Warning: index %u found in defaultNotifications is not within the range (%u) of the notifications array", notificationIndex, numAllNotifications);
+				--size;
+			} else if(notificationIndex > UCHAR_MAX) {
+				NSLog(@"Warning: index %u found in defaultNotifications is not within the range (%u) of an 8-bit unsigned number", notificationIndex);
+				--size;
+			} else {
+				++length;
+			}
 		} else {
-			++length;
+			notificationIndex = [allNotifications indexOfObject:obj];
+			if( notificationIndex == NSNotFound ) {
+				NSLog(@"Warning: defaultNotifications is not a subset of allNotifications (object found in defaultNotifications that is not in allNotifications; description of object is %@)", obj);
+				--size;
+			} else {
+				++length;
+			}
 		}
 	}
 
 	nr = (struct GrowlNetworkRegistration *)malloc( length );
-	nr->common.version = GROWL_PROTOCOL_VERSION;
-	nr->common.type = GROWL_TYPE_REGISTRATION;
-	nr->appNameLen = htons( applicationNameLen );
-	nr->numAllNotifications = (unsigned char)numAllNotifications;
+	nr->common.version          = GROWL_PROTOCOL_VERSION;
+	nr->common.type             = GROWL_TYPE_REGISTRATION;
+	nr->appNameLen              = htons( applicationNameLen );
+	nr->numAllNotifications     = (unsigned char)numAllNotifications;
 	nr->numDefaultNotifications = (unsigned char)size;
 	data = nr->data;
 	memcpy( data, applicationName, applicationNameLen );
 	data += applicationNameLen;
-	for ( i=0; i<numAllNotifications; ++i ) {
+	for(i = 0; i < numAllNotifications; ++i) {
 		notification = [[allNotifications objectAtIndex:i] UTF8String];
 		size = strlen( notification );
 		*(unsigned short *)data = htons( size );
@@ -114,10 +134,23 @@
 		memcpy( data, notification, size );
 		data += size;
 	}
-	for ( i=0; i<numDefaultNotifications; ++i ) {
-		notificationIndex = [allNotifications indexOfObject:[defaultNotifications objectAtIndex:i]];
-		if( notificationIndex != NSNotFound ) {
-			*data++ = notificationIndex;
+	for(i = 0; i < numDefaultNotifications; ++i) {
+		NSNumber *num = [defaultNotifications objectAtIndex:i];
+		if([num isKindOfClass:NSNumberClass]) {
+			notificationIndex = [num unsignedIntValue];
+			if((notificationIndex <  numAllNotifications)
+			&& (notificationIndex <= UCHAR_MAX))
+			{
+				*data++ = notificationIndex;
+			}
+		} else {
+			notificationIndex = [allNotifications indexOfObject:num];
+			if((notificationIndex <  numAllNotifications)
+			&& (notificationIndex <= UCHAR_MAX)
+			&& (notificationIndex != NSNotFound))
+			{
+				*data++ = notificationIndex;
+			}
 		}
 	}
 
