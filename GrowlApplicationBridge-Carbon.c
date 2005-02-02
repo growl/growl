b4308128daf169e4c1c48a721ea6af36a73bdccc
Peter Hosey <boredzo@users.noreply.github.com>
2005-02-02 19:45:14 +0000

    Removed warning about the use of an uninitialised variable.

--- GrowlApplicationBridge-Carbon.c
+++ GrowlApplicationBridge-Carbon.c
@@ -563,8 +563,7 @@ static CFStringRef _copyTemporaryFolderPath(void) {
 	CFStringRef string;
 	OSStatus err = FSFindFolder(kOnAppropriateDisk, kTemporaryFolderType, kCreateFolder, &ref);
 	if(err != noErr) {
-#warning string is used uninitialized here, we should set it to a default value or not include it in the log message.
-		NSLog(CFSTR("GrowlApplicationBridge: Could not locate temporary folder because FSFindFolder returned %li - will try using %@"), (long)err, string);
+		NSLog(CFSTR("GrowlApplicationBridge: Could not locate temporary folder because FSFindFolder returned %li"), (long)err);
 		string = NULL;
 	} else {
 		CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &ref);
