06427f7f14e02a4568e3b9e78e807786f64f29a0
toby <toby@localhost>
2005-03-03 05:00:22 +0000

    Update/add copyright headers.

--- Bindings/tcl/growl.m
+++ Bindings/tcl/growl.m
@@ -1,7 +1,7 @@
 /*
  * growl.m
  *
- * Copyright (c) 2004, Toby Peterson <toby@opendarwin.org>
+ * Copyright (c) 2004-2005, Toby Peterson <toby@opendarwin.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
