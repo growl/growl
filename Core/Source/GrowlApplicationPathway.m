a7ce12ff12d8ef9c7b5ba72dd461c11a5b166c90
Machina <Machina@localhost>
2005-03-10 15:08:36 +0000

    The files the framework will be talking to

---
+++ Core/Source/GrowlApplicationPathway.m
@@ -0,0 +1,14 @@
+//
+//  GrowlApplicationPathway.m
+//  Growl
+//
+//  Created by Karl Adam on 3/10/05.
+//  Copyright 2005 The Growl Project. All rights reserved.
+//
+
+#import "GrowlApplicationPathway.h"
+
+
+@implementation GrowlApplicationPathway
+
+@end
