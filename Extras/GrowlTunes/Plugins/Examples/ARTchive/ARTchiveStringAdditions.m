6fdaab3106117599eed1512fad042713965f270e
Peter Hosey <boredzo@users.noreply.github.com>
2005-03-18 09:31:16 +0000

    Corrected a variable name and restored a missing } in -[NSString(ARTchiveStringAdditions) stringByMakingPathSafe]. ARTchive now uses C99. This is needed for those C99 loops I added.

--- Extras/GrowlTunes/Plugins/Examples/ARTchive/ARTchiveStringAdditions.m
+++ Extras/GrowlTunes/Plugins/Examples/ARTchive/ARTchiveStringAdditions.m
@@ -11,7 +11,7 @@
 @implementation NSString (ARTchiveStringAdditions)
 
 - (NSString *)stringByMakingPathSafe {
-	size_t numChars = [fullAlbumName length];
+	size_t numChars = [self length];
 	unichar *chars = malloc(numChars * sizeof(unichar));
 	if(!chars) {
 		NSLog(@"In -[NSString(ARTchiveStringAdditions) stringByMakingPathSafe:]: Could not allocate %lu bytes of memory in which to examine the full album name", (unsigned long)(numChars * sizeof(unichar)));
@@ -24,6 +24,7 @@
 			case ':':
 			case '/':
 				chars[i] = '_';
+		}
 	}
 
 	NSString *result = [NSString stringWithCharacters:chars length:numChars];
