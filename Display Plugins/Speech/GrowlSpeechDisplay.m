b005b20652e6bc257c19fea453a63376e058cd26
Peter Hosey <boredzo@users.noreply.github.com>
2005-04-07 08:24:01 +0000

    Added screenshot mode - or not. It won't work yet because ld demands to have GrowlController linked into the plug-in, and that's not possible without also adding pretty much all of GHA.

--- Display Plugins/Speech/GrowlSpeechDisplay.m
+++ Display Plugins/Speech/GrowlSpeechDisplay.m
@@ -10,6 +10,10 @@
 #import "GrowlSpeechPrefs.h"
 #import "GrowlSpeechDefines.h"
 #import <GrowlDefinesInternal.h>
+#ifdef CAN_HAVE_SCREENSHOT_MODE
+//see below.
+#import "GrowlController.h"
+#endif
 
 @implementation GrowlSpeechDisplay
 - (void) dealloc {
@@ -29,8 +33,27 @@
 
 	READ_GROWL_PREF_VALUE(GrowlSpeechVoicePref, GrowlSpeechPrefDomain, NSString *, &voice);
 
+	NSString *desc = [noteDict objectForKey:GROWL_NOTIFICATION_DESCRIPTION];
+
 	NSSpeechSynthesizer *syn = [[NSSpeechSynthesizer alloc] initWithVoice:voice];
-	[syn startSpeakingString:[noteDict objectForKey:GROWL_NOTIFICATION_DESCRIPTION]];
+	[syn startSpeakingString:desc];
+
+#ifdef CAN_HAVE_SCREENSHOT_MODE
+	/*it is not currently possible to use GrowlController here without linking
+	 *	it into the plug-in, and it is not possible to link it into the plug-in
+	 *	without also adding entirely too much of GHA (in fact, pretty much all
+	 *	of it.)
+	 *this section can be enabled upon the completion of #114, or possibly #116.
+	 */
+
+	if([[noteDict objectForKey:GROWL_SCREENSHOT_MODE] boolValue]) {
+		GrowlController *growlController = [GrowlController standardController];
+		NSString *path = [[[growlController screenshotsDirectory] stringByAppendingPathComponent:[growlController nextScreenshotName]] stringByAppendingPathExtension:@"aiff"];
+		NSURL *URL = [NSURL fileURLWithPath:path];
+		[syn startSpeakingString:desc toURL:URL];
+	}
+#endif
+
 	[syn autorelease];
 }
 @end
