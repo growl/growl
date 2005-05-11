69c23e0af3c7f2d2ff24a2d5d2723d37830b1051
Peter Hosey <boredzo@users.noreply.github.com>
2005-05-11 16:46:15 +0000

    Added functions:  * {{{copyTemporaryFolderURL}}}  * {{{createURLByMakingDirectoryAtURLWithName}}} (which replicates {{{mkdir}}}(3) + {{{getcwd}}}(3))  * {{{copyFork}}} (this one is static)  * {{{createURLByCopyingFileFromURLToDirectoryURL}}}

--- Common/Source/CFGrowlAdditions.c
+++ Common/Source/CFGrowlAdditions.c
@@ -8,6 +8,8 @@
 
 #include <Carbon/Carbon.h>
 #include "CFGrowlAdditions.h"
+#include <c.h>
+#include <unistd.h>
 
 static CFStringRef _CFURLAliasDataKey  = CFSTR("_CFURLAliasData");
 static CFStringRef _CFURLStringKey     = CFSTR("_CFURLString");
@@ -46,19 +48,28 @@ CFStringRef copyCurrentProcessPath(void) {
 	return path;
 }
 
-CFStringRef copyTemporaryFolderPath(void) {
+CFURLRef copyTemporaryFolderURL(void) {
 	FSRef ref;
-	CFStringRef string;
+	CFURLRef url = NULL;
+
 	OSStatus err = FSFindFolder(kOnAppropriateDisk, kTemporaryFolderType, kCreateFolder, &ref);
-	if (err != noErr) {
+	if (err != noErr)
 		NSLog(CFSTR("in copyTemporaryFolderPath in CFGrowlAdditions: Could not locate temporary folder because FSFindFolder returned %li"), (long)err);
-		string = NULL;
-	} else {
-		CFURLRef url = CFURLCreateFromFSRef(kCFAllocatorDefault, &ref);
-		string = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
+	else
+		url = CFURLCreateFromFSRef(kCFAllocatorDefault, &ref);
+
+	return url;
+}
+CFStringRef copyTemporaryFolderPath(void) {
+	CFStringRef path = NULL;
+
+	CFURLRef url = copyTemporaryFolderURL();
+	if(url) {
+		path = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
 		CFRelease(url);
 	}
-	return string;
+
+	return path;
 }
 
 CFDictionaryRef createDockDescriptionForURL(CFURLRef url) {
@@ -172,4 +183,214 @@ CFDataRef copyIconDataForURL(CFURLRef URL) {
 	}
 
 	return data;
+}
+
+URL_TYPE createURLByMakingDirectoryAtURLWithName(URL_TYPE parent, STRING_TYPE name) {
+	CFURLRef newDirectory = NULL;
+
+	CFAllocatorRef allocator = parent ? CFGetAllocator(parent) : name ? CFGetAllocator(name) : kCFAllocatorDefault;
+
+	if(parent) parent = CFRetain(parent);
+	else {
+		char *cwdBytes = alloca(PATH_MAX);
+		getcwd(cwdBytes, PATH_MAX);
+		parent = CFURLCreateFromFileSystemRepresentation(allocator, (const unsigned char *)cwdBytes, strlen(cwdBytes), /*isDirectory*/ true);
+		if(!name) {
+			newDirectory = parent;
+			goto end;
+		}
+	}
+	if(parent) {
+		if(name)
+			name = CFRetain(name);
+		else {
+			name = CFURLCopyLastPathComponent(parent);
+			CFURLRef newParent = CFURLCreateCopyDeletingLastPathComponent(allocator, parent);
+			CFRelease(parent);
+			parent = newParent;
+		}
+
+		if(name) {
+			FSRef parentRef;
+			if(CFURLGetFSRef(parent, &parentRef)) {
+				FSRef newDirectoryRef;
+
+				struct HFSUniStr255 nameUnicode;
+				CFRange range = { 0, MIN(CFStringGetLength(name), USHRT_MAX) };
+				CFStringGetCharacters(name, range, nameUnicode.unicode);
+				nameUnicode.length = range.length;
+
+				struct FSRefParam refPB = {
+					.ref              = &parentRef,
+					.nameLength       = nameUnicode.length,
+					.name             = nameUnicode.unicode,
+					.whichInfo        = kFSCatInfoNone,
+					.catInfo          = NULL,
+					.textEncodingHint = kTextEncodingUnknown,
+					.newRef           = &newDirectoryRef,
+				};
+				
+				OSStatus err = PBCreateDirectoryUnicodeSync(&refPB);
+				if(err == dupFNErr) {
+					//dupFNErr == file (or folder) exists already. this is fine.
+					err = PBMakeFSRefUnicodeSync(&refPB);
+				}
+				if(err == noErr) {
+					NSLog(CFSTR("PBCreateDirectoryUnicodeSync or PBMakeFSRefUnicodeSync returned %li; calling CFURLCreateFromFSRef"), (long)err);
+					newDirectory = CFURLCreateFromFSRef(allocator, &newDirectoryRef);
+					NSLog(CFSTR("CFURLCreateFromFSRef returned %@"), newDirectory);
+				} else
+					NSLog(CFSTR("could not create directory '%@' in parent directory at %@: FSCreateDirectoryUnicode returned %li"), name, parent, (long)err);
+			}
+
+			CFRelease(parent);
+		} //if(name)
+		CFRelease(name);
+	} //if(parent)
+
+end:
+	return newDirectory;
+}
+
+#ifndef COPYFORK_BUFSIZE
+#	define COPYFORK_BUFSIZE 5242880U /*5 MiB*/
+#endif
+
+static OSStatus copyFork(const struct HFSUniStr255 *forkName, FSRef *srcFile, FSRef *destDir, const struct HFSUniStr255 *destName, FSRef *outDestFile) {
+	OSStatus err, closeErr;
+	struct FSForkIOParam srcPB = {
+		.ref = srcFile,
+		.forkNameLength = forkName->length,
+		.forkName = forkName->unicode,
+		.permissions = fsRdPerm,
+	};
+
+	err = PBOpenForkSync(&srcPB);
+	NSLog(CFSTR("PBOpenForkSync (source) returned %li"), (long)err);
+	if(err == noErr) {
+		FSRef destFile;
+
+		/*the first thing to do is get the name of the destination file, if one
+		 *	wasn't provided.
+		 *and while we're at it, we get the catalogue info as well.
+		 */
+		struct FSCatalogInfo catInfo;
+		struct FSRefParam refPB = {
+			.ref       = srcFile,
+			.whichInfo = kFSCatInfoGettableInfo & kFSCatInfoSettableInfo,
+			.catInfo   = &catInfo,
+			.spec      = NULL,
+			.parentRef = NULL,
+			.outName   = destName ? NULL : (struct HFSUniStr255 *)(destName = alloca(sizeof(struct HFSUniStr255))),
+		};
+
+		err = PBGetCatalogInfoSync(&refPB);
+		NSLog(CFSTR("PBGetCatalogInfoSync returned %li"), (long)err);
+		if(err == noErr) {
+			refPB.ref              = destDir;
+			refPB.nameLength       = destName->length;
+			refPB.name             = destName->unicode;
+			refPB.textEncodingHint = kTextEncodingUnknown;
+			refPB.newRef           = &destFile;
+
+			err = PBMakeFSRefUnicodeSync(&refPB);
+			NSLog(CFSTR("PBMakeFSRefUnicodeSync returned %li"), (long)err);
+			if(err == fnfErr) {
+				//that file doesn't exist in that folder; create it.
+				err = PBCreateFileUnicodeSync(&refPB);
+				NSLog(CFSTR("PBCreateFileUnicodeSync returned %li"), (long)err);
+				if(err == noErr) {
+					/*make sure the Finder knows about the new file.
+					 *FNNotify returns a status code too, but this isn't an
+					 *	essential step, so we just ignore it.
+					 */
+					FNNotify(destDir, kFNDirectoryModifiedMessage, kNilOptions);
+				}
+			}
+		}
+		if(err == noErr) {
+			if(outDestFile)
+				memcpy(outDestFile, &destFile, sizeof(destFile));
+
+			struct FSForkIOParam destPB = {
+				.ref            = &destFile,
+				.forkNameLength = forkName->length,
+				.forkName       = forkName->unicode,
+				.permissions    = fsWrPerm,
+			};
+			err = PBOpenForkSync(&destPB);
+			NSLog(CFSTR("PBOpenForkSync (dest) returned %li"), (long)err);
+			if(err == noErr) {
+				void *buf = malloc(COPYFORK_BUFSIZE);
+				if(buf) {
+					srcPB.buffer = destPB.buffer = buf;
+					srcPB.requestCount = COPYFORK_BUFSIZE;
+					while(err == noErr) {
+						err = PBReadForkSync(&srcPB);
+						NSLog(CFSTR("PBReadForkSync returned %li"), (long)err);
+						NSLog(CFSTR("request count: %llu; actual count: %llu"), (unsigned long long)srcPB.requestCount, (unsigned long long)srcPB.actualCount);
+						if(err == eofErr) {
+							err = noErr;
+							if(srcPB.actualCount == 0)
+								break;
+						}
+						if(err == noErr) {
+							destPB.requestCount = srcPB.actualCount;
+							err = PBWriteForkSync(&destPB);
+							NSLog(CFSTR("PBWriteForkSync returned %li"), (long)err);
+							NSLog(CFSTR("request count: %llu; actual count: %llu"), (unsigned long long)destPB.requestCount, (unsigned long long)destPB.actualCount);
+						}
+					}
+
+					free(buf);
+				}
+
+				closeErr = PBCloseForkSync(&destPB);
+				NSLog(CFSTR("PBCloseForkSync (dest) returned %li"), (long)closeErr);
+				if(err == noErr) err = closeErr;
+			}
+		}
+
+		closeErr = PBCloseForkSync(&srcPB);
+		NSLog(CFSTR("PBCloseForkSync (source) returned %li"), (long)closeErr);
+		if(err == noErr) err = closeErr;
+	}
+
+	return err;
+}
+
+CFURLRef createURLByCopyingFileFromURLToDirectoryURL(CFURLRef file, CFURLRef dest) {
+	CFURLRef destFileURL = NULL;
+
+	FSRef fileRef, destRef, destFileRef;
+	NSLog(CFSTR("getting FSRef for file %@: %hhu; getting FSRef for dest %@: %hhu"), file, CFURLGetFSRef(file, &fileRef), dest, CFURLGetFSRef(dest, &destRef));
+	if(CFURLGetFSRef(file, &fileRef) && CFURLGetFSRef(dest, &destRef)) {
+		OSStatus err;
+
+		struct HFSUniStr255 forkName;
+		struct FSForkIOParam forkPB = {
+			.ref = &fileRef,
+			.forkIterator = {
+				.initialize = 0L
+			},
+			.outForkName = &forkName,
+		};
+
+		do {
+			err = PBIterateForksSync(&forkPB);
+			NSLog(CFSTR("PBIterateForksSync returned %li"), (long)err);
+			if(err == noErr) {
+				err = copyFork(&forkName, &fileRef, &destRef, /*destName*/ NULL, /*outDestFile*/ &destFileRef);
+				CFStringRef CFForkName = CFStringCreateWithCharacters(kCFAllocatorDefault, forkName.unicode, forkName.length);
+				NSLog(CFSTR("for fork with name '%@', copyFork returned %li"), CFForkName, (long)err);
+				CFRelease(CFForkName);
+			}
+		} while(err == noErr);
+		if(err == errFSNoMoreItems) err = noErr;
+
+		if(err == noErr)
+			destFileURL = CFURLCreateFromFSRef(kCFAllocatorDefault, &destFileRef);
+	}
+
+	return destFileURL;
 }
--- Common/Source/CFGrowlAdditions.h
+++ Common/Source/CFGrowlAdditions.h
@@ -24,6 +24,7 @@ STRING_TYPE copyCurrentProcessName(void);
 URL_TYPE    copyCurrentProcessURL(void);
 STRING_TYPE copyCurrentProcessPath(void);
 
+URL_TYPE    copyTemporaryFolderURL(void);
 STRING_TYPE copyTemporaryFolderPath(void);
 
 DICTIONARY_TYPE createDockDescriptionForURL(URL_TYPE url);
@@ -38,3 +39,25 @@ DATA_TYPE copyIconDataForPath(STRING_TYPE path);
  *	@result	The icon data, in IconFamily format (same as used in the 'icns' resource and in .icns files).
  */
 DATA_TYPE copyIconDataForURL(URL_TYPE URL);
+
+/*	@function	createURLByMakingDirectoryAtURLWithName
+ *	@abstract	Create a directory.
+ *	@discussion	This function has a useful side effect: if you pass
+ *	 <code>NULL</code> for both parameters, this function will act basically as
+ *	 CFURL version of <code>getcwd</code>(3).
+ *
+ *	 Also, for CF clients: the allocator used to create the returned URL will
+ *	 be the allocator for the parent URL, the allocator for the name string, or
+ *	 the default allocator, in that order of preference.
+ *	@param	parent	The directory in which to create the new directory. If this is <code>NULL</code>, the current working directory (as returned by <code>getcwd</code>(3)) will be used.
+ *	@param	name	The name of the directory you want to create. If this is <code>NULL</code>, the directory specified by the URL will be created.
+ *	@result	The URL for the directory if it was successfully created; else, <code>NULL</code>.
+ */
+URL_TYPE createURLByMakingDirectoryAtURLWithName(URL_TYPE parent, STRING_TYPE name);
+
+/*	@function	createURLByCopyingFileFromURLToDirectoryURL
+ *	@param	file	The file to copy.
+ *	@param	dest	The folder to copy it to.
+ *	@result	The copy.
+ */
+URL_TYPE createURLByCopyingFileFromURLToDirectoryURL(URL_TYPE file, URL_TYPE dest);
