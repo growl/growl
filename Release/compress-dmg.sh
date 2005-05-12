592bb36c6102736e1687995661690b18a7ffd4ff
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-05-12 20:14:13 +0000

    Typo

--- Release/compress-dmg.sh
+++ Release/compress-dmg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b2-compressed.dmg Growl-0.7b22.dmg
+hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b2-compressed.dmg Growl-0.7b2.dmg
 hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b2-SDK-compressed.dmg Growl-0.7b2-SDK.dmg
