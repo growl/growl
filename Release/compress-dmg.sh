28e230b2e1ea5d73d49c5b380f8606164f0401d4
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-05-13 05:51:11 +0000

    Made compress-dmg.sh overwrite source dmgs

--- Release/compress-dmg.sh
+++ Release/compress-dmg.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b3-compressed.dmg Growl-0.7b3.dmg
 hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b3-SDK-compressed.dmg Growl-0.7b3-SDK.dmg
+mv Growl-0.7b3-compressed.dmg Growl-0.7b3.dmg
+mv Growl-0.7b3-SDK-compressed.dmg Growl-0.7b3-SDK.dmg
