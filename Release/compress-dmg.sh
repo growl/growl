8edd785d50d678e8cee0174b7dd2bca4a399792f
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-05-14 12:32:55 +0000

    Bumped version number to 0.7.0b4

--- Release/compress-dmg.sh
+++ Release/compress-dmg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b3-compressed.dmg Growl-0.7b3.dmg
-hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b3-SDK-compressed.dmg Growl-0.7b3-SDK.dmg
-mv Growl-0.7b3-compressed.dmg Growl-0.7b3.dmg
-mv Growl-0.7b3-SDK-compressed.dmg Growl-0.7b3-SDK.dmg
+hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b4-compressed.dmg Growl-0.7b4.dmg
+hdiutil convert -format UDZO -imagekey zlib-level=9 -o Growl-0.7b4-SDK-compressed.dmg Growl-0.7b4-SDK.dmg
+mv Growl-0.7b4-compressed.dmg Growl-0.7b4.dmg
+mv Growl-0.7b4-SDK-compressed.dmg Growl-0.7b4-SDK.dmg
