b91d7702fe61441819eaed878d0d9e7b191b39a4
pudge <pudge@localhost>
2005-05-15 07:10:54 +0000

    Clean up script a bit, remove distributed messages (now that 0.7 can do it), and add README.

--- Scripts/X-Chat/growl.pl
+++ Scripts/X-Chat/growl.pl
@@ -1,9 +1,5 @@
 #!/usr/bin/perl
 
-# Just install Growl and Mac::Growl, put this script in the
-# X-Chat Aqua/Plugins/ folder, make sure the correct perl plugin is
-# also in there, and then start X-Chat (or reload the perl plugin)
-
 # http://www.xchat.org/docs/xchat2-perl.html
 package Xchat::Mac::Growl;
 
@@ -17,43 +13,6 @@ use Mac::Growl ':all';
 use File::Spec::Functions qw(catfile tmpdir);
 use File::Temp qw(tmpnam);
 
-# this is a bad idea ... just proof of concept
-my $image = 0;
-my $uri_finder;
-our $IMAGE;
-if ($image) {
-	eval 'require LWP::Simple; require URI::Find;';
-	$image = $@ ? 0 : 1;
-	$uri_finder = URI::Find->new(\&uri_finder) if $image;
-}
-
-# put hosts in here that you can talk to via remote Apple events,
-# and get simple distributed messages
-my @hosts = qw();
-
-my $version;
-if ($Mac::Growl::glue) {
-	$Mac::Growl::glue->TIMEOUT(5);
-	$version = $Mac::Growl::glue->prop('version');
-}
-
-sub _gethost {
-	return unless $Mac::Growl::glue;
-	my $found = 0;
-	for my $host (@hosts) {
-		$Mac::Growl::glue->ADDRESS(eppc => GrowlHelperApp => $host);
-		$found = 1, last if &_alive;
-	}
-	$Mac::Growl::glue->ADDRESS if !$found;
-}
-
-sub _alive {
-	return unless $Mac::Growl::glue;
-	return defined $version->get;
-}
-
-
-
 Xchat::register('growl', '1.0');
 Xchat::print("Loading Growl interface ...\n");
 
@@ -62,18 +21,11 @@ RegisterNotifications($appname, [$notification], [$notification], $appname);
 Xchat::hook_server('PRIVMSG', \&privmsg);
 Xchat::hook_print('Channel Msg Hilight', \&hilight);
 
-for my $host (undef, @hosts) {
-	if ($host) {
-		$Mac::Growl::glue->ADDRESS(eppc => GrowlHelperApp => $host);
-		next unless &_alive;
-	}
-
-	PostNotification(
-		$appname, $notification,
-		"$appname plugin loaded",
-		"Growl interface loaded for perl $] and $Mac::Growl::base."
-	);
-}
+PostNotification(
+	$appname, $notification,
+	"$appname plugin loaded",
+	"Growl interface loaded for perl $] and $Mac::Growl::base."
+);
 
 sub privmsg {
 	my($msgs, $words) = @_;
@@ -98,32 +50,7 @@ sub hilight {
 
 sub notify {
 	my($title, $description, $sticky, $priority) = @_;
-
-	_gethost();
-
-	my($filename);
-	if ($image && $description) {
-		if ($uri_finder->find(\$description)) {
-			$filename = tmpnam();
-			LWP::Simple::getstore($IMAGE, $filename);
-			undef $IMAGE;
-		}
-	}
-
-	PostNotification($appname, $notification, $title, $description, $sticky, $priority, $filename);
-}
-
-sub uri_finder {
-	my($uri, $orig_uri) = @_;
-
-	my @head = LWP::Simple::head($uri);
-	if ($head[0] =~ /^image\//) {
-		$IMAGE = $uri;
-		return '[image]';
-	} else {
-		# auto-open URLs?  :-)
-		return '[url]';
-	}
+	PostNotification($appname, $notification, $title, $description, $sticky, $priority);
 }
 
 1;
