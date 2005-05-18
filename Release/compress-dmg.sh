1639bc8568ca72e70df52c1418a46090255477b0
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-05-18 13:50:02 +0000

    - Added a shellscript to create the release diskimages - Removed the FileStorm projects which are no longer needed

--- Release/compress-dmg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ $# -ne 1  ]; then
-	echo "Usage: $0 <prefix>"
-else
-	hdiutil convert -format UDZO -imagekey zlib-level=9 -o $1-compressed.dmg $1.dmg
-	hdiutil convert -format UDZO -imagekey zlib-level=9 -o $1-SDK-compressed.dmg $1-SDK.dmg
-	mv $1-compressed.dmg $1.dmg
-	mv $1-SDK-compressed.dmg $1-SDK.dmg
-fi
