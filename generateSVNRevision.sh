ee57e82a8aa24bfa5a57ea9f8fd035e68657e13b
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-06-07 16:41:44 +0000

    Added xCoDe 2.1 projects. The paths to Growl.framework in the subprojects need some tweaking.

--- generateSVNRevision.sh
+++ generateSVNRevision.sh
@@ -4,6 +4,6 @@
 PATH="$PATH:/opt/local/bin:/usr/local/bin:/usr/local/subversion/bin:/sw/bin"
 REVISION=`svnversion .`
 echo "*** Building Growl Revision: $REVISION"
-mkdir -p $OBJROOT/include
+mkdir -p $TARGET_BUILD_DIR/include
 #SVN_REVISION is a string because it may look like "4168M" or "4123:4168MS"
 echo "#define SVN_REVISION \"$REVISION\"" > $SCRIPT_OUTPUT_FILE_0
