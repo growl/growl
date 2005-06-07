ae54aaa46f5d0359787128ac8b493b36fb697f5e
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-06-07 20:30:43 +0000

    Updated some paths

--- Prefpane TestHarness/generateBuildPath.sh
+++ Prefpane TestHarness/generateBuildPath.sh
@@ -1,3 +1,3 @@
 # Get the local path to the built Growl Prefpane - for loading in the testing app.
-mkdir -p $OBJROOT/include
-echo "#define GROWL_OBJROOT \"$OBJROOT\"" > $SCRIPT_OUTPUT_FILE_0
+mkdir -p $TARGET_BUILD_DIR/include
+echo "#define GROWL_OBJROOT \"$TARGET_BUILD_DIR\"" > $SCRIPT_OUTPUT_FILE_0
