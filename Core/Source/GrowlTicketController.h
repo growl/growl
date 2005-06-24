16b79ff4419150d1309d66e524b73f48bf707027
Peter Hosey <boredzo@users.noreply.github.com>
2005-06-24 05:20:28 +0000

    Adding {{{GrowlTicketController.h}}}.

---
+++ Core/Source/GrowlTicketController.h
@@ -0,0 +1,23 @@
+//
+//  GrowlTicketController.h
+//  Growl
+//
+//  Created by Mac-arena the Bored Zo on 2005-06-08.
+//  Copyright 2005 Mac-arena the Bored Zo. All rights reserved.
+//
+
+@class GrowlApplicationTicket;
+
+@interface GrowlTicketController: NSObject
+{
+	NSMutableDictionary *ticketsByApplicationName;
+}
+
++ (id) sharedController;
+
+- (NSDictionary *) allSavedTickets;
+
+- (GrowlApplicationTicket *) ticketForApplicationName:(NSString *) appName;
+- (void) addTicket:(GrowlApplicationTicket *) newTicket;
+
+@end
