84f8e7273d799a4c8ab46aa3339517f45e2b2738
Peter Hosey <boredzo@users.noreply.github.com>
2005-08-06 07:34:59 +0000

    Adding GrowlApplicationNotification, to be used by pathways and GHA.

---
+++ Core/Source/GrowlApplicationNotification.h
@@ -0,0 +1,81 @@
+//
+//	GrowlApplicationNotification.h
+//	Growl
+//
+//	Created by Mac-arena the Bored Zo on 2005-07-31.
+//	Copyright 2005 The Growl Project. All rights reserved.
+//
+
+@interface GrowlApplicationNotification: NSObject
+{
+	NSString *name, *applicationName;
+	NSString *title, *description;
+	NSString *HTMLTitle, *HTMLDescription;
+	NSAttributedString *attributedTitle, *attributedDescription;
+
+	NSDictionary *dictionary, *auxiliaryDictionary;
+
+	unsigned GANReserved: 30;
+	unsigned hasHTMLTitle: 1;
+	unsigned hasHTMLDescription: 1;
+}
+
++ (GrowlApplicationNotification *) notificationWithDictionary:(NSDictionary *)dict;
+
+- (GrowlApplicationNotification *) initWithDictionary:(NSDictionary *)dict;
+
+//you can pass nil for description.
+- (GrowlApplicationNotification *) initWithName:(NSString *)newName
+                                applicationName:(NSString *)newAppName
+                                          title:(NSString *)newTitle
+                                    description:(NSString *)newDesc;
+
+//you can pass nil for description, or for either or both of the HTML properties.
+- (GrowlApplicationNotification *) initWithName:(NSString *)newName
+                                applicationName:(NSString *)newAppName
+                                          title:(NSString *)newTitle
+                                      HTMLTitle:(NSString *)newHTMLTitle
+                                    description:(NSString *)newDesc
+                                HTMLDescription:(NSString *)newHTMLDesc;
+
+#pragma mark -
+
+/*as of 0.8, this returns:
+ *	*	GROWL_NOTIFICATION_NAME
+ *	*	GROWL_APP_NAME
+ *	*	GROWL_NOTIFICATION_TITLE
+ *	*	GROWL_NOTIFICATION_TITLE_HTML
+ *	*	GROWL_NOTIFICATION_DESCRIPTION
+ *	*	GROWL_NOTIFICATION_DESCRIPTION_HTML
+ *you can pass this set to -dictionaryRepresentationWithKeys:.
+ */
++ (NSSet *) standardKeys;
+
+//same as dictionaryRepresentationWithKeys:nil.
+- (NSDictionary *) dictionaryRepresentation;
+
+/*with nil, returns all of the standard keys plus the auxiliary dictionary.
+ *with non-nil, returns only the keys (from internal storage plus the auxiliary
+ *	dictionary) that are in the set.
+ *in other words, returns the intersection of the standard dictionary keys, the
+ *	auxiliary dictionary, and the provided keys.
+ */
+- (NSDictionary *) dictionaryRepresentationWithKeys:(NSSet *)keys;
+
+#pragma mark -
+
+- (NSString *) name;
+- (NSString *) applicationName;
+
+- (NSString *) title;
+- (NSAttributedString *) attributedTitle;
+- (NSString *) HTMLTitle;
+
+- (NSString *) description;
+- (NSAttributedString *) attributedDescription;
+- (NSString *) HTMLDescription;
+
+- (NSDictionary *) auxiliaryDictionary;
+- (void) setAuxiliaryDictionary:(NSDictionary *)newAuxDict;
+
+@end
---
+++ Core/Source/GrowlApplicationNotification.m
@@ -0,0 +1,209 @@
+//
+//	GrowlApplicationNotification.m
+//	Growl
+//
+//	Created by Mac-arena the Bored Zo on 2005-07-31.
+//	Copyright 2005 The Growl Project. All rights reserved.
+//
+
+@implementation GrowlApplicationNotification
+
++ (GrowlApplicationNotification *) notificationWithDictionary:(NSDictionary *)dict {
+	return [[[self alloc] initWithDictionary:dict] autorelease];
+}
+
+- (GrowlApplicationNotification *) initWithDictionary:(NSDictionary *)dict {
+	return [self initWithName:[dict objectForKey:GROWL_NOTIFICATION_NAME]
+	          applicationName:[dict objectForKey:GROWL_APP_NAME]
+	                    title:[dict objectForKey:GROWL_NOTIFICATION_TITLE]
+	                HTMLTitle:[dict objectForKey:GROWL_NOTIFICATION_TITLE_HTML]
+	              description:[dict objectForKey:GROWL_NOTIFICATION_DESCRIPTION]
+	          HTMLDescription:[dict objectForKey:GROWL_NOTIFICATION_DESCRIPTION_HTML]];
+}
+
+//you can pass nil for description.
+- (GrowlApplicationNotification *) initWithName:(NSString *)newName
+                                applicationName:(NSString *)newAppName
+                                          title:(NSString *)newTitle
+                                    description:(NSString *)newDesc
+{
+	return [self initWithName:newName
+	          applicationName:newAppName
+	                    title:newTitle
+	                HTMLTitle:nil
+	              description:newDesc
+	          HTMLDescription:nil];
+}
+
+//you can pass nil for description, or for either or both of the HTML properties.
+- (GrowlApplicationNotification *) initWithName:(NSString *)newName
+                                applicationName:(NSString *)newAppName
+                                          title:(NSString *)newTitle
+                                      HTMLTitle:(NSString *)newHTMLTitle
+                                    description:(NSString *)newDesc
+                                HTMLDescription:(NSString *)newHTMLDesc
+{
+	if((self = [super init])) {
+		name            = [newName      copy];
+		applicationName = [newAppName   copy];
+
+		title           = [newTitle     copy];
+		HTMLTitle       = [newHTMLTitle copy];
+		description     = [newDesc      copy];
+		HTMLDescription = [newHTMLDesc  copy];
+
+		hasHTMLTitle       = HTMLTitle       != nil;
+		hasHTMLDescription = HTMLDescription != nil;
+	}
+	return self;
+}
+
+- (void) dealloc {
+	[name            release];
+	[applicationName release];
+	[title           release];
+	[HTMLTitle       release];
+	[description     release];
+	[HTMLDescription release];
+
+	[dictionary          release];
+	[auxiliaryDictionary release];
+
+	[super dealloc];
+}
+
+#pragma mark -
+
++ (NSSet *) standardKeys {
+	static NSSet *standardKeys = nil;
+
+	if(!standardKeys) {
+		standardKeys = [[NSSet alloc] initWithObjects:
+			GROWL_NOTIFICATION_NAME,
+			GROWL_APP_NAME,
+			GROWL_NOTIFICATION_TITLE,
+			GROWL_NOTIFICATION_TITLE_HTML,
+			GROWL_NOTIFICATION_DESCRIPTION,
+			GROWL_NOTIFICATION_DESCRIPTION_HTML,
+			nil];
+	}
+
+	return standardKeys;
+}
+
+- (NSDictionary *)dictionaryRepresentation {
+	return [self dictionaryRepresentationWithKeys:nil];
+}
+- (NSDictionary *)dictionaryRepresentationWithKeys:(NSSet *)keys {
+	NSMutableDictionary *dict = nil;
+
+	if (!keys) {
+		//try cache first.
+		if (dictionary)
+			return [[dictionary retain] autorelease];
+
+		//no joy - create it.
+		dict = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+			name, GROWL_NOTIFICATION_NAME,
+			applicationName, GROWL_APP_NAME,
+			title, GROWL_NOTIFICATION_TITLE,
+			description, GROWL_NOTIFICATION_DESCRIPTION,
+			nil];
+
+		if (HTMLTitle)
+			[dict setObject:HTMLTitle       forKey:GROWL_NOTIFICATION_TITLE_HTML];
+		if (HTMLDescription)
+			[dict setObject:HTMLDescription forKey:GROWL_NOTIFICATION_DESCRIPTION_HTML];
+
+		NSEnumerator *auxKeyEnum = [auxiliaryDictionary keyEnumerator];
+		id key;
+		while ((key = [auxKeyEnum nextObject])) {
+			if (![dict objectForKey:key])
+				[dict setObject:[auxiliaryDictionary objectForKey:key]]
+		}
+	} else {
+		//only include keys in the set.
+		dict = [[NSMutableDictionary alloc] initWithCapacity:[keys count]];
+
+		if ([keys containsObject:GROWL_NOTIFICATION_NAME])
+			[dict setObject:name forKey:GROWL_NOTIFICATION_NAME];
+		if ([keys containsObject:GROWL_APP_NAME])
+			[dict setObject:applicationName forKey:GROWL_APP_NAME];
+		if ([keys containsObject:GROWL_NOTIFICATION_TITLE])
+			[dict setObject:title forKey:GROWL_NOTIFICATION_TITLE];
+		if ([keys containsObject:GROWL_NOTIFICATION_DESCRIPTION])
+			[dict setObject:description forKey:];
+		if (HTMLTitle && [keys containsObject:GROWL_NOTIFICATION_TITLE_HTML])
+			[dict setObject:HTMLTitle forKey:GROWL_NOTIFICATION_TITLE_HTML];
+		if (HTMLDescription && [keys containsObject:GROWL_NOTIFICATION_DESCRIPTION_HTML])
+			[dict setObject:HTMLDescription forKey:GROWL_NOTIFICATION_DESCRIPTION_HTML];
+
+		NSEnumerator *auxKeyEnum = [auxiliaryDictionary keyEnumerator];
+		id key;
+		while ((key = [auxKeyEnum nextObject])) {
+			if ([keys containsObject:key] && ![dict objectForKey:key])
+				[dict setObject:[auxiliaryDictionary objectForKey:key]]
+		}
+	}
+
+	NSDictionary *result = [NSDictionary dictionaryWithDictionary:dict];
+	[dict release];
+
+	if (!keys) {
+		//update our cache.
+		[dictionary release];
+		 dictionary = nil;
+
+		dictionary = [result retain];
+	}
+
+	return result;
+}
+
+#pragma mark -
+
+- (NSString *)name; {
+	return name;
+}
+- (NSString *)applicationName; {
+	return applicationName;
+}
+
+- (NSString *)title; {
+	return title;
+}
+- (NSAttributedString *)attributedTitle {
+	if (HTMLTitle) 
+		return [[[NSAttributedString alloc] initWithHTML:[HTMLTitle dataUsingEncoding:NSUTF8StringEncoding] documentAttributes:NULL] autorelease];
+}
+- (NSString *)HTMLTitle; {
+	return HTMLTitle;
+}
+
+- (NSString *)description; {
+	return description;
+}
+- (NSAttributedString *)attributedDescription {
+	if (HTMLDescription) 
+		return [[[NSAttributedString alloc] initWithHTML:[HTMLDescription dataUsingEncoding:NSUTF8StringEncoding] documentAttributes:NULL] autorelease];
+}
+- (NSString *)HTMLDescription; {
+	return HTMLDescription;
+}
+
+- (NSDictionary *) auxiliaryDictionary {
+	return auxiliaryDictionary;
+}
+- (void) setAuxiliaryDictionary:(NSDictionary *)newAuxDict {
+	[auxiliaryDictionary release];
+	 auxiliaryDictionary = [newAuxDict copy];
+
+	/*-dictionaryRepresentationWithKeys:nil depends on the auxiliary dictionary.
+	 *so if the auxiliary dictionary changes, we must dirty the cache used by
+	 *	-dictionaryRepresentation.
+	 */
+	[dictionary release];
+	 dictionary = nil;
+}
+
+@end
