c8261f1768bfc8aa718f93412acebe966b17e4ea
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2005-08-08 16:04:33 +0000

    Improved efficiency for the default configuration in which logging is off.  The log strings are now only generated when logging is on (and they are therefore going to be used) rather than with every notification.

--- Core/Source/GrowlLog.m
+++ Core/Source/GrowlLog.m
@@ -12,15 +12,14 @@
 #import "NSDictionaryAdditions.h"
 
 @implementation GrowlLog
-+ (void) log:(NSString *)message {
-	GrowlPreferencesController *preferences = [GrowlPreferencesController preferences];
-	if (![preferences boolForKey:GrowlLoggingEnabledKey])
-		return;
-
++ (void) _performLog:(NSString *)message {
+	GrowlPreferences *preferences = [GrowlPreferences preferences];
+	
 	int typePref = [preferences integerForKey:GrowlLogTypeKey];
 	if (typePref == 0) {
 		NSLog(@"%@", message);
 	} else {
+		BOOL written = NO;
 		NSString *logFile = [preferences objectForKey:GrowlCustomHistKey1];
 		NSFileManager *fileManager = [NSFileManager defaultManager];
 		if (![fileManager fileExistsAtPath:logFile])
@@ -30,31 +29,53 @@
 			[logHandle seekToEndOfFile];
 			[logHandle writeData:[[message stringByAppendingString:@"\n"] dataUsingEncoding:NSUTF8StringEncoding]];
 			[logHandle closeFile];
-		} else  {
+			written = YES;
+		}
+		if (!written) {
 			// Falling back to NSLogging...
-			if (logFile)
+			if (logFile) {
 				NSLog(@"Failed to write notification to file %@", logFile);
+			}
 			NSLog(@"%@", message);
 		}
+	}	
+}
+
++ (void) log:(NSString *)message {
+	GrowlPreferences *preferences = [GrowlPreferences preferences];
+	if (![preferences boolForKey:GrowlLoggingEnabledKey]) {
+		return;
 	}
+	
+	[self _performLog:message];
 }
 
 + (void) logNotificationDictionary:(NSDictionary *)noteDict {
+	GrowlPreferences *preferences = [GrowlPreferences preferences];
+	if (![preferences boolForKey:GrowlLoggingEnabledKey]) {
+		return;
+	}
+	
 	NSString *logString = [[NSString alloc] initWithFormat:@"%@ %@: %@ (%@) - Priority %d",
 		[NSDate date],
-		[noteDict objectForKey: GROWL_APP_NAME],
-		[noteDict objectForKey: GROWL_NOTIFICATION_TITLE],
-		[noteDict objectForKey: GROWL_NOTIFICATION_DESCRIPTION],
-		[noteDict integerForKey:GROWL_NOTIFICATION_PRIORITY]];
-	[self log:logString];
+		[noteDict objectForKey:GROWL_APP_NAME],
+		[noteDict objectForKey:GROWL_NOTIFICATION_TITLE],
+		[noteDict objectForKey:GROWL_NOTIFICATION_DESCRIPTION],
+		[[noteDict objectForKey:GROWL_NOTIFICATION_PRIORITY] intValue]];
+	[self _performLog:logString];
 	[logString release];
 }
 
 + (void) logRegistrationDictionary:(NSDictionary *)regDict {
+	GrowlPreferences *preferences = [GrowlPreferences preferences];
+	if (![preferences boolForKey:GrowlLoggingEnabledKey]) {
+		return;
+	}
+	
 	NSString *logString = [[NSString alloc] initWithFormat:@"%@ %@ registered",
 		[NSDate date],
 		[regDict objectForKey:GROWL_APP_NAME]];
-	[self log:logString];
+	[self _performLog:logString];
 	[logString release];
 }
 
