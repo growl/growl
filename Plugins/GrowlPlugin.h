0c10e6ee431c261e15e8a967cefb75e3a1cf9a4d
Peter Hosey <boredzo@users.noreply.github.com>
2005-09-07 05:52:22 +0000

    Adding methods to GrowlPlugin:  * {{{-pluginName}}}  * {{{-pluginAuthor}}}  * {{{-pluginVersion}}}  * {{{-pluginPathname}}} GrowlPluginController will use these.

--- Plugins/GrowlPlugin.h
+++ Plugins/GrowlPlugin.h
@@ -9,13 +9,35 @@
 #import <Cocoa/Cocoa.h>
 
 /*!	@class	GrowlPlugin
- *	@abstract	The base plug-in protocol.
- *	@discussion	The methods declared in this protocol are supported by all
- *	 Growl plug-ins.
+ *	@abstract	The base plug-in class.
+ *	@discussion	All Growl plug-in instances are a kind of this class, including
+ *	 display plug-ins, which are kinds of <code>GrowlDisplayPlugin</code>.
  */
 @interface GrowlPlugin : NSObject {
+	NSString *pluginName, *pluginAuthor, *pluginVersion, *pluginDesc;
+	NSBundle *pluginBundle;
+	NSString *pluginPathname;
+
 	NSPreferencePane *preferencePane;
 }
+
+//designated initialiser.
+- (id) initWithName:(NSString *)name author:(NSString *)author version:(NSString *)version pathname:(NSString *)pathname;
+/*use this initialiser for plug-ins in bundles. the name, author, version, and
+ *	pathname will be obtained from the bundle.
+ */
+- (id) initWithBundle:(NSBundle *)bundle;
+
+#pragma mark -
+
+- (NSString *) name;
+- (NSString *) author;
+- (NSString *) version;
+
+- (NSBundle *) bundle;
+- (NSString *) pathname;
+
+#pragma mark -
 
 /*!	@method	preferencePane
  *	@abstract	Return an <code>NSPreferencePane</code> instance that manages
@@ -27,6 +49,8 @@
  *	 289 pixels, but you should set the springs of the view and its subviews
  *	 under the assumption that it can be resized horizontally and vertically to
  *	 any size.
+ *
+ *	 The default implementation of this method returns <code>nil</code>.
  *	@result	The preference pane. Can be <code>nil</code>.
  */
 - (NSPreferencePane *) preferencePane;
--- Plugins/GrowlPlugin.m
+++ Plugins/GrowlPlugin.m
@@ -11,6 +11,72 @@
 
 @implementation GrowlPlugin
 
+//designated initialiser.
+- (id) initWithName:(NSString *)name author:(NSString *)author version:(NSString *)version pathname:(NSString *)pathname {
+	if ((self = [super init])) {
+		pluginName     = [name     copy];
+		pluginAuthor   = [author   copy];
+		pluginVersion  = [version  copy];
+		pluginPathname = [pathname copy];
+	}
+	return self;
+}
+/*use this initialiser for plug-ins in bundles. the name, author, version, and
+ *	pathname will be obtained from the bundle.
+ */
+- (id) initWithBundle:(NSBundle *)bundle {
+	NSDictionary *infoDict = [bundle infoDictionary];
+	self = [self initWithName:[infoDict objectForKey:(NSString *)kCFBundleNameKey]
+					   author:[infoDict objectForKey:@"GrowlPluginAuthor"]
+					  version:[infoDict objectForKey:(NSString *)kCFBundleVersionKey]
+					 pathname:[bundle bundlePath]];
+	if (self) {
+		[self setDescription:[infoDict objectForKey:@"GrowlPluginDescription"]];
+		pluginBundle = [bundle retain];
+	}
+	return self;
+}
+
+- (void) dealloc {
+	[pluginName release];
+	[pluginAuthor release];
+	[pluginVersion release];
+	[pluginDesc release];
+
+	[pluginBundle release];
+	[pluginPathname release];
+
+	[super dealloc];
+}
+
+#pragma mark -
+
+- (NSString *) name {
+	return pluginName;
+}
+- (NSString *) author {
+	return pluginAuthor;
+}
+- (NSString *) version {
+	return pluginVersion;
+}
+- (NSString *) description {
+	return pluginDesc;
+}
+- (void) setDescription:(NSString *)newDesc {
+	[pluginDesc release];
+	pluginDesc = [newDesc copy];
+}
+
+- (NSBundle *) bundle {
+	return pluginBundle;
+}
+- (NSString *) pathname {
+	return pluginPathname;
+}
+
+#pragma mark -
+
 - (NSPreferencePane *) preferencePane {
 	return preferencePane;
 }
