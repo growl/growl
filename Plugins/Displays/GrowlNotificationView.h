42dd75288cc8253a56a45a209a33e2c3d7ac696e
Peter Hosey <boredzo@users.noreply.github.com>
2005-10-04 12:50:52 +0000

    * Made this more useful for subclasses. Now, overridden {{{-drawRect:}}} methods call {{{-dispatchToThread}}}, which does the necessary magic and returns a {{{BOOL}}}: {{{YES}}} if it was called from another thread, {{{NO}}} if it was called from the main thread.  * Also added thread safety: {{{-dispatchToThread}}} increments and {{{-threadedDrawRectWithRectValue:}}} (see next item) decrements an ivar, and {{{-release}}} will put off calling {{{[super release]}}} until this ivar is 0. (Still need reentrant increments; see http://rentzsch.com/papers/atomicity.)  * Renamed {{{-drawRectWithRectValue:}}} to {{{-threadedDrawRectWithRectValue:}}}. Should be slightly clearer in crash logs.

--- Plugins/Displays/GrowlNotificationView.h
+++ Plugins/Displays/GrowlNotificationView.h
@@ -9,12 +9,17 @@
 #import <Cocoa/Cocoa.h>
 
 
-/*!
- * @class GrowlNotificationView
- * @abstract GrowlNotificationView is a subclass of NSView that performs all drawing on a secondary thread.
+/*!	@class	GrowlNotificationView
+ *	@abstract	GrowlNotificationView is a subclass of NSView that can perform drawing on a secondary thread.
+ *	@discussion	When subclassing, in your -drawRect:, call [self dispatchToThread]. If the current thread is the main thread (as it will be when AppKit calls -drawRect:), -dispatchToThread will dispatch a drawing thread with NSApplication (hence the name), and then it will return NO, indicating that you should simply return without doing anything further. When the current thread is the drawing thread, -dispatchToThread returns YES, indicating that you should now draw.
+ *
+ *	 All of this is optional. If you want, you can omit the call to -dispatchToThread, and simply draw on the main thread.
  */
 @interface GrowlNotificationView : NSView {
 	NSThread *mainThread;
+	unsigned numberOfThreads;
 }
+
+- (BOOL) dispatchToThread;
 
 @end
--- Plugins/Displays/GrowlNotificationView.m
+++ Plugins/Displays/GrowlNotificationView.m
@@ -9,11 +9,9 @@
 #import "GrowlNotificationView.h"
 #import "GrowlApplicationController.h"
 
-
 @interface GrowlNotificationView (private)
 - (void) drawRectWithRectValue:(NSValue *)aRect;
 @end
-
 
 @implementation GrowlNotificationView
 
@@ -25,27 +23,41 @@
 	return self;
 }
 
+- (void) release {
+	//only really release when we have no active threads. it would be Very Bad if we were released while things were happening.
+	[NSObject cancelPreviousPerformRequestsWithTarget:self
+											 selector:@selector(release)
+											   object:nil];
+	if (numberOfThreads) {
+		[self performSelector:@selector(release)
+		           withObject:nil
+		           afterDelay:0.1];
+	} else {
+		[super release];
+	}
+}
 - (void) dealloc {
 	[mainThread release];
 	[super dealloc];
 }
 
-
-//All drawings are done in a secondary thread
-- (void) drawRect:(NSRect)aRect {
-	if ([NSThread currentThread] == mainThread) {
-		[NSApplication detachDrawingThread:@selector(drawRectWithRectValue:)
+- (BOOL) dispatchToThread {
+	BOOL isMain = [NSThread currentThread] == mainThread;
+	if (isMain) {
+		++numberOfThreads;
+		[NSApplication detachDrawingThread:@selector(threadedDrawRectWithRectValue:)
 								  toTarget:self
 								withObject:[NSValue valueWithRect:aRect]];
-		return;
 	}
+	return !isMain;
 }
 
-//Convert the value back to a rect
-- (void) drawRectWithRectValue:(NSValue *)aRect {
+//Convert the value back to a rect, then lock focus (ordinarily done for us, but this is no ordinary view!) and draw. And unlock focus, of course.
+- (void) threadedDrawRectWithRectValue:(NSValue *)aRect {
 	[self lockFocus];
 	[self drawRect:[aRect rectValue]];
 	[self unlockFocus];
+	--numberOfThreads;
 }
 
 @end
