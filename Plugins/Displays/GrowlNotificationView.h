3bc6db2b798acf20400d526a59dfc2f7703cfa17
Peter Hosey <boredzo@users.noreply.github.com>
2005-10-07 13:34:47 +0000

    Comment expansion and reformatting.

--- Plugins/Displays/GrowlNotificationView.h
+++ Plugins/Displays/GrowlNotificationView.h
@@ -11,7 +11,7 @@
 
 /*!	@class GrowlNotificationView
  *	@abstract GrowlNotificationView is a subclass of NSView that can perform drawing on a secondary thread.
- *	@discussion	When subclassing, in your -drawRect:, call [self dispatchDrawingToThread:].
+ *	@discussion	When subclassing, in your -drawRect:, call [self dispatchDrawingToThread:] with the same rect that was passed to -drawRect:.
  *  If the current thread is the main thread (as it will be when AppKit calls -drawRect:), -dispatchDrawingToThread: will dispatch a drawing thread with NSApplication (hence the name), and then it will return NO, indicating that you should simply return without doing anything further.
  *  When the current thread is the drawing thread, -dispatchDrawingToThread: returns YES, indicating that you should now draw.
  *  All of this is optional. If you want, you can omit the call to -dispatchDrawingToThread:, and simply draw on the main thread.
@@ -21,10 +21,10 @@
 	unsigned numberOfThreads;
 }
 
-/*!
- * @method dispatchDrawingToThread:
- * @abstract Used to execute -drawRect: in a drawing thread so that the main thread will not freeze.
- * @param aRect The rect that will be used by the threaded -drawRect:.
+/*!	@method	dispatchDrawingToThread:
+ *	@abstract	Used to execute -drawRect: in a drawing thread so that the main thread will not freeze.
+ *	@param	aRect	The rect that will be used by the threaded -drawRect:.
+ *	@result	Returns YES if it was called on a thread that is not the main thread, which means that you should draw (because you are on the drawing thread). Returns NO if it was called on the main thread, which means that the drawing thread has now been dispatched.
  */
 - (BOOL) dispatchDrawingToThread:(NSRect)aRect;
 
