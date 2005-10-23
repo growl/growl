3ff82120dfa337404e66080dcc3b768c923363a1
toby <toby@localhost>
2005-10-23 12:16:46 +0000

    Assign copyright to Growl project; remove my email from the docs.

--- Bindings/tcl/example.tcl
+++ Bindings/tcl/example.tcl
@@ -1,6 +1,6 @@
 # example.tcl
 #
-# Copyright (c) 2004, Toby Peterson <toby@opendarwin.org>
+# Copyright (c) 2004, The Growl Project
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the Growl project nor the names of its
+# 3. Neither the name of Growl nor the names of its
 #    contributors may be used to endorse or promote products derived from
 #    this software without specific prior written permission.
 #
--- Bindings/tcl/growl.m
+++ Bindings/tcl/growl.m
@@ -1,7 +1,7 @@
 /*
  * growl.m
  *
- * Copyright (c) 2004-2005, Toby Peterson <toby@opendarwin.org>
+ * Copyright (c) 2004-2005, The Growl Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,7 +12,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Growl project nor the names of its
+ * 3. Neither the name of Growl nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
  *
