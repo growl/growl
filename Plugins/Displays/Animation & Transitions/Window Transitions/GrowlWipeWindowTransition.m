3e62b09187ded4204cd5736b232a44850d05396f
Ingmar Stein <IngmarStein@users.noreply.github.com>
2005-12-22 09:49:25 +0000

    Fixed two warnings Common subexpression elimination

--- Plugins/Displays/Animation & Transitions/Window Transitions/GrowlWipeWindowTransition.m
+++ Plugins/Displays/Animation & Transitions/Window Transitions/GrowlWipeWindowTransition.m
@@ -23,31 +23,30 @@
 	endingPoint = to;
 	xDistance = (to.x - from.x);
 	yDistance = (to.y - from.y);
-	
+
 	NSLog(@"%f %f\n", xDistance, yDistance);
 }
 
 - (void) drawTransitionWithWindow:(NSWindow *)aWindow progress:(GrowlAnimationProgress)progress {
-	NSSize newSize;
-	NSRect newFrame = [aWindow frame];
 	if (aWindow) {
+		NSSize newSize;
+		NSRect newFrame = [aWindow frame];
+		float deltaX = progress * xDistance;
+		float deltaY = progress * yDistance;
 		switch (direction) {
+			default:
 			case GrowlForwardTransition:
-				newSize.width = startingPoint.x + (progress * xDistance);
-				newSize.height = startingPoint.y + (progress * yDistance);
-				
+				newSize.width = startingPoint.x + deltaX;
+				newSize.height = startingPoint.y + deltaY;
 				break;
 			case GrowlReverseTransition:
-				newSize.width = endingPoint.x - (progress * xDistance);
-				newSize.height = endingPoint.y - (progress * yDistance);
-
-				break;
-			default:
+				newSize.width = endingPoint.x - deltaX;
+				newSize.height = endingPoint.y - deltaY;
 				break;
 		}
 		newFrame.size.height = newSize.height;
 		newFrame.size.width = newSize.width;
-		
+
 		[aWindow setFrame:newFrame display:YES];
 		[aWindow setViewsNeedDisplay:YES];
 	}
