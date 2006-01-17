67c572fa92dadb5242c5b42a8e18b6c8aa8e94ec
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2006-01-17 21:31:18 +0000

    i think this was a prettier layout than previous checkin on nano

--- Plugins/Displays/Nano/GrowlNanoWindowView.m
+++ Plugins/Displays/Nano/GrowlNanoWindowView.m
@@ -62,116 +62,115 @@ void addRoundedBottomToPath(CGContextRef context, CGRect rect, float radius) {
 
 - (void) drawRect:(NSRect)rect {
 	NSLog(@"%s %f %f %f %f\n", __FUNCTION__, rect.origin.x, rect.origin.y, rect.size.height, rect.size.width);
-	////Make sure that we don't draw in the main thread
-	////if ([super dispatchDrawingToThread:rect]) {
-		NSGraphicsContext *context = [NSGraphicsContext currentContext];
-		CGContextRef cgContext = [context graphicsPort];
-		NSRect bounds = [self bounds];
+		
+	NSGraphicsContext *context = [NSGraphicsContext currentContext];
+	CGContextRef cgContext = [context graphicsPort];
+	NSRect bounds = [self bounds];
 
-		if (needsDisplay) {
-			// rects and sizes
-			int sizePref = 0;
-			READ_GROWL_PREF_INT(Nano_SIZE_PREF, GrowlNanoPrefDomain, &sizePref);
-			//NSRect titleRect, textRect;
-			////NSRect iconRect;
+	if (needsDisplay) {
+		// rects and sizes
+		int sizePref = 0;
+		READ_GROWL_PREF_INT(Nano_SIZE_PREF, GrowlNanoPrefDomain, &sizePref);
+		NSRect titleRect, textRect;
+		NSRect iconRect;
 
-			/*if (sizePref == Nano_SIZE_HUGE) {
-				titleRect.origin.x = 192.0f;
-				titleRect.origin.y = NSHeight(bounds) - 72.0f;
-				titleRect.size.width = NSWidth(bounds) - 192.0f - 32.0f;
-				titleRect.size.height = 40.0f;
-				textRect.origin.y = NSHeight(bounds) - 176.0f;
-				textRect.size.height = 96.0f;
-				//iconRect.origin.x = 32.0f;
-				//iconRect.origin.y = NSHeight(bounds) - 160.0f;
-				//iconRect.size.width = 128.0f;
-				//iconRect.size.height = 128.0f;
-			} else {
-				titleRect.origin.x = 96.0f;
-				titleRect.origin.y = NSHeight(bounds) - 36.0f;
-				titleRect.size.width = NSWidth(bounds) - 96.0f - 16.0f;
-				titleRect.size.height = 25.0f;
-				textRect.origin.y = NSHeight(bounds) - 88.0f,
-					textRect.size.height = 48.0f;
-				//iconRect.origin.x = 8.0f;
-				//iconRect.origin.y = NSHeight(bounds) - 88.0f;
-				//iconRect.size.width = 80.0f;
-				//iconRect.size.height = 80.0f;
-			}
-			textRect.origin.x = titleRect.origin.x;
-			textRect.size.width = titleRect.size.width;*/
+		if (sizePref == Nano_SIZE_HUGE) {
+			titleRect.origin.x = 10.0f;
+			titleRect.origin.y = NSHeight(bounds) - 28.0f;
+			titleRect.size.width = NSWidth(bounds) - 32.0f;
+			titleRect.size.height = 20.0f;
+			
+			textRect.origin.y = NSHeight(bounds) - 41.0f;
+			textRect.size.height = 15.0f;
+			
+			iconRect.origin.x = 200.0f;
+			iconRect.origin.y = NSHeight(bounds) - 40.0f;
+			iconRect.size.width = 32.0f;
+			iconRect.size.height = 32.0f;
+		} /*else {
+			titleRect.origin.x = 96.0f;
+			titleRect.origin.y = NSHeight(bounds) - 36.0f;
+			titleRect.size.width = NSWidth(bounds) - 96.0f - 16.0f;
+			titleRect.size.height = 25.0f;
+			textRect.origin.y = NSHeight(bounds) - 88.0f,
+				textRect.size.height = 48.0f;
+			//iconRect.origin.x = 8.0f;
+			//iconRect.origin.y = NSHeight(bounds) - 88.0f;
+			//iconRect.size.width = 80.0f;
+			//iconRect.size.height = 80.0f;
+		}*/
+		textRect.origin.x = titleRect.origin.x;
+		textRect.size.width = titleRect.size.width;
 
-			//draw to cache
-			/*if (CGLayerCreateWithContext) {
-				if (!layer)
-					layer = CGLayerCreateWithContext(cgContext, CGSizeMake(bounds.size.width, bounds.size.height), NULL);
-				[NSGraphicsContext setCurrentContext:
-					[NSGraphicsContext graphicsContextWithGraphicsPort:CGLayerGetContext(layer) flipped:NO]];
-			} else {
-				[cache lockFocus];
-			}*/
-
-			NSRect c = [self bounds];
-			CGRect b = CGRectMake(c.origin.x, c.origin.y, c.size.width, c.size.height);
-			addRoundedBottomToPath(cgContext, b, 10.0f);
-
-			float opacityPref = Nano_DEFAULT_OPACITY;
-			READ_GROWL_PREF_FLOAT(Nano_OPACITY_PREF, GrowlNanoPrefDomain, &opacityPref);
-			float alpha = opacityPref * 0.01f;
-			[backgroundColor release];
-			backgroundColor = [NSColor blackColor];
-			[[backgroundColor colorWithAlphaComponent:alpha] set];
-			CGContextFillPath(cgContext);
-
-			////NSRectFill(bounds);
-			//[title drawInRect:titleRect withAttributes:titleAttributes];
-
-			//[text drawInRect:textRect withAttributes:textAttributes];
-
-			////[icon setFlipped:NO];
-			////[icon drawScaledInRect:iconRect operation:NSCompositeSourceOver fraction:1.0f];
-
-			/*if (CGLayerCreateWithContext)
-				[NSGraphicsContext setCurrentContext:context];
-			else
-				[cache unlockFocus];
-
-			needsDisplay = NO;*/
-		}
-
-		// draw background
-		//[[NSColor clearColor] set];
-		//NSRectFill(rect);
-		//CGContextFillPath(cgContext);
-		// draw cache to screen
-		NSRect imageRect = rect;
-		int effect = Nano_EFFECT_SLIDE;
-		READ_GROWL_PREF_BOOL(Nano_EFFECT_PREF, GrowlNanoPrefDomain, &effect);
-		if (effect == Nano_EFFECT_SLIDE) {
-			if (CGLayerCreateWithContext)
-				imageRect.origin.y = 0.0f;
-		} else if (effect == Nano_EFFECT_WIPE) {
-			rect.size.height -= imageRect.origin.y;
-			imageRect.size.height -= imageRect.origin.y;
-			if (!CGLayerCreateWithContext)
-				imageRect.origin.y = 0.0f;
-		}
-
-		if (CGLayerCreateWithContext) {
-			CGRect cgRect;
-			cgRect.origin.x = imageRect.origin.x;
-			cgRect.origin.y = imageRect.origin.y;
-			cgRect.size.width = rect.size.width;
-			if (effect == Nano_EFFECT_WIPE) {
-				cgRect.size.height = rect.size.height;
-				CGContextClipToRect(cgContext, cgRect);
-			}
-			cgRect.size.height = bounds.size.height;
-			CGContextDrawLayerInRect(cgContext, cgRect, layer);
+		//draw to cache
+		/*if (CGLayerCreateWithContext) {
+			if (!layer)
+				layer = CGLayerCreateWithContext(cgContext, CGSizeMake(bounds.size.width, bounds.size.height), NULL);
+			[NSGraphicsContext setCurrentContext:
+				[NSGraphicsContext graphicsContextWithGraphicsPort:CGLayerGetContext(layer) flipped:NO]];
 		} else {
-			[cache drawInRect:rect fromRect:imageRect operation:NSCompositeSourceOver fraction:1.0f];
+			[cache lockFocus];
+		}*/
+
+		NSRect c = [self bounds];
+		CGRect b = CGRectMake(c.origin.x, c.origin.y, c.size.width, c.size.height);
+		addRoundedBottomToPath(cgContext, b, 10.0f);
+
+		float opacityPref = Nano_DEFAULT_OPACITY;
+		READ_GROWL_PREF_FLOAT(Nano_OPACITY_PREF, GrowlNanoPrefDomain, &opacityPref);
+		float alpha = opacityPref * 0.01f;
+		[backgroundColor release];
+		backgroundColor = [NSColor blackColor];
+		[[backgroundColor colorWithAlphaComponent:alpha] set];
+		CGContextFillPath(cgContext);
+
+		////NSRectFill(bounds);
+		[title drawInRect:titleRect withAttributes:titleAttributes];
+
+		[text drawInRect:textRect withAttributes:textAttributes];
+		[icon setFlipped:NO];
+		[icon drawScaledInRect:iconRect operation:NSCompositeSourceOver fraction:1.0f];
+
+		/*if (CGLayerCreateWithContext)
+			[NSGraphicsContext setCurrentContext:context];
+		else
+			[cache unlockFocus];
+
+		needsDisplay = NO;*/
+	}
+
+	// draw background
+	//[[NSColor clearColor] set];
+	//NSRectFill(rect);
+	//CGContextFillPath(cgContext);
+	// draw cache to screen
+	NSRect imageRect = rect;
+	int effect = Nano_EFFECT_SLIDE;
+	READ_GROWL_PREF_BOOL(Nano_EFFECT_PREF, GrowlNanoPrefDomain, &effect);
+	if (effect == Nano_EFFECT_SLIDE) {
+		if (CGLayerCreateWithContext)
+			imageRect.origin.y = 0.0f;
+	} else if (effect == Nano_EFFECT_WIPE) {
+		rect.size.height -= imageRect.origin.y;
+		imageRect.size.height -= imageRect.origin.y;
+		if (!CGLayerCreateWithContext)
+			imageRect.origin.y = 0.0f;
+	}
+
+	if (CGLayerCreateWithContext) {
+		CGRect cgRect;
+		cgRect.origin.x = imageRect.origin.x;
+		cgRect.origin.y = imageRect.origin.y;
+		cgRect.size.width = rect.size.width;
+		if (effect == Nano_EFFECT_WIPE) {
+			cgRect.size.height = rect.size.height;
+			CGContextClipToRect(cgContext, cgRect);
 		}
-	//}
+		cgRect.size.height = bounds.size.height;
+		CGContextDrawLayerInRect(cgContext, cgRect, layer);
+	} else {
+		[cache drawInRect:rect fromRect:imageRect operation:NSCompositeSourceOver fraction:1.0f];
+	}
 }
 
 - (void) setIcon:(NSImage *)anIcon {
@@ -252,11 +251,11 @@ void addRoundedBottomToPath(CGContextRef context, CGRect rect, float radius) {
 	READ_GROWL_PREF_INT(Nano_SIZE_PREF, GrowlNanoPrefDomain, &sizePref);
 
 	if (sizePref == Nano_SIZE_HUGE) {
-		titleFontSize = 32.0f;
-		textFontSize = 20.0f;
-	} else {
-		titleFontSize = 16.0f;
+		titleFontSize = 14.0f;
 		textFontSize = 12.0f;
+	} else {
+		titleFontSize = 12.0f;
+		textFontSize = 10.0f;
 	}
 
 	NSShadow *textShadow = [[NSShadow alloc] init];
