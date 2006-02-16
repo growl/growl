2291a335877ffaef6fd32eb3f9eed75a0ad3a0cb
keith <keith@localhost>
2006-02-16 05:28:59 +0000

    Somehow forgot to commit these...

---
+++ Extras/Feed/CustomViews/KNStatusBar.h
@@ -0,0 +1,16 @@
+//
+//  KNStatusBar.h
+//  Feed
+//
+//  Created by Keith on 2/4/06.
+//  Copyright 2006 Keith Anderson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface KNStatusBar : NSView {
+	
+}
+
+@end
