bbef0ff6697dded1913562361f07db06127781fb
sdwilsh <sdwilsh@localhost>
2007-02-24 01:30:57 +0000

    Work to get this building from withing Mozilla.  This will greatly simplfy the building of the extension. yey!

--- trunk/components/files.mk
+++
@@ -1,9 +0,0 @@
-#
-#  $Id$
-#
-#  Copyright 2007 The Growl Project. All rights reserved.
-#
-# This file is under the BSD License, refer to license.txt for details
-
-COMPONENT_FILES = \
-	nsIAlertsService.xpt
