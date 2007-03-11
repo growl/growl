7f743ffdf1321cd7d3ce2c959992f2e0ece8bb12
sdwilsh <sdwilsh@localhost>
2007-03-11 05:06:39 +0000

    Added a download prompt if growl is not installed.

--- trunk/content/growl.js
+++ trunk/content/growl.js
@@ -52,6 +52,24 @@ GrowlNotifications.prototype =
     if (!this.frameworkInstalled) {
       this.installFramework();
     }
+
+    var gab = Components.classes["@growl.info/application-bridge;1"]
+                        .getService(Components.interfaces.grIApplicationBridge);
+    if (!gab.growlInstalled) {
+      window.setTimeout(this.promptForGrowlDownload, 5000);
+    }
+  },
+
+  promptForGrowlDownload: function promptForGrowlDownload()
+  {
+    var out = { accepted: false };
+    window.openDialog("chrome://growl/content/download.xul", "DownloadPrompt",
+                      "chrome,modal,centerscreen", out);
+
+    if (out.accepted) {
+      // when this is called from setTimeout, this doesn't point to the object
+      gGrowl.sendToGrowlHomepage();
+    }
   },
 
   /**
@@ -113,6 +131,23 @@ GrowlNotifications.prototype =
     Components.classes["@mozilla.org/toolkit/app-startup;1"]
               .getService(nsIAppStartup)
               .quit(nsIAppStartup.eRestart | nsIAppStartup.eAttemptQuit);
+  },
+
+  sendToGrowlHomepage: function sendToGrowlHomepage()
+  {
+    const HOMEPAGE = "http://growl.info/";
+
+    if (gBrowser && ("addTab" in gBrowser)) {
+      gBrowser.selectedTab = gBrowser.addTab(HOMEPAGE);
+    } else {
+      var uri = Components.classes["@mozilla.org/network/io-service;1"]
+                          .getService(Components.interfaces.nsIIOService)
+                          .newURI(HOMEPAGE, null, null);
+      var eps = Components.classes["@mozilla.org/uriloader/external-helper-app-service;1"]
+                          .createInstance(Components.interfaces.nsIExternalProtocolService);
+
+      eps.loadUrl(uri);
+    }
   },
 
   //////////////////////////////////////////////////////////////////////////////
