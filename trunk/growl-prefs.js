0a22f6409a5c35751afb8cefb80e1edc5c85df65
sdwilsh <sdwilsh@localhost>
2007-03-21 04:13:52 +0000

    Typo in locale url.

--- trunk/growl-prefs.js
+++ trunk/growl-prefs.js
@@ -5,7 +5,7 @@
 //
 // This file is under the BSD License, refer to license.txt for details
 
-pref("extensions.growl@growl.info.description", "chrome://growl/local/text.properties");
+pref("extensions.growl@growl.info.description", "chrome://growl/locale/text.properties");
 pref("extensions.growl.count.notifications", 0);
 pref("extensions.growl.count.browser", 0);
 pref("extensions.growl.count.mail", 0);
