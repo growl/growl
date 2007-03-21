4634f5d2cb8acc27fc1378be8a44d5c3fe659ff6
sdwilsh <sdwilsh@localhost>
2007-03-21 04:04:07 +0000

    Added localization ability to extension description. Default preferences are back in (code to be checked in later)

---
+++ trunk/growl-prefs.js
@@ -0,0 +1,11 @@
+//
+//  $Id$
+//
+//  Copyright 2007 The Growl Project. All rights reserved.
+//
+// This file is under the BSD License, refer to license.txt for details
+
+pref("extensions.growl@growl.info.description", "chrome://growl/local/text.properties");
+pref("extensions.growl.count.notifications", 0);
+pref("extensions.growl.count.browser", 0);
+pref("extensions.growl.count.mail", 0);
