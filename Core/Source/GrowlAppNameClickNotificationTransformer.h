a7cf2fe02a0ecf02e675e9111d6282d26a8c1b73
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-05-25 15:33:17 +0000

    Use a transformer to specify the title for the click notification checkbox rather than a text field next to it.  Fixes #686

---
+++ Core/Source/GrowlAppNameClickNotificationTransformer.h
@@ -0,0 +1,15 @@
+//
+//  GrowlAppNameClickNotificationTransformer.h
+//  Growl
+//
+//  Created by Evan Schoenberg on 5/25/07.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface GrowlAppNameClickNotificationTransformer : NSValueTransformer {
+
+}
+
+@end
