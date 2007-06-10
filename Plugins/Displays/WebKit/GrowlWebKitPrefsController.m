0217fe87171513934381716aba83e0b4668e2f8a
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-06-10 23:00:56 +0000

    One more use of a constant where appropriate

--- Plugins/Displays/WebKit/GrowlWebKitPrefsController.m
+++ Plugins/Displays/WebKit/GrowlWebKitPrefsController.m
@@ -13,7 +13,7 @@
 
 @implementation GrowlWebKitPrefsController
 - (id) initWithStyle:(NSString *)styleName {
-	if ((self = [self initWithBundle:[NSBundle bundleWithIdentifier:@"com.growl.prefpanel"]])) {
+	if ((self = [self initWithBundle:[NSBundle bundleWithIdentifier:GROWL_PREFPANE_BUNDLE_IDENTIFIER]])) {
 		style = [styleName retain];
 		prefDomain = [[NSString alloc] initWithFormat:@"%@.%@", GrowlWebKitPrefDomain, style];
 	}
