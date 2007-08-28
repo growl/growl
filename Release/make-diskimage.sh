d5383cd131a89c5c185ed213076046794117a1cd
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-08-28 17:47:21 +0000

    Finished Growl release build scripts. Awesome.

--- Release/make-diskimage.sh
+++ Release/make-diskimage.sh
@@ -4,6 +4,7 @@
 #                       <src_folder>
 #                       <volume_name>
 #                       <applescript>
+#                       <artpath>
 #                       <eula_resource_file>
 
 set -e;
@@ -17,48 +18,59 @@ VOLUME_NAME=$3
 
 # optional arguments
 APPLESCRIPT=$4
-EULA_RSRC=$5
+ART_PATH=$5
+EULA_RSRC=$6
 
 # Create the image
-echo "creating disk image"
-rm -f $DMG_TEMP_NAME
-hdiutil create -srcfolder $SRC_FOLDER -volname $VOLUME_NAME -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW $DMG_TEMP_NAME
+echo "Creating disk image..."
+rm -f "${DMG_TEMP_NAME}"
+hdiutil create -srcfolder "${SRC_FOLDER}" -volname "${VOLUME_NAME}" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW "${DMG_TEMP_NAME}"
 
 # mount it
-echo "mounting disk image"
-MOUNT_DIR=/Volumes/$VOLUME_NAME
-DEV_NAME=`hdiutil attach -readwrite -noverify -noautoopen $DMG_TEMP_NAME | egrep '^/dev/' | sed 1q | awk '{print $1}'`
+echo "Mounting disk image..."
+MOUNT_DIR="/Volumes/${VOLUME_NAME}"
+DEV_NAME=`hdiutil attach -readwrite -noverify -noautoopen "${DMG_TEMP_NAME}" | egrep '^/dev/' | sed 1q | awk '{print $1}'`
 
 # run applescript
 if [ ! -z "${APPLESCRIPT}" -a "${APPLESCRIPT}" != "-null-" ]; then
-	osascript $APPLESCRIPT
+#	osascript "${APPLESCRIPT}"
+    #   pass the applescript our volume name and our artwork path, to its process_disk_image function
+    echo "Running Applescript: ./AdiumApplescriptRunner \"${APPLESCRIPT}\" process_disk_image \"${VOLUME_NAME}\""
+    ./AdiumApplescriptRunner "${APPLESCRIPT}" process_disk_image "${VOLUME_NAME}" "${ART_PATH}" || true
+    echo "Done running the applescript..."
 fi
 
+
+# run shell script
+# if [ ! -z "${SHELLSCRIPT}" -a "${SHELLSCRIPT}" != "-null-" ]; then
+#   ./${SHELLSCRIPT} \"${VOLUME_NAME}\"
+# fi
+
 # make sure it's not world writeable
-echo "fixing permissions"
-chmod -Rf go-w ${MOUNT_DIR} || true
+echo "Fixing permissions..."
+chmod -Rf go-w "${MOUNT_DIR}" || true
 
 # make the top window open itself on mount:
 if [ -x /usr/local/bin/openUp ]; then
-    /usr/local/bin/openUp ${MOUNT_DIR}
+    /usr/local/bin/openUp "${MOUNT_DIR}"
 fi
 
 # unmount
-echo "unmounting disk image"
-hdiutil detach $DEV_NAME
+echo "Unmounting disk image..."
+hdiutil detach "${DEV_NAME}"
 
 # compress image
-echo "compressing disk image"
-hdiutil convert $DMG_TEMP_NAME -format UDZO -imagekey zlib-level=9 -o ${DMG_DIR}/${DMG_NAME}
-rm -f $DMG_TEMP_NAME
+echo "Compressing disk image..."
+hdiutil convert "${DMG_TEMP_NAME}" -format UDZO -imagekey zlib-level=9 -o "${DMG_DIR}/${DMG_NAME}"
+rm -f "${DMG_TEMP_NAME}"
 
 # adding EULA resources
 if [ ! -z "${EULA_RSRC}" -a "${EULA_RSRC}" != "-null-" ]; then
         echo "adding EULA resources"
-        hdiutil unflatten ${DMG_DIR}/${DMG_NAME}
-        /Developer/Tools/ResMerger -a ${EULA_RSRC} -o ${DMG_DIR}/${DMG_NAME}
-        hdiutil flatten ${DMG_DIR}/${DMG_NAME}
+        hdiutil unflatten "${DMG_DIR}/${DMG_NAME}"
+        /Developer/Tools/ResMerger -a "${EULA_RSRC}" -o "${DMG_DIR}/${DMG_NAME}"
+        hdiutil flatten "${DMG_DIR}/${DMG_NAME}"
 fi
 
-echo "disk image done"
+echo "Disk image done"
 exit 0
