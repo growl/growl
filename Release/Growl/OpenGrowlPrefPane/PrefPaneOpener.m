b36ec0cd5f8e6fa322e701d7fd97b50a28cbb910
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-01 15:54:14 +0000

    PrefPaneOpener now works on all versions of OS X instead of just 10.5. Added documentation to the class

--- Release/Growl/OpenGrowlPrefPane/PrefPaneOpener.m
+++ Release/Growl/OpenGrowlPrefPane/PrefPaneOpener.m
@@ -8,6 +8,15 @@
 #import "PrefPaneOpener.h"
 #import "AEVTBuilder.h"
 
+/*!
+ * @class PrefPaneOpener
+ * @brief Opens a specified preference pane in the System Preferences
+ *
+ * This is equivalent to the applescript:
+ *		tell application "System Preferences" to set current pane to pane "preferencePaneName"
+ * except it works with any localization, not just English.  Applescript has no way of talking to an application
+ * with a specified bundle ID.
+ */
 @implementation PrefPaneOpener
 
 + (void)openPrefPane:(NSString *)preferencePaneName
@@ -29,16 +38,24 @@
 		}
 	}
 	
-	NSAppleEventDescriptor *descriptor = [AEVT class:'misc' id:'mvis'
+	/* tell application "System Preferences" to set current pane to pane "preferencePaneName" */
+	NSAppleEventDescriptor *descriptor = [AEVT class:'core' id:'setd'
 											  target:psn,
-										  [KEY : '----'],
+										  [KEY : 'data'],
 										  [RECORD : 'obj ',
 										   [KEY : 'form'], [ENUM : 'name'],
 										   [KEY : 'want'], [TYPE : 'xppb'],
 										   [KEY : 'seld'], [STRING  : preferencePaneName],
 										   [KEY : 'from'], [DESC null],
 										   ENDRECORD],
-										  ENDRECORD];
+										  [KEY : '----'],
+										  [RECORD : 'obj ',
+										   [KEY : 'form'], [ENUM : 'prop'],
+										   [KEY : 'want'], [TYPE : 'prop'],
+										   [KEY : 'seld'], [TYPE  : 'xpcp'],
+										   [KEY : 'from'], [DESC null],
+										   ENDRECORD],
+										   ENDRECORD];
 	[descriptor sendWithImmediateReply];	
 }
 
