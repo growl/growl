b990bf9893ceaca4861e375b587194f3152d43e7
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-03 18:16:40 +0000

    GrowlMail and GrowlSafari now use modern .mkpg installers

---
+++ Release/GrowlMail/scripts/postflight.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+defaults write /Library/Preferences/com.apple.mail EnableBundles 1
+defaults write /Library/Preferences/com.apple.mail BundleCompatibilityVersion 2
---
+++ Release/GrowlMail/scripts/preflight.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm -rf ~/Library/Mail/Bundles/GrowlMail.mailBundle
+sudo rm -rf /Library/Mail/Bundles/GrowlMail.mailBundle
