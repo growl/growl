f6991b395b12daf1eb704e0b665665aa03d1dca2
tick <1464091+ChrisForsythe@users.noreply.github.com>
2007-09-04 04:01:48 +0000

    Inspired by http://groups.google.com/group/growl-localizations/msg/aac28b075652bcc8 I made this quick little hackjob app. Maybe we can improve itand regain some space on the disk image.

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Linkinator
+//
+//  Created by chris on 9/3/07.
+//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
