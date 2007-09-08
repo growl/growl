d05dc4be8e3a4c46524a77656713dd9649cf377b
Peter Hosey <boredzo@users.noreply.github.com>
2007-09-08 20:26:43 +0000

    Fix assumption that the file-system is case-insensitive. This resulted in the installer failing because we tried to invoke TRUE, and there is no such utility on the system.
    The correct name is true. ?TRUE? matches on case-insensitive file-systems; however, on case-''sensitive'' file-systems, TRUE is not found, causing the failure.

--- Release/Growl/scripts/preflight.sh
+++ Release/Growl/scripts/preflight.sh
@@ -3,6 +3,6 @@ rm -rf ~/Library/PreferencePanes/Growl.prefPane
 sudo rm -rf /Library/PreferencePanes/Growl.prefPane
 
 #The installer will have killed these if this is a fresh install but not if it is an upgrade... where upgrade is defined fairly inconsistently.  Killing the system prefs by name won't work for non-English systems, but the attempt is better than nothing. Optimally, we'd kill the process with the bundle identifier com.apple.systempreferenes.
-killall "System Preferences" || TRUE
-killall GrowlHelperApp || TRUE
-killall GrowlMenu || TRUE
+killall "System Preferences" || true
+killall GrowlHelperApp || true
+killall GrowlMenu || true
