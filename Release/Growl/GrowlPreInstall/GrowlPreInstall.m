47a2686bf34398b0f11332a827a67afa15fd0935
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-10 23:08:28 +0000

    Added the source for the GrowlPreInstall utility which will quit system preferences, growl helper app, and growl menu, as well as deleting old copies of growl, before installation occurs. This is needed because system preferences has a localized name so can only be reliably accessed via its ProcessSerialNumber obtained with its bundle ID.
    Technique used for obtaining the PSN posted to http://www.cocoadev.com/index.pl?AEVTBuilder.

---
+++ Release/Growl/GrowlPreInstall/GrowlPreInstall.m
@@ -0,0 +1,53 @@
+#import <Cocoa/Cocoa.h>
+#import "AEVTBuilder.h"
+
+@interface NSWorkspace (ProcessSerialNumberFinder)
+- (ProcessSerialNumber)processSerialNumberForApplicationWithIdentifier:(NSString *)identifier;
+@end
+
+@implementation NSWorkspace (ProcessSerialNumberFinder)
+- (ProcessSerialNumber)processSerialNumberForApplicationWithIdentifier:(NSString *)identifier
+{
+	ProcessSerialNumber psn = {0, 0};
+
+	NSEnumerator *enumerator = [[self launchedApplications] objectEnumerator];
+	NSDictionary *dict;
+	while ((dict = [enumerator nextObject])) {
+		if ([[dict objectForKey:@"NSApplicationBundleIdentifier"] isEqualToString:identifier]) {
+			psn.highLongOfPSN = [[dict objectForKey:@"NSApplicationProcessSerialNumberHigh"] longValue];
+			psn.lowLongOfPSN  = [[dict objectForKey:@"NSApplicationProcessSerialNumberLow"] longValue];
+			break;
+		}
+	}
+	
+	return psn;
+}
+@end
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	NSAppleEventDescriptor *descriptor;
+	/* tell application "System Preferences" to quit. The name may be localized, so we can't use applescript directly. */
+	descriptor = [AEVT class:'aevt' id:'quit'
+					  target:[[NSWorkspace sharedWorkspace] processSerialNumberForApplicationWithIdentifier:@"com.apple.systempreferences"],
+				  ENDRECORD];
+	[descriptor sendWithImmediateReply];	
+
+	/* tell application "GrowlHelperApp" to quit */
+	[NSTask launchedTaskWithLaunchPath:@"/usr/bin/killall" arguments:[NSArray arrayWithObject:@"GrowlHelperApp"]];
+	[NSTask launchedTaskWithLaunchPath:@"/usr/bin/killall" arguments:[NSArray arrayWithObject:@"GrowlMenu"]];
+
+	/* delete old Growl installations */
+	[[NSFileManager defaultManager] removeFileAtPath:[[[NSHomeDirectory()
+														stringByAppendingPathComponent:@"Library"]
+													   stringByAppendingPathComponent:@"PreferencePanes"] 
+													  stringByAppendingPathComponent:@"Growl.prefPane"] handler:nil];
+
+	/* We'll be running sudo'd from the installer, so this will have appropriate permissions */
+	[[NSFileManager defaultManager] removeFileAtPath:@"/Library/PreferencePanes/Growl.prefPane" handler:nil];
+	
+
+    [pool release];
+    return 0;
+}
