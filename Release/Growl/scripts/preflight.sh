6e3aecdca43f3c9253796cc8ef78e71ebe2b6e33
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-09-10 23:09:13 +0000

    Added GrowlPreInstall utility and removed preflight.sh script. Growl should now reliably install on all systems regardless of localization settings

--- Release/Growl/scripts/preflight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -rf ~/Library/PreferencePanes/Growl.prefPane
-sudo rm -rf /Library/PreferencePanes/Growl.prefPane
-
-#The installer will have killed these if this is a fresh install but not if it is an upgrade... where upgrade is defined fairly inconsistently.  Killing the system prefs by name won't work for non-English systems, but the attempt is better than nothing. Optimally, we'd kill the process with the bundle identifier com.apple.systempreferenes.
-killall "System Preferences" || true
-killall GrowlHelperApp || true
-killall GrowlMenu || true
