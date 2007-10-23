9564ed6baf8e39be376ff138497d078e936b2fa3
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-10-23 13:00:52 +0000

    Finished fixing the GrowlMail installation process, I think.

--- Release/GrowlMail/scripts/postflight.sh
+++ Release/GrowlMail/scripts/postflight.sh
@@ -5,7 +5,15 @@
 #
 # Mail must NOT be running by the time this script executes
 ######
-defaults write com.apple.mail EnableBundles -bool YES
+if [ `whoami` == root ] ; then
+    #defaults acts funky when asked to write to the root domain but seems to work with a full path
+    defaults write /Library/Preferences/com.apple.mail EnableBundles -bool YES
 
-# Mac OS X 10.5's Mail.app requires bundle version 3 or greater
-defaults write com.apple.mail BundleCompatibilityVersion -int 3
+    # Mac OS X 10.5's Mail.app requires bundle version 3 or greater
+    defaults write /Library/Preferences/com.apple.mail BundleCompatibilityVersion -int 3
+else
+    defaults write com.apple.mail EnableBundles -bool YES
+
+    # Mac OS X 10.5's Mail.app requires bundle version 3 or greater
+    defaults write com.apple.mail BundleCompatibilityVersion -int 3
+fi
