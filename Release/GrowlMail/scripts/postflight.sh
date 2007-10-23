7ae8bd35904e8edcf936b156b11a5e26d601faa3
Evan D. Schoenberg, M.D <evan@regularrateandrhythm.com>
2007-10-23 12:43:54 +0000

    Fixed GrowlMail Installer on 10.5, added an instruction to quit Mail before continuing, and enforced qutting of Mail via the preflight script.  The quit actions added by 10.5's package maker simply don't work.

--- Release/GrowlMail/scripts/postflight.sh
+++ Release/GrowlMail/scripts/postflight.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+######
+# Note that we are running sudo'd, so these defaults will be written to
+# /Library/Preferences/com.apple.mail.plist
+#
+# Mail must NOT be running by the time this script executes
+######
 defaults write com.apple.mail EnableBundles -bool YES
-defaults write com.apple.mail BundleCompatibilityVersion -int 2
+
+# Mac OS X 10.5's Mail.app requires bundle version 3 or greater
+defaults write com.apple.mail BundleCompatibilityVersion -int 3
--- Release/GrowlMail/scripts/preflight.sh
+++ Release/GrowlMail/scripts/preflight.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+#####
+# We politely asked the user to quit Mail in the installer intro.  Now
+# we'll request the same a bit more strongly.
+####
+osascript -e "quit app \"Mail\""
+
+# Delete any old copies of the bundle
 rm -rf ~/Library/Mail/Bundles/GrowlMail.mailBundle
+
+# We're running sudo'd already, so this won't have to prompt for a password.
 sudo rm -rf /Library/Mail/Bundles/GrowlMail.mailBundle
