5adaa12baba7f7701f7734743b3da2d57cf75692
Peter Hosey <boredzo@users.noreply.github.com>
2008-06-15 00:08:24 +0000

    Don't delete /Library/Mail/Bundles/GrowlMail.mailbundle before installing. Probably would fix the same ?Install Failed? problem that the Growl installer had, if we were going to keep supporting that destination in the first place.

--- Release/GrowlMail/scripts/preflight.sh
+++ Release/GrowlMail/scripts/preflight.sh
@@ -7,6 +7,3 @@ osascript -e "quit app \"Mail\""
 
 # Delete any old copies of the bundle
 rm -rf ~/Library/Mail/Bundles/GrowlMail.mailbundle
-
-# We're running sudo'd already, so this won't have to prompt for a password.
-sudo rm -rf /Library/Mail/Bundles/GrowlMail.mailbundle
