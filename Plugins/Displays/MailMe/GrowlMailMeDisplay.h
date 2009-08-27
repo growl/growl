23e9681dd5a96a7a3287931b8d9e89814553b6b4
Peter Hosey <boredzo@users.noreply.github.com>
2009-08-27 14:05:13 -0700

    Rewrite MailMe to be based on my Python-based simple-mailer program. This enables it to not care what mail client you use, although it currently still does obtain your SMTP settings from Mail's preferences (like the Message framework did).

--- Plugins/Displays/MailMe/GrowlMailMeDisplay.h
+++ Plugins/Displays/MailMe/GrowlMailMeDisplay.h
@@ -9,6 +9,10 @@
 #import "GrowlDisplayPlugin.h"
 
 @interface GrowlMailMeDisplay: GrowlDisplayPlugin {
+	NSString *pathToMailSenderProgram;
+	NSDictionary *defaultSMTPAccount;
+	NSString *fromAddress; //May be a "Name <Address>" string.
+	NSMutableArray *tasks;
 }
 
 - (void) displayNotification:(GrowlApplicationNotification *)notification;
