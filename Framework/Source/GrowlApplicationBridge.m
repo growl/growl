941ed0637396d24d191c6f31896f16bf4bb1c0d4
Peter Hosey <boredzo@users.noreply.github.com>
2009-09-24 13:22:25 -0700

    Switched from using Launch Services to open the .growlRegDict file to sending GHA the open-document event ourselves. This seems to fix the bug where GrowlSafari registering yanks the user out of Safari.

--- Framework/Source/GrowlApplicationBridge.m
+++ Framework/Source/GrowlApplicationBridge.m
@@ -753,62 +753,97 @@ static BOOL		registerWhenGrowlIsReady = NO;
 
 	//Houston, we are go for launch.
 	if (growlHelperAppPath) {
-		//Let's launch in the background (unfortunately, requires Carbon on Jaguar)
-		LSLaunchFSRefSpec spec;
-		FSRef appRef;
-		OSStatus status = FSPathMakeRef((UInt8 *)[growlHelperAppPath fileSystemRepresentation], &appRef, NULL);
-		if (status == noErr) {
-			FSRef regItemRef;
-			BOOL passRegDict = NO;
+		//Let's launch in the background (requires sending the Apple Event ourselves, as LS may activate the application anyway if it's already running)
+		NSURL *appURL = [NSURL fileURLWithPath:growlHelperAppPath];
+		if (appURL) {
+			OSStatus err;
 
-			if (regDict) {
-				OSStatus regStatus;
-				NSString *regDictFileName;
-				NSString *regDictPath;
-
-				//Obtain a truly unique file name
-				CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
-				CFStringRef uuidString = CFUUIDCreateString(kCFAllocatorDefault, uuid);
-				CFRelease(uuid);
-				regDictFileName = [[NSString stringWithFormat:@"%@-%u-%@", [self _applicationNameForGrowlSearchingRegistrationDictionary:regDict], getpid(), (NSString *)uuidString] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
-				CFRelease(uuidString);
-				if ([regDictFileName length] > NAME_MAX)
-					regDictFileName = [[regDictFileName substringToIndex:(NAME_MAX - [GROWL_REG_DICT_EXTENSION length])] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
-
-				//make sure it's within pathname length constraints
-				regDictPath = [NSTemporaryDirectory() stringByAppendingPathComponent:regDictFileName];
-				if ([regDictPath length] > PATH_MAX)
-					regDictPath = [[regDictPath substringToIndex:(PATH_MAX - [GROWL_REG_DICT_EXTENSION length])] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
-
-				//Write the registration dictionary out to the temporary directory
-				NSData *plistData;
-				NSString *error;
-				plistData = [NSPropertyListSerialization dataFromPropertyList:regDict
-																	   format:NSPropertyListBinaryFormat_v1_0
-															 errorDescription:&error];
-				if (plistData) {
-					if (![plistData writeToFile:regDictPath atomically:NO])
-						NSLog(@"GrowlApplicationBridge: Error writing registration dictionary at %@", regDictPath);
-				} else {
-					NSLog(@"GrowlApplicationBridge: Error writing registration dictionary at %@: %@", regDictPath, error);
-					NSLog(@"GrowlApplicationBridge: Registration dictionary follows\n%@", regDict);
-					[error release];
+			//Find the PSN for GrowlHelperApp. (We'll need this later.)
+			struct ProcessSerialNumber appPSN = {
+				0, kNoProcess
+			};
+			while ((err = GetNextProcess(&appPSN)) == noErr) {
+				NSDictionary *dict = [(id)ProcessInformationCopyDictionary(&appPSN, kProcessDictionaryIncludeAllInformationMask) autorelease];
+				NSString *bundlePath = [dict objectForKey:@"BundlePath"];
+				if ([bundlePath isEqualToString:growlHelperAppPath]) {
+					//Match!
+					break;
 				}
-
-				regStatus = FSPathMakeRef((UInt8 *)[regDictPath fileSystemRepresentation], &regItemRef, NULL);
-				if (regStatus == noErr)
-					passRegDict = YES;
 			}
 
-			spec.appRef = &appRef;
-			spec.numDocs = (passRegDict != NO);
-			spec.itemRefs = (passRegDict ? &regItemRef : NULL);
-			spec.passThruParams = NULL;
-			spec.launchFlags = kLSLaunchDontAddToRecents | kLSLaunchDontSwitch | kLSLaunchNoParams | kLSLaunchAsync;
-			spec.asyncRefCon = NULL;
-			status = LSOpenFromRefSpec(&spec, NULL);
+			if (err == noErr) {
+				NSURL *regItemURL = nil;
+				BOOL passRegDict = NO;
 
-			success = (status == noErr);
+				if (regDict) {
+					NSString *regDictFileName;
+					NSString *regDictPath;
+
+					//Obtain a truly unique file name
+					CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+					CFStringRef uuidString = CFUUIDCreateString(kCFAllocatorDefault, uuid);
+					CFRelease(uuid);
+					regDictFileName = [[NSString stringWithFormat:@"%@-%u-%@", [self _applicationNameForGrowlSearchingRegistrationDictionary:regDict], getpid(), (NSString *)uuidString] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
+					CFRelease(uuidString);
+					if ([regDictFileName length] > NAME_MAX)
+						regDictFileName = [[regDictFileName substringToIndex:(NAME_MAX - [GROWL_REG_DICT_EXTENSION length])] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
+
+					//make sure it's within pathname length constraints
+					regDictPath = [NSTemporaryDirectory() stringByAppendingPathComponent:regDictFileName];
+					if ([regDictPath length] > PATH_MAX)
+						regDictPath = [[regDictPath substringToIndex:(PATH_MAX - [GROWL_REG_DICT_EXTENSION length])] stringByAppendingPathExtension:GROWL_REG_DICT_EXTENSION];
+
+					//Write the registration dictionary out to the temporary directory
+					NSData *plistData;
+					NSString *error;
+					plistData = [NSPropertyListSerialization dataFromPropertyList:regDict
+																		   format:NSPropertyListBinaryFormat_v1_0
+																 errorDescription:&error];
+					if (plistData) {
+						if (![plistData writeToFile:regDictPath atomically:NO])
+							NSLog(@"GrowlApplicationBridge: Error writing registration dictionary at %@", regDictPath);
+					} else {
+						NSLog(@"GrowlApplicationBridge: Error writing registration dictionary at %@: %@", regDictPath, error);
+						NSLog(@"GrowlApplicationBridge: Registration dictionary follows\n%@", regDict);
+						[error release];
+					}
+
+					if ([[NSFileManager defaultManager] fileExistsAtPath:regDictPath]) {
+						regItemURL = [NSURL fileURLWithPath:regDictPath];
+						passRegDict = YES;
+					}
+				}
+
+				AEStreamRef stream = AEStreamCreateEvent(kAECoreSuite, kAEOpen,
+					//Target application
+					typeProcessSerialNumber, &appPSN, sizeof(appPSN),
+					kAutoGenerateReturnID, kAnyTransactionID);
+				if (!stream) {
+					NSLog(@"%@: Could not create open-document event to register this application with Growl", [self class]);
+				} else {
+					if (passRegDict) {
+						NSString *regItemURLString = [regItemURL absoluteString];
+						NSData *regItemURLUTF8Data = [regItemURLString dataUsingEncoding:NSUTF8StringEncoding];
+						err = AEStreamWriteKeyDesc(stream, kEventParamDirectObject, typeFileURL, [regItemURLUTF8Data bytes], [regItemURLUTF8Data length]);
+						if (err != noErr) {
+							NSLog(@"%@: Could not set direct object of open-document event to register this application with Growl because AEStreamWriteKeyDesc returned %li/%s", [self class], (long)err, GetMacOSStatusCommentString(err));
+						}
+					}
+
+					AppleEvent event;
+					err = AEStreamClose(stream, &event);
+					if (err != noErr) {
+						NSLog(@"%@: Could not finish open-document event to register this application with Growl because AEStreamClose returned %li/%s", [self class], (long)err, GetMacOSStatusCommentString(err));
+					} else {
+						err = AESend(&event, /*reply*/ NULL, kAENoReply | kAEDontReconnect | kAENeverInteract | kAEDontRecord, kAENormalPriority, kAEDefaultTimeout, /*idleProc*/ NULL, /*filterProc*/ NULL);
+						if (err != noErr) {
+							NSLog(@"%@: Could not send open-document event to register this application with Growl because AESend returned %li/%s", [self class], (long)err, GetMacOSStatusCommentString(err));
+						}
+					}
+					
+					success = (err == noErr);
+				}
+			}
 		}
 	}
 
