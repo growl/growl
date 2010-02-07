c60e5a9b209cb38426e21ebff73430427b393e77
Peter Hosey <boredzo@users.noreply.github.com>
2010-02-07 07:22:08 -0800

    Made the Base64 code much faster (saving 20% of the time off of every WebKit display fade-in) by using -appendCharacter: instead of -appendFormat:. Also declared the character array as const.

--- Plugins/Displays/WebKit/GrowlWebKitWindowController.m
+++ Plugins/Displays/WebKit/GrowlWebKitWindowController.m
@@ -378,7 +378,7 @@
 
 @implementation NSData (Base64Additions)
 
-static char encodingTable[64] = {
+static const unichar encodingTable[64] = {
 	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
 	'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
 	'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
@@ -421,7 +421,7 @@ static char encodingTable[64] = {
 		}
 		
 		for( i = 0; i < ctcopy; i++ )
-			[result appendFormat:@"%c", encodingTable[outbuf[i]]];
+			[result appendCharacter:encodingTable[outbuf[i]]];
 		
 		for( i = ctcopy; i < 4; i++ )
 			[result appendString:@"="];
