30ff48ce5741c417ac27d1a803504b3dbd85fe97
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2010-07-07 23:52:55 -0400

    moving this from the public defines to the private ones for now, we should consider just removing it though

--- Common/Source/GrowlDefines.h
+++ Common/Source/GrowlDefines.h
@@ -185,16 +185,6 @@
  */
 #define GROWL_NOTIFICATION_CLICK_CONTEXT			XSTR("NotificationClickContext")
 
-/*!	@defined GROWL_DISPLAY_PLUGIN
- *	@discussion The name of a display plugin which should be used for this notification.
- *    Optional. If this key is not set or the specified display plugin does not
- *    exist, the display plugin stored in the application ticket is used. This key
- *    allows applications to use different default display plugins for their
- *    notifications. The user can still override those settings in the preference
- *    pane.
- */
-#define GROWL_DISPLAY_PLUGIN				XSTR("NotificationDisplayPlugin")
-
 /*!	@defined GROWL_NOTIFICATION_IDENTIFIER
  *	@abstract An identifier for the notification for coalescing purposes.
  *   Notifications with the same identifier fall into the same class; only
