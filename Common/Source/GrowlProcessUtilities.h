226e2807285548615d0c197d02313ab1b49a0793
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2010-07-10 19:38:44 -0400

    addresses #101

--- Common/Source/GrowlProcessUtilities.h
+++ Common/Source/GrowlProcessUtilities.h
@@ -6,7 +6,7 @@
 //  Copyright 2010 The Growl Project. All rights reserved.
 //
 
-#include "GrowlDefines.h"
+#include "Growl/GrowlDefines.h"
 
 bool Growl_GetPSNForProcessWithBundlePath(STRING_TYPE bundlePath, ProcessSerialNumber *outPSN);
 bool Growl_ProcessExistsWithBundleIdentifier(STRING_TYPE bundleID);
