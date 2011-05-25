08dbe0abbc78c52bc254474ade76aeb5d33dd4e2
Peter Hosey <boredzo@users.noreply.github.com>
2011-05-25 16:53:10 -0700

    Increasing GrowlMail version to 1.2.4.

--- GrowlVersion.h
+++ GrowlVersion.h
@@ -4,8 +4,8 @@
 #define GROWL_VERSION 1.2.2
 #define GROWL_VERSION_STRING "1.2.2"
 
-#define GROWLMAIL_VERSION 1.2.4b1
-#define GROWLMAIL_VERSION_STRING "1.2.4b1"
+#define GROWLMAIL_VERSION 1.2.4
+#define GROWLMAIL_VERSION_STRING "1.2.4"
 
 #include "hgRevision.h"
 
