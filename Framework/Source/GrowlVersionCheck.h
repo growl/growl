77ae4fd52363b5c3c5193a06cb3d872095401b63
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-07-04 18:30:48 -0400

    purge of Growl-WithInstaller

--- Framework/Source/GrowlVersionCheck.h
+++
@@ -1,9 +0,0 @@
-//
-//  GrowlVersionCheck.h
-//  Growl
-//
-//  Created by Peter Hosey on 2009-11-27.
-//  Copyright 2009 Peter Hosey. All rights reserved.
-//
-
-Boolean GrowlCheckOSXVersion(void);
