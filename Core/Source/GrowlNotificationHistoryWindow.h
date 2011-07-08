985d51365c8bf069f634b2fc1628102917ad9007
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2011-07-07 22:14:24 -0500

    Initial rework of history rollup to take advantage of 10.7 features, more changes and improvements to come

--- Core/Source/GrowlNotificationHistoryWindow.h
+++ Core/Source/GrowlNotificationHistoryWindow.h
@@ -15,16 +15,10 @@
    IBOutlet NSTableView *historyTable;
    IBOutlet NSArrayController *arrayController;
    IBOutlet NSTextField *countLabel;
-   IBOutlet NSView *storage;
-   IBOutlet NSView *countView;
-   IBOutlet NSView *searchView;
-   IBOutlet NSTableColumn *dateColumn;
    IBOutlet NSTableColumn *notificationColumn;
-   IBOutlet NSTableHeaderView *headerView;
    GrowlNotificationDatabase *historyController;
    
    NSDate *awayDate;
-   BOOL expanded;
    BOOL currentlyShown;
    NSSize expandSize;
 }
@@ -32,19 +26,12 @@
 @property (assign) IBOutlet NSTableView *historyTable;
 @property (assign) IBOutlet NSArrayController *arrayController;
 @property (assign) IBOutlet NSTextField *countLabel;
-@property (assign) IBOutlet NSView *storage;
-@property (nonatomic, retain) IBOutlet NSView *countView;
-@property (nonatomic, retain) IBOutlet NSView *searchView;
-@property (assign) IBOutlet NSTableColumn *dateColumn;
 @property (assign) IBOutlet NSTableColumn *notificationColumn;
-@property (assign) IBOutlet NSTableHeaderView *headerView;
 
 @property (nonatomic, retain) NSDate *awayDate;
 
--(IBAction)expandWindow:(id)sender;
 -(void)updateTableView:(BOOL)willMerge;
 -(void)resetArrayWithDate:(NSDate*)newAway;
--(void)shrinkWindow;
 -(IBAction)openFullLog:(id)sender;
 
 @end
--- Core/Source/GrowlNotificationHistoryWindow.m
+++ Core/Source/GrowlNotificationHistoryWindow.m
@@ -10,7 +10,6 @@
 #import "GrowlNotificationDatabase.h"
 #import "GrowlHistoryNotification.h"
 #import "GrowlApplicationController.h"
-#import "GrowlHistoryWindowNotificationCell.h"
 #import "GrowlPathUtilities.h"
 
 #define GROWL_ROLLUP_WINDOW_HEIGHT @"GrowlRollupWindowHeight"
@@ -22,30 +21,20 @@
 #define GROWL_ROLLUP_SMALL_WINDOW_WIDTH 400
 #define GROWL_ROLLUP_SMALL_WINDOW_HEIGHT 263
 #define GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT 63
-#define GROWL_ROLLUP_ROW_HEIGHT 37
 
 @implementation GrowlNotificationHistoryWindow
 
 @synthesize historyTable;
 @synthesize arrayController;
 @synthesize countLabel;
-@synthesize storage;
-@synthesize countView;
-@synthesize searchView;
-@synthesize dateColumn;
 @synthesize notificationColumn;
-@synthesize headerView;
 @synthesize awayDate;
 
 -(id)init
 {
    if((self = [super initWithWindowNibName:@"AwayHistoryWindow" owner:self]))
    {
-      expanded = NO;
       currentlyShown = NO;
-      [[self window] setFrame:NSMakeRect(0, 0, GROWL_ROLLUP_SMALL_WINDOW_WIDTH, GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + GROWL_ROLLUP_ROW_HEIGHT) display:YES animate:NO];
-      [[self window] setMaxSize:NSMakeSize(GROWL_ROLLUP_SMALL_WINDOW_WIDTH, GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + GROWL_ROLLUP_ROW_HEIGHT)];
-      [[self window] center];
       [[self window] setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces];
       [(NSPanel*)[self window] setFloatingPanel:YES];
       
@@ -66,10 +55,6 @@
       expandSize = NSMakeSize(width, height);
       
       [historyTable setDoubleAction:@selector(userDoubleClickedNote:)];
-      GrowlHistoryWindowNotificationCell *noteCell = [[[GrowlHistoryWindowNotificationCell alloc] init] autorelease];
-      [notificationColumn setDataCell:noteCell];
-      [historyTable setHeaderView:nil];
-      [historyTable sizeLastColumnToFit];
    }
    return self;
 }
@@ -81,78 +66,13 @@
    historyController = nil;
    
    [awayDate release]; awayDate = nil;
-   [countView release]; countView = nil;
-   [searchView release]; searchView = nil;
    
    [super dealloc];
-}
-
--(IBAction)expandWindow:(id)sender
-{
-   if(expanded)
-      return;
-   
-   [self updateTableView:NO];
-   
-   NSRect temp = [[self window] frame];
-   NSInteger oldX = temp.origin.x;
-   NSInteger oldY = temp.origin.y;
-   NSInteger newX = 0, newY = 0;
-   
-   newX = oldX - (expandSize.width - temp.size.width) / 2;
-   newY = oldY - (expandSize.height - temp.size.height) / 2;
-   
-   [historyTable setHeaderView:headerView];
-   [historyTable addTableColumn:dateColumn];
-   [historyTable sizeLastColumnToFit];
-   
-   [searchView setFrame:[storage frame]];
-   [[storage animator] replaceSubview:countView with:searchView];
-   
-   [[self window] setMaxSize:NSMakeSize(FLT_MAX, FLT_MAX)];
-   [[self window] setFrame:NSMakeRect(newX, newY, expandSize.width, expandSize.height) 
-                  display:YES 
-                  animate:YES];
-   [[self window] setMinSize:NSMakeSize(GROWL_ROLLUP_MIN_WINDOW_WIDTH, GROWL_ROLLUP_MIN_WINDOW_HEIGHT)];
-   expanded = YES;
-}
-
--(void)shrinkWindow
-{
-   if(!expanded)
-      return;
-   
-   [historyTable setHeaderView:nil];
-   [historyTable removeTableColumn:dateColumn];
-   [historyTable sizeLastColumnToFit];
-   expandSize = [[self window] frame].size;
-   [[GrowlPreferencesController sharedController] setInteger:expandSize.height 
-                                                      forKey:GROWL_ROLLUP_WINDOW_HEIGHT];
-   [[GrowlPreferencesController sharedController] setInteger:expandSize.width 
-                                                      forKey:GROWL_ROLLUP_WINDOW_WIDTH];
-   
-   NSRect temp = [[self window] frame];
-   NSInteger oldX = temp.origin.x;
-   NSInteger oldY = temp.origin.y;
-   NSInteger newX = 0, newY = 0;
-   NSInteger rows = [[arrayController arrangedObjects] count];
-   NSInteger heightRows = GROWL_ROLLUP_ROW_HEIGHT * ((rows > 5) ? 5 : rows);
-   
-   newX = oldX + (temp.size.width - GROWL_ROLLUP_SMALL_WINDOW_WIDTH) / 2;
-   newY = oldY + (temp.size.height - (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows)) / 2;
-   
-   [countView setFrame:[storage frame]];
-   [[storage animator] replaceSubview:searchView with:countView];
-   [[self window] setMinSize:NSMakeSize(GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows))];
-   [[self window] setFrame:NSMakeRect(newX, newY, GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows)) display:YES animate:YES];
-   [[self window] setMaxSize:NSMakeSize(GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows))];
-   expanded = NO;
 }
 
 -(void)windowWillClose:(NSNotification *)notification
 {
    [[GrowlNotificationDatabase sharedInstance] userReturnedAndClosedList];
-   [self shrinkWindow];
    currentlyShown = NO;
 }
 
@@ -178,37 +98,13 @@
     
    NSString* description;
    
-   if(numberOfNotifications == 0){
-      /* No sense keeping the window around, likely the user deleted all the notes that happened while away */
-      [[self window] performClose:self];
-      return;
-   } else if(numberOfNotifications == 1){
+   if(numberOfNotifications == 1){
       description = [NSString stringWithFormat:NSLocalizedString(@"There was %d notification while you were away", nil), numberOfNotifications];
    } else {
       description = [NSString stringWithFormat:NSLocalizedString(@"There were %d notifications while you were away", nil), numberOfNotifications];
    }
-   
-   [[countLabel cell] setStringValue:description];
-   
-   if(numberOfNotifications > 5)
-      [(NSScrollView*)[[historyTable superview] superview] setHasVerticalScroller:YES];
-   else 
-      [(NSScrollView*)[[historyTable superview] superview] setHasVerticalScroller:NO];
 
-   if(expanded)
-      return;
-   NSRect temp = [[self window] frame];
-   NSInteger oldY = temp.origin.y;
-   NSInteger newY = 0;
-   
-   NSInteger heightRows = GROWL_ROLLUP_ROW_HEIGHT * ((numberOfNotifications > 5) ? 5 : numberOfNotifications);
-   
-   newY = oldY - ((GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows) - temp.size.height) / 2;
-   
-   [[self window] setMinSize:NSMakeSize(GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows))];
-   [[self window] setFrame:NSMakeRect(temp.origin.x, newY, GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows)) display:YES animate:YES];
-   [[self window] setMaxSize:NSMakeSize(GROWL_ROLLUP_SMALL_WINDOW_WIDTH, (GROWL_ROLLUP_SMALL_WINDOW_BASE_HEIGHT + heightRows))];
-   expanded = NO;
+    [countLabel setObjectValue:description];
 }
 
 -(void)resetArrayWithDate:(NSDate*)newAway
@@ -250,8 +146,7 @@
 
 - (id) tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex {
    if(aTableColumn == notificationColumn){
-      GrowlHistoryWindowNotificationCell *cell = (GrowlHistoryWindowNotificationCell*)[aTableColumn dataCellForRow:rowIndex];
-      [cell setNote:[[arrayController arrangedObjects] objectAtIndex:rowIndex]];
+      return [[arrayController arrangedObjects] objectAtIndex:rowIndex];
    }
 	return nil;
 }
