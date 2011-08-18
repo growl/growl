aa2b03cc44ac4cfc711b4130c284945af1ee5c95
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-08-17 20:14:59 -0400

    Fixes up AppleScript image support, removes the pictImage parameter

---
+++ Core/Source/GrowlAppleScriptSupport.h
@@ -0,0 +1,13 @@
+//
+//  GrowlAppleScriptSupport.h
+//  Growl
+//
+//  Created by Rudy Richter on 8/17/11.
+//  Copyright 2011 The Growl Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (GrowlAppleScriptSupport)
+
+@end
