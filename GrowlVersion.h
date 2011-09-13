b2c24550b0de2d6c30e5a204fe23721275f29a58
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-09-12 21:27:39 -0400

    bump to 1.3.0b2

--- GrowlVersion.h
+++ GrowlVersion.h
@@ -1,8 +1,8 @@
 #define COMMENT_1 DO NOT EDIT THE VERSION IN THIS FILE! Change the version in Release/Makefile instead, then run make version while in the Release directory.
 #define COMMENT_2 In case you're wondering why we're using #define for comments: This header is #imported into both Objective-C code and XML Info.plist files.
 
-#define GROWL_VERSION 1.3.0b1
-#define GROWL_VERSION_STRING "1.3.0b1"
+#define GROWL_VERSION 1.3.0b2
+#define GROWL_VERSION_STRING "1.3.0b2"
 
 #include "hgRevision.h"
 
