3fa5da1be61134c368b9b209836a889c8ad3bb77
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-09-15 00:54:04 -0400

    change to simplify importing after creating the references in xcode

--- scripts/import-localizations.rb
+++ scripts/import-localizations.rb
@@ -18,51 +18,51 @@ end
 
 def import(path, language)
     srcPath = path+"/Contents/Library/Automator/Show Growl Notification.action/Contents/Resources/"+language
-    destPath = "../Plugins/System/GrowlAction/"+language
+    destPath = "../Plugins/System/GrowlAction/"
     FileUtils.cp_r(srcPath, destPath)
     
     srcPath = path+"/Contents/PlugIns/MailMe.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/MailMe/"+language
+    destPath = "../Plugins/Displays/MailMe/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Speech.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Speech/"+language
+    destPath = "../Plugins/Displays/Speech/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/SMS.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/SMS/"+language
+    destPath = "../Plugins/Displays/SMS/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/iCal.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/iCal/"+language
+    destPath = "../Plugins/Displays/iCal/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Brushed.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Brushed/"+language
+    destPath = "../Plugins/Displays/Brushed/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/MusicVideo.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/MusicVideo/"+language
+    destPath = "../Plugins/Displays/MusicVideo/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Nano.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Nano/"+language
+    destPath = "../Plugins/Displays/Nano/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Smoke.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Smoke/"+language
+    destPath = "../Plugins/Displays/Smoke/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Bubbles.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Bubbles/"+language
+    destPath = "../Plugins/Displays/Bubbles/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/PlugIns/Bezel.growlView/Contents/Resources/"+language
-    destPath = "../Plugins/Displays/Bezel/"+language
+    destPath = "../Plugins/Displays/Bezel/"
     FileUtils.cp_r(srcPath, destPath)
 
     srcPath = path+"/Contents/Resources/"+language
-    destPath = "../Core/Resources/"+language
+    destPath = "../Core/Resources/"
     FileUtils.cp_r(srcPath, destPath)
     
 end
