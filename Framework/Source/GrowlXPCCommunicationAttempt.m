1671bcd5776d2e3c46c52cfc9ed54a861b3a6714
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2011-09-15 09:25:31 -0500

    No need to have two methods building the same thing, if handler = nil, just send message.

--- Framework/Source/GrowlXPCCommunicationAttempt.m
+++ Framework/Source/GrowlXPCCommunicationAttempt.m
@@ -89,8 +89,11 @@
     xpc_dictionary_set_value(xpcMessage, "GrowlDict", growlDict);
     xpc_release(growlDict);
     
-    xpc_connection_send_message_with_reply(xpcConnection, xpcMessage, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT ,0), handler);
-    
+    if (handler) {
+       xpc_connection_send_message_with_reply(xpcConnection, xpcMessage, dispatch_get_main_queue(), handler);
+    }else{
+       xpc_connection_send_message(xpcConnection, xpcMessage);
+    }
     xpc_release(xpcMessage);
     
     return YES;
@@ -98,45 +101,7 @@
 
 - (BOOL) sendMessageWithPurpose:(NSString *)purpose
 {
-    if (!xpcConnection)
-        return NO;
-    
-    xpc_object_t xpcMessage = xpc_dictionary_create(NULL, NULL, 0);
-    
-    // Add the known parameters, yay!
-    xpc_dictionary_set_string(xpcMessage, "growlMessagePurpose", [purpose UTF8String]);
-    
-    // And now we start building the message.
-    for (NSString *key in [self.dictionary allKeys]) {
-        id keyValue = [self.dictionary objectForKey:key];
-        
-        if ([keyValue isKindOfClass:[NSString class]]) {
-            NSString *keyString = (NSString *)keyValue;
-            xpc_dictionary_set_string(xpcMessage, [key UTF8String], [keyString UTF8String]);
-        }
-        else if ([keyValue isKindOfClass:[NSData class]]) {
-            NSData *keyData = (NSData *)keyValue;
-            xpc_dictionary_set_data(xpcMessage, [key UTF8String], [keyData bytes], [keyData length]);
-        }
-        else if ([keyValue isKindOfClass:[NSNumber class]]) {
-            // Okay, we need to get a little clever here, since I can't tell if this is a bool or no.
-            // At least, not without checking what this parameter is.
-            
-            NSNumber *keyNumber = (NSNumber *)keyValue;
-            if ([key isEqualToString:GROWL_NOTIFICATION_STICKY]) {
-                xpc_dictionary_set_bool(xpcMessage, [key UTF8String], [keyNumber boolValue]);
-            }
-            else {
-                xpc_dictionary_set_int64(xpcMessage, [key UTF8String], [keyNumber integerValue]);
-            }
-        }
-    }
-    
-    xpc_connection_send_message(xpcConnection, xpcMessage);
-    
-    xpc_release(xpcMessage);
-    
-    return YES;
+    return [self sendMessageWithPurpose:purpose andReplyHandler:nil];
 }
 
 @end
