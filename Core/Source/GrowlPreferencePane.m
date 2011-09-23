acff0926079efa8d63b2decbcaaa367de49d0aef
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-09-23 00:02:17 -0400

    addresses the issue chris found where you fail to get the display style chosen at the application level if you set the display style at the notification level to default

--- Core/Source/GrowlPreferencePane.m
+++ Core/Source/GrowlPreferencePane.m
@@ -691,8 +691,18 @@ static void scCallback(SCDynamicStoreRef store, CFArrayRef changedKeys, void *in
 	}
 
 	if (!pluginName)
-		pluginName = [pluginToUse objectForKey:GrowlPluginInfoKeyName];
-			
+	{
+        //fall back to the application's plugin name
+        
+        NSArray *apps = [ticketsArrayController selectedObjects];
+        if(apps && [apps count])
+        {
+            NSDictionary *parentApp = [apps objectAtIndex:0U];
+            pluginName = [parentApp valueForKey:@"displayPluginName"];
+        }
+        if(!pluginName)
+            pluginName = [pluginToUse objectForKey:GrowlPluginInfoKeyName];
+	}		
 	[[NSNotificationCenter defaultCenter] postNotificationName:GrowlPreview
 																   object:pluginName];
 }
