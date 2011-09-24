d9795c8a3027bbcc34b59a930506f535c2203825
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2011-09-23 23:49:12 -0400

    resolves two separate crashes, one if you try and double click a growlTicket as a result of it thinking its a plugin. and one if you double click a growlStyle that's already installed and tell it that you don't want to replace the already installed style

--- Core/Source/GrowlPluginController.m
+++ Core/Source/GrowlPluginController.m
@@ -874,7 +874,7 @@ NSString *GrowlPluginInfoKeyInstance          = @"GrowlPluginInstance";
 		stringByAppendingPathComponent:@"Library/Application Support/Growl/Plugins"]
 		stringByAppendingPathComponent:pluginFile];
 	// retain a copy of the filename because it is passed as context to the sheetDidEnd selectors
-	NSString *filenameCopy = [[[NSString alloc] initWithString:filename] autorelease];
+	NSString *filenameCopy = [[NSString alloc] initWithString:filename];
 
 	//Check to see if we've got valid architectures in this plugin for our use, if not, bail.
 	if(![self hasNativeArchitecture:filenameCopy]) {
