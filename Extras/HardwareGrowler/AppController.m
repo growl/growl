d7ca74fcde079c030ed0f281415d1b925623054f
Vasileios Georgitzikis <billgeo13@gmail.com>
2011-10-05 00:26:38 +0300

    Made the necessary changes for localization

--- Extras/HardwareGrowler/AppController.m
+++ Extras/HardwareGrowler/AppController.m
@@ -759,7 +759,7 @@ static void powerCallback(void *refcon, io_service_t service, natural_t messageT
 - (IBAction)moveToStatusbar:(id)sender{
 	[[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"HideDockIcon"];
 	NSAlert *alert = [[[NSAlert alloc] init] autorelease];
-	[alert setMessageText:@"This setting will take effect when Hardware Growler restarts"];
+	[alert setMessageText:NSLocalizedString(@"This setting will take effect when Hardware Growler restarts",nil)];
 	[alert runModal];
 
 }
