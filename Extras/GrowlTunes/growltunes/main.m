759a0f0d1dd41d3624e727ed8d5c539dfeb47cab
Travis Tilley <ttilley@gmail.com>
2011-11-19 00:15:02 -0500

    SPLowVerbosity 4tw?

--- Extras/GrowlTunes/growltunes/main.m
+++ Extras/GrowlTunes/growltunes/main.m
@@ -1,6 +1,17 @@
 #import <Cocoa/Cocoa.h>
+#import "LoggerClient.h"
 
 int main(int argc, char *argv[])
 {
+    @autoreleasepool {
+        // configures the default logger, but doesn't start it. this is deferred to the first log message
+        Logger* nsl = LoggerGetDefaultLogger();
+        NSString *bufferPath = [NSHomeDirectory() stringByAppendingPathComponent:@"GrowlTunesTemp.rawnsloggerdata"];
+        LoggerSetBufferFile(nsl, (__bridge CFStringRef)bufferPath);
+        LoggerSetViewerHost(nsl, CFSTR("127.0.0.1"), 50000);
+        LoggerSetOptions(nsl, kLoggerOption_BufferLogsUntilConnection |
+                            kLoggerOption_UseSSL | kLoggerOption_BrowseBonjour);
+    }
+    
     return NSApplicationMain(argc, (const char **)argv);
 }
