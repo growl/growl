ec9a449d1e395251bd3fe630f40aeee5b11a5c46
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2011-12-19 00:38:47 -0600

    Start of GrowlCal

---
+++ Extras/GrowlCal/GrowlCal/GrowlCalAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GrowlCalAppDelegate.h
+//  GrowlCal
+//
+//  Created by Daniel Siemer on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GrowlCalAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Extras/GrowlCal/GrowlCal/GrowlCalAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  GrowlCalAppDelegate.m
+//  GrowlCal
+//
+//  Created by Daniel Siemer on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GrowlCalAppDelegate.h"
+
+@implementation GrowlCalAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+   // Insert code here to initialize your application
+}
+
+@end
