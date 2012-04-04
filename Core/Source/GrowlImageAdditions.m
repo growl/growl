d35aaf9a9a8dbc8cd7848e13b9ad7a5d7f61ba32
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-04-04 10:44:32 -0500

    Tweak this a bit, don't leak the destination ref.

--- Core/Source/GrowlImageAdditions.m
+++ Core/Source/GrowlImageAdditions.m
@@ -99,13 +99,16 @@
 - (NSData *) representationWithType:(NSString*)type {
 	NSMutableData *mutableData = [NSMutableData data];
 	CGImageDestinationRef cgDestRef = CGImageDestinationCreateWithData((CFMutableDataRef)mutableData, (CFStringRef)type, 1, NULL);
-	if(cgDestRef == NULL){
-		NSLog(@"Error creating CGImageDestinationRef");
-		return nil;
+	if(cgDestRef)
+	{
+		CGImageRef imageRef = [self CGImageForProposedRect:NULL context:nil hints:nil];
+		if(imageRef)
+		{
+			CGImageDestinationAddImage(cgDestRef, imageRef, NULL);
+			CGImageDestinationFinalize(cgDestRef);
+		}
+		CFRelease(cgDestRef);
 	}
-	CGImageRef imageRef = [self CGImageForProposedRect:NULL context:nil hints:nil];
-	CGImageDestinationAddImage(cgDestRef, imageRef, NULL);
-	CGImageDestinationFinalize(cgDestRef);
 	return mutableData;
 }
 
