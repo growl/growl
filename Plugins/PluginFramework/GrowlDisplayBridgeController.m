0276a9b3719c42af198512ddaf89d8b74a69b550
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-04-06 09:44:33 -0500

    Support addition and removal of screens.

--- Plugins/PluginFramework/GrowlDisplayBridgeController.m
+++ Plugins/PluginFramework/GrowlDisplayBridgeController.m
@@ -17,10 +17,11 @@
 
 @property (nonatomic, retain) NSMutableSet *pending;
 @property (nonatomic, retain) NSMutableSet *allWindows;
-@property (nonatomic, retain) NSMutableArray *displayedBridges;
-@property (nonatomic, retain) NSMutableArray *bridgeQueue;
+@property (nonatomic, retain) NSMutableSet *displayedWindows;
+@property (nonatomic, retain) NSMutableArray *windowQueue;
+@property (nonatomic, retain) NSMutableDictionary *windowsByDisplayID;
 
-@property (nonatomic, retain) NSMutableArray *positionControllers;
+@property (nonatomic, retain) NSMutableDictionary *positionControllers;
 
 @end
 
@@ -28,8 +29,9 @@
 
 @synthesize pending;
 @synthesize allWindows;
-@synthesize displayedBridges;
-@synthesize bridgeQueue;
+@synthesize displayedWindows;
+@synthesize windowQueue;
+@synthesize windowsByDisplayID;
 @synthesize positionControllers;
 
 +(GrowlDisplayBridgeController*)sharedController {
@@ -45,37 +47,55 @@
 	if((self = [super init])){
 		self.pending = [NSMutableSet set];
 		self.allWindows = [NSMutableSet set];
-		self.displayedBridges = [NSMutableArray array];
-		self.bridgeQueue = [NSMutableArray array];
-		self.positionControllers = [NSMutableArray arrayWithCapacity:[[NSScreen screens] count]];
+		self.displayedWindows = [NSMutableSet set];
+		self.windowQueue = [NSMutableArray array];
+		self.positionControllers = [NSMutableDictionary dictionaryWithCapacity:[[NSScreen screens] count]];
+		self.windowsByDisplayID = [NSMutableDictionary dictionaryWithCapacity:[[NSScreen screens] count]];
 		
 		__block GrowlDisplayBridgeController *blockSelf = self;
 		//Generate a position controller for each display
 		[[NSScreen screens] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-			GrowlPositionController *positionController = [[GrowlPositionController alloc] initWithScreenFrame:[obj visibleFrame]];
-			NSUInteger screenID = [blockSelf deviceIDForScreen:obj];
-			NSLog(@"%lu", screenID);
-			[positionController setDeviceID:screenID];
-			[blockSelf.positionControllers addObject:positionController];
-			[positionController release];
+			[blockSelf addPositionControllersForScreen:obj];
 		}];
 		
 		void (^screenChangeBlock)(NSNotification*) = ^(NSNotification *note){
 			NSArray *screens = [NSScreen screens];
-			if([screens count] != [blockSelf.positionControllers count]){				
-				return;
-			}
+			__block NSMutableArray *newIDs = [NSMutableArray array];
+			[screens enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+				NSUInteger screenID = [blockSelf deviceIDForScreen:obj];
+				[newIDs addObject:[NSString stringWithFormat:@"%lu", screenID]];
+			}];
+			
+			__block NSMutableArray *toRemove = [NSMutableArray array];
+			[blockSelf.positionControllers enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+				if(![newIDs containsObject:key])
+					[toRemove addObject:key];
+			}];
+			
+			if([toRemove count]) NSLog(@"Removing %lu positioning controller(s)", [toRemove count]);
+			[[blockSelf.positionControllers dictionaryWithValuesForKeys:toRemove] enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+				NSMutableSet *displayed = [blockSelf.windowsByDisplayID valueForKey:key];
+				[blockSelf.displayedWindows minusSet:displayed];
+				[blockSelf.allWindows minusSet:displayed];
+				[blockSelf.positionControllers removeObjectForKey:key];
+				[blockSelf.windowsByDisplayID removeObjectForKey:key];
+			}];
+			
 			[screens enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
 				CGRect newRect = [obj visibleFrame];
-				GrowlPositionController *controller = [blockSelf.positionControllers objectAtIndex:idx];
-				CGRect currentRect = [controller screenFrame];
-				if(!CGRectEqualToRect(newRect, currentRect))
-				{
-					if([controller isFrameFree:[controller screenFrame]])
-						[controller setScreenFrame:newRect];
-					else{
-						[controller setUpdateFrame:YES];
-						[controller setNewFrame:newRect];
+				GrowlPositionController *controller = [blockSelf positionControllerForScreen:obj];
+				if(!controller){
+					[blockSelf addPositionControllersForScreen:obj];
+				}else{
+					CGRect currentRect = [controller screenFrame];
+					if(!CGRectEqualToRect(newRect, currentRect))
+					{
+						if([controller isFrameFree:[controller screenFrame]])
+							[controller setScreenFrame:newRect];
+						else{
+							[controller setUpdateFrame:YES];
+							[controller setNewFrame:newRect];
+						}
 					}
 				}
 			}];
@@ -93,12 +113,31 @@
 	return [[[screen deviceDescription] valueForKey:@"NSScreenNumber"] unsignedIntegerValue];
 }
 
+-(void)addPositionControllersForScreen:(NSScreen*)screen {
+	GrowlPositionController *controller = [[GrowlPositionController alloc] initWithScreenFrame:[screen visibleFrame]];
+	NSUInteger screenID = [self deviceIDForScreen:screen];
+	NSString *screenIDKey = [NSString stringWithFormat:@"%lu", screenID];
+	NSLog(@"add screen with id %@", screenIDKey);
+
+	[controller setDeviceID:screenID];
+	[positionControllers setValue:controller forKey:screenIDKey];
+	
+	NSMutableSet *controllerSet = [NSMutableSet set];
+	[windowsByDisplayID setValue:controllerSet forKey:screenIDKey];
+	[controller release];
+}
+
+-(GrowlPositionController*)positionControllerForScreenID:(NSUInteger)screenID {
+	return [positionControllers valueForKey:[NSString stringWithFormat:@"%lu", screenID]];
+}
+
+-(GrowlPositionController*)positionControllerForScreen:(NSScreen*)screen {
+	return [self positionControllerForScreenID:[self deviceIDForScreen:screen]];
+}
+
 -(GrowlPositionController*)positionControllerForWindow:(GrowlDisplayWindowController*)window
 {
-	NSUInteger screenNumber = [window screenNumber];
-	if(screenNumber < [positionControllers count])
-		return [positionControllers objectAtIndex:screenNumber];
-	return [positionControllers objectAtIndex:0U];
+	return [self positionControllerForScreen:[window screen]];
 }
 
 -(BOOL)displayWindow:(GrowlDisplayWindowController*)window
@@ -137,38 +176,43 @@
 -(void)displayWindow:(GrowlDisplayWindowController*)window reposition:(BOOL)reposition
 {
 	[allWindows addObject:window];
+	GrowlPositionController *controller = [self positionControllerForWindow:window];
 	if(reposition){
-		GrowlPositionController *controller = [positionControllers objectAtIndex:0U];
 		[self clearRect:[window occupiedRect] inPositionController:controller];
 		if(![self displayWindow:window]){
 			NSLog(@"Couldnt find space for coalescing notification, adding to queue");
 			[window stopDisplay];
-			[displayedBridges removeObject:window];
-			[bridgeQueue addObject:window];
+			[displayedWindows removeObject:window];
+			[windowQueue addObject:window];
 		}
 	}else if([self displayWindow:window]){
 		[window foundSpaceToStart];
-		[displayedBridges addObject:window];
+		[displayedWindows addObject:window];
+		NSMutableSet *controllerSet = [windowsByDisplayID valueForKey:[NSString stringWithFormat:@"%lu", [controller deviceID]]];
+		if(controllerSet) [controllerSet addObject:window];
 	}else{
 		//NSLog(@"putting in queue");
-		[bridgeQueue addObject:window];
+		[windowQueue addObject:window];
 	}
 }
 
 -(void)checkQueuedWindows
 {
 	__block GrowlDisplayBridgeController *blockSelf = self;
-	if([bridgeQueue count]){
+	if([windowQueue count]){
 		dispatch_async(dispatch_get_main_queue(), ^{
 			__block NSMutableArray *found = [NSMutableArray array];
-			[blockSelf.bridgeQueue enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+			[blockSelf.windowQueue enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
 				if([blockSelf displayWindow:obj]){
 					[found addObject:obj];
 					[obj foundSpaceToStart];
-					[[blockSelf displayedBridges] addObject:obj];
+					[[blockSelf displayedWindows] addObject:obj];
+					GrowlPositionController *controller = [self positionControllerForWindow:obj];
+					NSMutableSet *controllerSet = [blockSelf.windowsByDisplayID valueForKey:[NSString stringWithFormat:@"%lu", [controller deviceID]]];
+					if(controllerSet) [controllerSet addObject:obj];
 				}
 			}];
-			[blockSelf.bridgeQueue removeObjectsInArray:found];
+			[blockSelf.windowQueue removeObjectsInArray:found];
 		});
 	}
 }
@@ -183,10 +227,9 @@
 
 -(void)takeDownDisplay:(GrowlDisplayWindowController*)window
 {
-	[displayedBridges removeObject:window];
+	GrowlPositionController *controller = [self positionControllerForWindow:window];
 	if([[window plugin] requiresPositioning]){
 		CGRect clearRect = [window occupiedRect];
-		GrowlPositionController *controller = [self positionControllerForWindow:window];
 		[self clearRect:clearRect inPositionController:controller];
 		
 		[[self class] cancelPreviousPerformRequestsWithTarget:self
@@ -194,7 +237,9 @@
 																	  object:nil];
 		[self performSelector:@selector(checkQueuedWindows) withObject:nil afterDelay:.2];
 	}
-	[displayedBridges removeObject:window];
+	NSMutableSet *controllerSet = [windowsByDisplayID valueForKey:[NSString stringWithFormat:@"%lu", [controller deviceID]]];
+	if(controllerSet) [controllerSet removeObject:window];
+	[displayedWindows removeObject:window];
 	[allWindows removeObject:window];
 }
 
