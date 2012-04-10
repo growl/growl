75ff07edc631f71f2c9c58d02a32e75bf8d86832
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-04-10 10:20:48 -0500

    Add a string encoder that encodes the allowed URL characters for our arguments in Boxcar/Prowl

---
+++ Plugins/PluginFramework/NSURL+StringEncoding.m
@@ -0,0 +1,25 @@
+//
+//  NSURL+StringEncoding.m
+//  Boxcar
+//
+//  Created by Daniel Siemer on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSURL+StringEncoding.h"
+
+@implementation NSURL (StringEncoding)
+
++(NSString*)encodedStringByAddingPercentEscapesToString:(NSString*)string {
+	CFStringRef stringRef = CFURLCreateStringByAddingPercentEscapes(NULL,
+																						 (CFStringRef)string, 
+																						 NULL,
+																						 (CFStringRef)@";/?:@&=+$",
+																						 kCFStringEncodingUTF8);
+	NSString *encodedString = [(NSString*)stringRef copy];
+	CFRelease(stringRef);
+	
+	return [encodedString autorelease];
+}
+
+@end
