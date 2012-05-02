b0143fb5fe007f1515fe9f1faa6ed29ea3a8309a
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-05-02 10:29:33 -0500

    Basics of menu bar/start at login system.  Likely eeds some more testing and tweaking, but the basic framework for an extra is now in place.  On to making HWGrowler do what it does.

--- Extras/HardwareGrowler/HardwareGrowler/AppDelegate.h
+++ Extras/HardwareGrowler/HardwareGrowler/AppDelegate.h
@@ -7,9 +7,42 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <Growl/GrowlApplicationBridge.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@class GrowlOnSwitch;
+
+typedef enum { 
+	kShowIconInMenu = 0,
+	kShowIconInDock = 1,
+	kShowIconInBoth = 2,
+	kDontShowIcon = 3
+} HWGrowlIconState;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, GrowlApplicationBridgeDelegate> {
+	NSStatusItem *statusItem;
+	IBOutlet NSMenu *statusMenu;
+	
+	NSPopUpButton *iconPopUp;
+	GrowlOnSwitch *onLoginSwitch;
+	
+	HWGrowlIconState oldIconValue;
+	BOOL oldOnLoginValue;
+}
+
+@property (nonatomic, retain) NSString *showDevices;
+@property (nonatomic, retain) NSString *groupNetworkTitle;
+@property (nonatomic, retain) NSString *quitTitle;
+@property (nonatomic, retain) NSString *preferencesTitle;
+@property (nonatomic, retain) NSString *openPreferencesTitle;
+@property (nonatomic, retain) NSString *iconTitle;
+@property (nonatomic, retain) NSString *startAtLoginTitle;
+
+@property (nonatomic, retain) NSString *iconInMenu;
+@property (nonatomic, retain) NSString *iconInDock;
+@property (nonatomic, retain) NSString *iconInBoth;
+@property (nonatomic, retain) NSString *noIcon;
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, assign) IBOutlet NSPopUpButton *iconPopUp;
 
 @end
--- Extras/HardwareGrowler/HardwareGrowler/AppDelegate.m
+++ Extras/HardwareGrowler/HardwareGrowler/AppDelegate.m
@@ -7,19 +7,303 @@
 //
 
 #import "AppDelegate.h"
+#import "GrowlOnSwitch.h"
+#import <ServiceManagement/ServiceManagement.h>
+
+#define ShowDevicesTitle     NSLocalizedString(@"Show Connected Devices at Launch", nil)
+#define GroupNetworkTitle    NSLocalizedString(@"Group Network Notifications", nil)
+#define QuitTitle	           NSLocalizedString(@"Quit HardwareGrowler", nil)
+#define PreferencesTitle     NSLocalizedString(@"Preferences...", nil)
+#define OpenPreferencesTitle NSLocalizedString(@"Open HardwareGrowler Preferences...", nil)
+#define IconTitle            NSLocalizedString(@"Icon:", nil)
+#define StartAtLoginTitle    NSLocalizedString(@"Start HardwareGrowler at Login:", nil)
 
 @implementation AppDelegate
 
 @synthesize window = _window;
+@synthesize iconPopUp;
+
+@synthesize showDevices;
+@synthesize groupNetworkTitle;
+@synthesize quitTitle;
+@synthesize preferencesTitle;
+@synthesize openPreferencesTitle;
+@synthesize iconTitle;
+@synthesize startAtLoginTitle;
+
+@synthesize iconInMenu;
+@synthesize iconInDock;
+@synthesize iconInBoth;
+@synthesize noIcon;
 
 - (void)dealloc
 {
     [super dealloc];
 }
 
+- (void) awakeFromNib {
+	self.iconInMenu = NSLocalizedString(@"Show icon in the menubar", @"default option for where the icon should be seen");
+	self.iconInDock = NSLocalizedString(@"Show icon in the dock", @"display the icon only in the dock");
+	self.iconInBoth = NSLocalizedString(@"Show icon in both", @"display the icon in both the menubar and the dock");
+	self.noIcon = NSLocalizedString(@"No icon visible", @"display no icon at all");            
+	
+	[[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
+																				[NSNumber numberWithInteger:1], @"OnLogin",
+																				[NSNumber numberWithBool:YES], @"ShowExisting",
+																				[NSNumber numberWithBool:NO], @"GroupNetwork",
+																				[NSNumber numberWithInteger:0], @"Visibility", nil]];
+	
+	NSNumber *visibility = [[NSUserDefaults standardUserDefaults] objectForKey:@"Visibility"];
+	if(visibility == nil || [visibility integerValue] == kShowIconInDock || [visibility integerValue] == kShowIconInBoth){
+		[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+	}
+	
+	if(visibility == nil || [visibility integerValue] == kShowIconInMenu || [visibility integerValue] == kShowIconInBoth){
+		[self initMenu];
+	}
+	
+	[onLoginSwitch setState:[[[NSUserDefaultsController sharedUserDefaultsController] defaults] boolForKey:@"OnLogin"]];
+   [onLoginSwitch addObserver:self 
+						 forKeyPath:@"state" 
+							 options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld
+							 context:nil];
+}
+
+- (IBAction)showPreferences:(id)sender
+{
+	[NSApp activateIgnoringOtherApps:YES];
+   if(![self.window isVisible]){
+      [self.window center];
+      [self.window setFrameAutosaveName:@"HWGrowlerPrefsWindowFrame"];
+      [self.window setFrameUsingName:@"HWGrowlerPrefsWindowFrame" force:YES];
+   }
+	[self.window makeKeyAndOrderFront:sender];
+}
+
+- (void) initMenu{
+	statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain];
+	[statusItem setMenu:statusMenu];
+	
+	NSString* icon_path = [[NSBundle mainBundle] pathForResource:@"menubarIcon_Normal" ofType:@"png"];
+	NSString* icon_path_selected = [[NSBundle mainBundle] pathForResource:@"menubarIcon_Selected" ofType:@"png"];    
+	NSImage *icon = [[NSImage alloc] initWithContentsOfFile:icon_path];
+	NSImage *icon_selected = [[NSImage alloc] initWithContentsOfFile:icon_path_selected];
+	
+	[statusItem setImage:icon];
+	[statusItem setAlternateImage:icon_selected];
+	[icon release];
+	[icon_selected release];
+	
+	[statusItem setHighlightMode:YES];
+	
+}
+
+- (void) initTitles{
+	self.showDevices = ShowDevicesTitle;
+	self.groupNetworkTitle = GroupNetworkTitle;
+	self.quitTitle = QuitTitle;
+	self.preferencesTitle = PreferencesTitle;
+	self.openPreferencesTitle = OpenPreferencesTitle;
+	self.iconTitle = IconTitle;
+	self.startAtLoginTitle = StartAtLoginTitle;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	// Insert code here to initialize your application
+	[self expiryCheck];
+		
+	[GrowlApplicationBridge setGrowlDelegate:self];
+	[GrowlApplicationBridge setShouldUseBuiltInNotifications:YES];
+		
+	[self initTitles];
+	
+	[[NSUserDefaultsController sharedUserDefaultsController] addObserver:self 
+																				 forKeyPath:@"values.Visibility" 
+																					 options:NSKeyValueObservingOptionNew 
+																					 context:nil];
+	[[NSUserDefaultsController sharedUserDefaultsController] addObserver:self 
+																				 forKeyPath:@"values.OnLogin" 
+																					 options:NSKeyValueObservingOptionNew 
+																					 context:nil];
+	oldIconValue = [[[NSUserDefaultsController sharedUserDefaultsController] defaults] integerForKey:@"Visibility"];
+	oldOnLoginValue = [[[NSUserDefaultsController sharedUserDefaultsController] defaults] boolForKey:@"OnLogin"];
 }
+
+- (BOOL) applicationShouldHandleReopen:(NSApplication *)theApplication hasVisibleWindows:(BOOL)flag {
+	[self showPreferences:self];
+	return YES;
+}
+
+- (void)observeValueForKeyPath:(NSString*)keyPath 
+							 ofObject:(id)object 
+								change:(NSDictionary*)change 
+							  context:(void*)context
+{
+	if([keyPath isEqualToString:@"values.Visibility"])
+	{
+		
+		NSNumber *value = [[[NSUserDefaultsController sharedUserDefaultsController] defaults] valueForKey:@"Visibility"];
+		HWGrowlIconState index   = [value integerValue];
+		switch (index) {
+			case kDontShowIcon:
+				[NSApp activateIgnoringOtherApps:YES];
+				NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Warning! Enabling this option will cause HardwareGrowler to run in the background", nil)
+															defaultButton:NSLocalizedString(@"Ok", nil)
+														 alternateButton:NSLocalizedString(@"Cancel", nil)
+															  otherButton:nil
+											informativeTextWithFormat:NSLocalizedString(@"Enabling this option will cause HardwareGrowler to run without showing a dock icon or a menu item.\n\nTo access preferences, tap HardwareGrowler in Launchpad, or open HardwareGrowler in Finder.", nil)];
+				NSInteger allow = [alert runModal];
+				if(allow == NSAlertDefaultReturn)
+				{
+					[self warnUserAboutIcons];
+					[[NSStatusBar systemStatusBar] removeStatusItem:statusItem];
+					[statusItem release];
+					statusItem = nil;
+				}
+				else
+				{
+					[[[NSUserDefaultsController sharedUserDefaultsController] defaults] setInteger:oldIconValue forKey:@"Visibility"];
+					[[[NSUserDefaultsController sharedUserDefaultsController] defaults] synchronize];
+					[iconPopUp selectItemAtIndex:oldIconValue];
+				}
+				break;
+			case kShowIconInBoth:
+				[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+				if(!statusItem)
+					[self initMenu];
+				break;
+			case kShowIconInDock:
+				[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+				[[NSStatusBar systemStatusBar] removeStatusItem:statusItem];
+				[statusItem release];
+				statusItem = nil;
+				break;
+			case kShowIconInMenu:
+			default:
+				if(!statusItem)
+					[self initMenu];
+				if(oldIconValue == kShowIconInBoth || oldIconValue == kShowIconInDock)
+					[self warnUserAboutIcons];
+				break;
+		}
+		oldIconValue = index;
+	}
+	else if ([keyPath isEqualToString:@"values.OnLogin"])
+	{
+		BOOL state = [[[NSUserDefaultsController sharedUserDefaultsController] defaults] boolForKey:@"OnLogin"];
+		if(!state && (oldOnLoginValue != state))
+		{
+			[NSApp activateIgnoringOtherApps:YES];
+			NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Alert! Enabling this option will add HardwareGrowler to your login items", nil)
+														defaultButton:NSLocalizedString(@"Ok", nil)
+													 alternateButton:NSLocalizedString(@"Cancel", nil)
+														  otherButton:nil
+										informativeTextWithFormat:NSLocalizedString(@"Allowing this will let HardwareGrowler launch everytime you login, so that it is available for applications which use it at all times", nil)];
+			NSInteger allow = [alert runModal];
+			if(allow == NSAlertDefaultReturn)
+			{
+				[self setStartAtLogin:[[NSBundle mainBundle] bundlePath] enabled:YES];
+			}
+			else
+			{
+				[self setStartAtLogin:[[NSBundle mainBundle] bundlePath] enabled:NO];
+				[[[NSUserDefaultsController sharedUserDefaultsController] defaults] setBool:oldOnLoginValue forKey:@"OnLogin"];
+				[[[NSUserDefaultsController sharedUserDefaultsController] defaults] synchronize];
+				[onLoginSwitch setState:oldOnLoginValue];
+			}
+		}
+		else
+			[self setStartAtLogin:[[NSBundle mainBundle] bundlePath] enabled:NO];
+		
+		oldOnLoginValue = state;
+	}
+	else if(object == onLoginSwitch && [keyPath isEqualToString:@"state"])
+	{
+		[[[NSUserDefaultsController sharedUserDefaultsController] defaults] setBool:[(GrowlOnSwitch*)object state] forKey:@"OnLogin"];
+		[[[NSUserDefaultsController sharedUserDefaultsController] defaults] synchronize];
+	}
+}
+
+- (void)warnUserAboutIcons
+{
+	NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+	[alert setMessageText:NSLocalizedString(@"This setting will take effect when Hardware Growler restarts",nil)];
+	[alert runModal];    
+}
+
+- (void) setStartAtLogin:(NSString *)path enabled:(BOOL)enabled {
+   NSURL *urlOfLoginItem = [[NSURL URLWithString:path] URLByAppendingPathComponent:@"Contents/Library/LoginItems/GrowlLauncher.app"];
+   if(!LSRegisterURL((__bridge CFURLRef)urlOfLoginItem, YES)){
+      //NSLog(@"Failure registering %@ with Launch Services", [urlOfLoginItem description]);
+   }
+   if(!SMLoginItemSetEnabled(CFSTR("com.growl.HardwareGrowlLauncher"), enabled)){
+      //NSLog(@"Failure Setting HardwareGrowlLauncher to %@start at login", flag ? @"" : @"not ");
+   }
+}
+
+#ifdef BETA
+#define DAYSTOEXPIRY 21
+- (NSCalendarDate *)dateWithString:(NSString *)str {
+	str = [str stringByReplacingOccurrencesOfString:@"  " withString:@" "];
+	NSArray *dateParts = [str componentsSeparatedByString:@" "];
+	int month = 1;
+	NSString *monthString = [dateParts objectAtIndex:0];
+	if ([monthString isEqualToString:@"Feb"]) {
+		month = 2;
+	} else if ([monthString isEqualToString:@"Mar"]) {
+		month = 3;
+	} else if ([monthString isEqualToString:@"Apr"]) {
+		month = 4;
+	} else if ([monthString isEqualToString:@"May"]) {
+		month = 5;
+	} else if ([monthString isEqualToString:@"Jun"]) {
+		month = 6;
+	} else if ([monthString isEqualToString:@"Jul"]) {
+		month = 7;
+	} else if ([monthString isEqualToString:@"Aug"]) {
+		month = 8;
+	} else if ([monthString isEqualToString:@"Sep"]) {
+		month = 9;
+	} else if ([monthString isEqualToString:@"Oct"]) {
+		month = 10;
+	} else if ([monthString isEqualToString:@"Nov"]) {
+		month = 11;
+	} else if ([monthString isEqualToString:@"Dec"]) {
+		month = 12;
+	}
+	
+	NSString *dateString = [NSString stringWithFormat:@"%@-%d-%@ 00:00:00 +0000", [dateParts objectAtIndex:2], month, [dateParts objectAtIndex:1]];
+	return [NSCalendarDate dateWithString:dateString];
+}
+
+- (BOOL)expired
+{
+	BOOL result = YES;
+	
+	NSCalendarDate* nowDate = [self dateWithString:[NSString stringWithUTF8String:__DATE__]];
+	NSCalendarDate* expiryDate = [nowDate dateByAddingTimeInterval:(60*60*24* DAYSTOEXPIRY)];
+	
+	if ([expiryDate earlierDate:[NSDate date]] != expiryDate)
+		result = NO;
+	
+	return result;
+}
+
+- (void)expiryCheck
+{
+	if([self expired])
+	{
+		[NSApp activateIgnoringOtherApps:YES];
+		NSInteger alert = NSRunAlertPanel(@"This Beta Has Expired", [NSString stringWithFormat:@"Please download a new version to keep using %@.", [[NSProcessInfo processInfo] processName]], @"Quit", nil, nil);
+		if (alert == NSOKButton) 
+		{
+			[NSApp terminate:self];
+		}
+	}
+}
+#else
+- (void)expiryCheck{
+}
+#endif
 
 @end
