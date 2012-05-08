51f7aa8cd6347b5943d3bd74b94fc9467ffb2166
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-05-08 09:30:54 -0500

    Rework NetworkMonitor to not require GrowlNetworkObserver, since it is meant only for IP observing in growl for address data cache removal, and NetworkMonitor is going to need a whole lot more.

--- Extras/HardwareGrowler/NetworkMonitor/HWGrowlNetworkMonitor.m
+++ Extras/HardwareGrowler/NetworkMonitor/HWGrowlNetworkMonitor.m
@@ -7,38 +7,118 @@
 //
 
 #import "HWGrowlNetworkMonitor.h"
-#import "GrowlNetworkObserver.h"
+#import "GrowlNetworkUtilities.h"
+#import <SystemConfiguration/SystemConfiguration.h>
 
 @interface HWGrowlNetworkMonitor ()
 
 @property (nonatomic, assign) id<HWGrowlPluginControllerProtocol> delegate;
+
+@property (nonatomic, assign) SCDynamicStoreRef dynStore;
+@property (nonatomic, assign) CFRunLoopSourceRef rlSrc;
 
 @end
 
 @implementation HWGrowlNetworkMonitor
 
 @synthesize delegate;
+@synthesize rlSrc;
+@synthesize dynStore;
 
 -(id)init {
 	if((self = [super init])){
-		[[GrowlNetworkObserver sharedObserver] startObserving];
-		[[NSNotificationCenter defaultCenter] addObserver:self 
-															  selector:@selector(updateIP:) 
-																	name:IPAddressesUpdateNotification 
-																 object:nil];
+		[self startObserving];
 	}
 	return self;
 }
 
--(void)updateIP:(NSNotification*)note {
+-(void)dealloc {
+	if (rlSrc)
+		CFRunLoopRemoveSource(CFRunLoopGetMain(), rlSrc, kCFRunLoopDefaultMode);
+   if (dynStore)
+		CFRelease(dynStore);
+	[super dealloc];
+}
+
+-(void)fireOnLaunchNotes {
+	[self networkChanged];
+}
+
+-(void)setupDynamicStore
+{
+   if(dynStore != NULL)
+      return;
+   
+   SCDynamicStoreContext context = {0, self, NULL, NULL, NULL};
+   
+	dynStore = SCDynamicStoreCreate(kCFAllocatorDefault,
+                                   CFBundleGetIdentifier(CFBundleGetMainBundle()),
+                                   scCallback,
+                                   &context);
+	if (!dynStore) {
+		NSLog(@"SCDynamicStoreCreate() failed: %s", SCErrorString(SCError()));
+	}
+   
+   rlSrc = SCDynamicStoreCreateRunLoopSource(kCFAllocatorDefault, dynStore, 0);
+	CFRunLoopAddSource(CFRunLoopGetMain(), rlSrc, kCFRunLoopDefaultMode);
+   CFRelease(rlSrc);
+}
+
+-(void)startObserving
+{
+   [self setupDynamicStore];
+	
+   const CFStringRef keys[1] = {
+		CFSTR("State:/Network/Interface/*"),
+	};
+	CFArrayRef watchedKeys = CFArrayCreate(kCFAllocatorDefault,
+                                          (const void **)keys,
+                                          1,
+                                          &kCFTypeArrayCallBacks);
+	if (!SCDynamicStoreSetNotificationKeys(dynStore,
+                                          NULL,
+                                          watchedKeys)) 
+   {
+		NSLog(@"SCDynamicStoreSetNotificationKeys() failed: %s", SCErrorString(SCError()));
+		CFRelease(dynStore);
+		dynStore = NULL;
+	}
+	CFRelease(watchedKeys);
+}
+
+-(void)networkChanged {
+	[self updateInterfaces];
+	[self updateIP];
+}
+
+-(void)updateInterfaces {
+	
+}
+
+-(void)updateIP {
+	NSArray *routable = [GrowlNetworkUtilities routableIPAddresses];
+	NSString *combined = [routable componentsJoinedByString:@"\n"];
+	if([combined isEqualToString:@""])
+		combined = nil;
+	
 	[delegate notifyWithName:@"IPAddressChange"
-							 title:@"Changed IP!"
-					 description:[[GrowlNetworkObserver sharedObserver] routableCombined] ? [[GrowlNetworkObserver sharedObserver] routableCombined] : @"NO IP"
+							 title:@"IP Addresses Updated"
+					 description:combined ? combined : @"No routable IP addresses"
 							  icon:nil
 			  identifierString:@"HWGrowlIPAddressChange"
-				  contextString:[[GrowlNetworkObserver sharedObserver] primaryIP] ? [[GrowlNetworkObserver sharedObserver] primaryIP] : @"NO IP"
+				  contextString:nil
 							plugin:self];
+}
 
+static void scCallback(SCDynamicStoreRef store, CFArrayRef changedKeys, void *info) {
+	HWGrowlNetworkMonitor *observer = info;
+	CFIndex count = CFArrayGetCount(changedKeys);
+	for (CFIndex i=0; i<count; ++i) {
+		CFStringRef key = CFArrayGetValueAtIndex(changedKeys, i);
+      if (CFStringCompare(key, CFSTR("State:/Network/Interface"), 0) == kCFCompareEqualTo) {
+			[observer networkChanged];
+		}
+	}
 }
 
 #pragma mark HWGrowlPluginProtocol
@@ -69,10 +149,6 @@
 }
 -(NSArray*)defaultNotifications {
 	return [NSArray arrayWithObject:@"IPAddressChange"];
-}
-
--(void)noteClosed:(NSString*)contextString byClick:(BOOL)clicked {
-	NSLog(@"testing! %@ was %@", contextString, clicked ? @"clicked" : @"timed out/closed");
 }
 
 @end
