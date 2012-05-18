0302ef4e2ea3d13fd4ab1a7c91febea99d982215
Chris Forsythe <1464091+ChrisForsythe@users.noreply.github.com>
2012-05-17 22:43:31 -0500

    1.4b9

--- GrowlVersion.h
+++ GrowlVersion.h
@@ -1,8 +1,8 @@
 #define COMMENT_1 DO NOT EDIT THE VERSION IN THIS FILE!
 #define COMMENT_2 In case you're wondering why we're using #define for comments: This header is #imported into both Objective-C code and XML Info.plist files.
 
-#define GROWL_VERSION 1.4b8
-#define GROWL_VERSION_STRING "1.4b8"
+#define GROWL_VERSION 1.4b9
+#define GROWL_VERSION_STRING "1.4b9"
 
 #include "VCSData.h"
 
