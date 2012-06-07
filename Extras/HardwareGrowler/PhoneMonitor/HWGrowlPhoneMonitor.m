b6cfc77e69973010a3abf8a2ee6198957ea12303
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-06-07 09:56:59 -0500

    Initial attempt at a bluetooth phone monitor, not working, need to talk to apple engineers about what I am doing wrong at WWDC

---
+++ Extras/HardwareGrowler/PhoneMonitor/HWGrowlPhoneMonitor.m
@@ -0,0 +1,186 @@
+//
+//  HWGrowlPhoneMonitor.m
+//  HardwareGrowler
+//
+//  Created by Daniel Siemer on 6/6/12.
+//  Copyright (c) 2012 The Growl Project, LLC. All rights reserved.
+//
+
+#import "HWGrowlPhoneMonitor.h"
+
+@interface HWGrowlPhoneMonitor ()
+
+@property (nonatomic, assign) id<HWGrowlPluginControllerProtocol> delegate;
+
+@property (nonatomic, assign) BOOL starting;
+@property (nonatomic, assign) IOBluetoothUserNotification *connectionNotification;
+
+@property (nonatomic, retain) IOBluetoothHandsFree *phone;
+
+@end
+
+@implementation HWGrowlPhoneMonitor
+
+@synthesize delegate;
+
+@synthesize starting;
+@synthesize connectionNotification;
+
+@synthesize phone;
+
+-(void)dealloc {
+	[connectionNotification unregister];
+	connectionNotification = nil;
+	[super dealloc];
+}
+
+#ifndef NSFoundationVersionNumber10_7
+#define NSFoundationVersionNumber10_7   833.1
+#endif
+#ifndef NSFoundationVersionNumber10_7_3
+#define NSFoundationVersionNumber10_7_3 833.24
+#endif
+-(id)init {
+	if((BOOL)isless(NSFoundationVersionNumber, NSFoundationVersionNumber10_7_3))
+	{
+		NSLog(@"Phone Module does not work on 10.7-10.7.2, please upgrade to 10.7.3");
+		if(![[NSUserDefaults standardUserDefaults] boolForKey:@"SuppressPhoneModuleWarn"]){
+			NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Phone Module requires OSX Lion 10.7.3 or above", @"")
+														defaultButton:NSLocalizedString(@"Ok", @"") 
+													 alternateButton:nil
+														  otherButton:nil
+										informativeTextWithFormat:NSLocalizedString(@"In order to receive notifications about Phone Devices, please upgrade to OSX Lion 10.7.3 or above", @"")];
+			alert.showsSuppressionButton = YES;
+			[[alert suppressionButton] bind:NSValueBinding
+										  toObject:[NSUserDefaultsController sharedUserDefaultsController]
+									  withKeyPath:@"values.SuppressPhoneModuleWarn"
+											options:nil];
+			[alert runModal];
+		}
+		[self release];
+		return nil;
+	}
+	
+	if((self = [super init])){
+		
+	}
+	return self;
+}
+
+-(void)postRegistrationInit {
+	self.starting = YES;
+	self.connectionNotification = [IOBluetoothDevice registerForConnectNotifications:self 
+																									selector:@selector(bluetoothConnection:device:)];
+	self.starting = NO;
+}
+
+-(void)bluetoothDisconnection:(IOBluetoothUserNotification*)note 
+							  device:(IOBluetoothDevice*)device
+{
+	NSLog(@"disconnected");
+	self.phone = nil;
+	[note	unregister];
+}
+
+-(void)handsFree:(IOBluetoothHandsFreeDevice *)device 
+		currentCall:(NSDictionary *)currentCall
+{
+	NSLog(@"Call %@", currentCall);	
+}
+
+-(void)handsFree:(IOBluetoothHandsFreeDevice *)device 
+		incomingSMS:(NSDictionary *)sms
+{
+	NSLog(@"SMS %@", sms);
+}
+
+- (void)handsFree:(IOBluetoothHandsFree *)device connected:(NSNumber *)status {
+	
+}
+- (void)handsFree:(IOBluetoothHandsFree *)device disconnected:(NSNumber *)status {
+	
+}
+- (void)handsFree:(IOBluetoothHandsFree *)device scoConnectionOpened:(NSNumber *)status {
+	
+}
+- (void)handsFree:(IOBluetoothHandsFree *)device scoConnectionClosed:(NSNumber *)status {
+	
+}
+
+-(void)bluetoothConnection:(IOBluetoothUserNotification*)note 
+						  device:(IOBluetoothDevice*)device 
+{
+	if(device.isHandsFreeAudioGateway){
+		NSLog(@"%@", [device name]);
+		if(IOBluetoothLaunchHandsFreeAgent([device addressString]))
+			NSLog(@"agent launched?");
+		
+		NSDictionary *scoDict = [NSDictionary dictionaryWithObject:[NSNumber numberWithBool:YES]
+																			 forKey:@"Autoconfig hidden"];
+		IOReturn result = IOBluetoothAddSCOAudioDevice((IOBluetoothDeviceRef)[device getDeviceRef], (CFDictionaryRef)scoDict); 
+		if (result != kIOReturnSuccess)
+		{
+			NSLog(@"error 0x%x, trying removing and readding", result);
+			result = IOBluetoothRemoveSCOAudioDevice((IOBluetoothDeviceRef)device);
+			NSLog(@"remove result 0x%x", result);
+			result = IOBluetoothAddSCOAudioDevice([device getDeviceRef], (CFDictionaryRef)scoDict);
+			if (result != kIOReturnSuccess)
+			{
+				NSLog(@"error adding SCO audio device. 0x%x", result);
+			}
+		}
+		
+		double delayInSeconds = 5.0;
+		dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+		dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+			IOBluetoothHandsFreeDevice *handsFree = [[IOBluetoothHandsFreeDevice alloc] initWithDevice:device 
+																														 delegate:self];
+			if(handsFree)
+				NSLog(@"yay!");
+			[handsFree connect];
+			self.phone = handsFree;
+			[handsFree release];
+		});
+		[device registerForDisconnectNotification:self selector:@selector(bluetoothDisconnection:device:)];
+	}
+}
+
+#pragma mark HWGrowlPluginProtocol
+
+-(void)setDelegate:(id<HWGrowlPluginControllerProtocol>)aDelegate{
+	delegate = aDelegate;
+}
+-(id<HWGrowlPluginControllerProtocol>)delegate {
+	return delegate;
+}
+-(NSString*)pluginDisplayName{
+	return NSLocalizedString(@"Phone Module", @"");
+}
+-(NSImage*)preferenceIcon {
+	static NSImage *_icon = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		//_icon = [[NSImage imageNamed:@"HWGPrefsDrivesVolumes"] retain];
+	});
+	return _icon;
+}
+-(NSView*)preferencePane {
+	return nil;
+}
+
+#pragma mark HWGrowlPluginNotifierProtocol
+
+-(NSArray*)noteNames {
+	return [NSArray arrayWithObjects:@"IncomingPhoneCall", @"IncomingSMS", nil];
+}
+-(NSDictionary*)localizedNames {
+	return [NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Incoming Phone Call", @""), @"IncomingPhoneCall", NSLocalizedString(@"Incoming SMS", @""), @"IncomingSMS", nil];
+}
+-(NSDictionary*)noteDescriptions {
+	return [NSDictionary dictionaryWithObjectsAndKeys:NSLocalizedString(@"Incoming Phone Call", @""), @"IncomingPhoneCall", NSLocalizedString(@"Incoming SMS", @""), @"IncomingSMS", nil];
+}
+-(NSArray*)defaultNotifications {
+	return [NSArray arrayWithObjects:@"IncomingPhoneCall", @"IncomingSMS", nil];
+}
+
+@end
