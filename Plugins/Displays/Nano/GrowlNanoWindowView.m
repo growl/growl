4d4c1df7969900b3ae5a68bce76f23eb10776839
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-07-21 19:50:57 -0500

    Continuing to work on localization, fix build scripts up for Nano and MusicVideo

--- Plugins/Displays/Nano/GrowlNanoWindowView.m
+++ Plugins/Displays/Nano/GrowlNanoWindowView.m
@@ -3,7 +3,7 @@
 //  Display Plugins
 //
 //  Created by Rudy Richter on 12/12/2005.
-//  Copyright 2005Ð2011, The Growl Project. All rights reserved.
+//  Copyright 2005â€“2011, The Growl Project. All rights reserved.
 //
 
 #import "GrowlNanoWindowView.h"
