37bc75c876ffff2405b8748130f635338b917641
Chris Forsythe <1464091+ChrisForsythe@users.noreply.github.com>
2012-07-28 02:03:46 -0500

    Bumping to 2.0b2.

--- Core/Source/GrowlFirstLaunchWindowController.m
+++ Core/Source/GrowlFirstLaunchWindowController.m
@@ -32,7 +32,7 @@ typedef void(^GrowlFirstLaunchAction)(void);
 
 +(BOOL)previousVersionOlder
 {
-   NSString *current = @"2.0b1";//[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+   NSString *current = @"2.0b2";//[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
    NSString *previous = [[GrowlPreferencesController sharedController] objectForKey:LastKnownVersionKey];
    return (!previous || compareVersionStrings(previous, current) == kCFCompareLessThan);
 }
--- GrowlVersion.h
+++ GrowlVersion.h
@@ -1,8 +1,8 @@
 #define COMMENT_1 DO NOT EDIT THE VERSION IN THIS FILE!
 #define COMMENT_2 In case you're wondering why we're using #define for comments: This header is #imported into both Objective-C code and XML Info.plist files.
 
-#define GROWL_VERSION 2.0b1
-#define GROWL_VERSION_STRING "2.0b1"
+#define GROWL_VERSION 2.0b2
+#define GROWL_VERSION_STRING "2.0b2"
 
 #include "VCSData.h"
 
