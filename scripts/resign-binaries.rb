2d86046aa56493f6742c424eb74d8363f453b023
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2012-11-07 21:06:44 -0500

    build_system: changes to make building and signing the releases easier

--- scripts/resign-binaries.rb
+++ scripts/resign-binaries.rb
@@ -3,37 +3,58 @@ require 'find'
 require 'fileutils'
 require 'getoptlong'
 
-# default signer
-signer = "3rd Party Mac Developer Application: The Growl Project, LLC"
-problem = false
-opts = GetoptLong.new(
-		[ "--signing-identity", "-i", GetoptLong::REQUIRED_ARGUMENT ]
-	)
-opts.each do |opt, arg|
-	case opt
-		when "--signing-identity"
-			signer = arg
+def sign(path,signer)
+	problem = false
+	command = ['codesign', '--preserve-metadata=identifier,entitlements,resource-rules,requirements', '-f', '-s', "\""+signer+"\"", "\""+path+"\""]
+	system command.join(' ')
+	status = $?
+	if status.exitstatus != 0
+		puts "Failed to run: #{command}"
+		problem = true
 	end
+	return problem
 end
 
-
-Find.find(ARGV.shift) do |path|
+def resign(path, signer)
+	problem = false
 	if not File.exists? path
 		puts "No such file #{path}"
 		problem = true
 		break
 	end
-	fileCheck = %x{/usr/bin/file -h "#{path}"}
-	if fileCheck.include? "Mach-O universal binary"  or 
-        fileCheck.include? "Mach-O 64-bit bundle" or
-        fileCheck.include? "Mach-O 64-bit dynamically linked shared library"
-        
-		#we found a binary now lets call codesign on it and see if it has the right signer
-		command = "codesign -f -s \"#{signer}\" \"#{path}\" 2>&1"
-        system command
+    
+	Find.find(path) do |subpath|
+		fileCheck = %x{/usr/bin/file -h "#{subpath}"}
+		if fileCheck.include? "Mach-O"
+			#we found a binary now lets call codesign on it and see if it has the right signer
+			problem = sign(subpath, signer)
+		elsif path[-10..-1] == '.framework'
+    		versions = Dir.glob(path+"/Versions/*")
+    		for version in versions
+      			if File.basename(version) != 'Current'       
+      		          problem = resign(version, signer)
+        		end
+ 			end
+		end
 	end
+	return problem
 end
 
-if problem
-	exit(1)
+def main
+    signer = "3rd Party Mac Developer Application: The Growl Project, LLC"
+	path = ARGV[0]
+	opts = GetoptLong.new(
+                          [ "--signing-identity", "-i", GetoptLong::REQUIRED_ARGUMENT ]
+                          )
+	opts.each do |opt, arg|
+        case opt
+            when "--signing-identity"
+			signer = arg
+		end
+	end
+    resign(path, signer)
+end
+
+if __FILE__ == $0
+	main()
 end
