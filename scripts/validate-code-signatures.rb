a274ec3eae7063a966e83912b9169611c42f03df
Rudy Richter <157878+rudyrichter@users.noreply.github.com>
2012-11-08 00:33:40 -0500

    validate the code signature as part of the build

--- scripts/validate-code-signatures.rb
+++ scripts/validate-code-signatures.rb
@@ -3,42 +3,75 @@ require 'find'
 require 'fileutils'
 require 'getoptlong'
 
-# default signer
-signer = "3rd Party Mac Developer Application: The Growl Project, LLC"
-problem = false
-opts = GetoptLong.new(
-		[ "--signing-identity", "-i", GetoptLong::REQUIRED_ARGUMENT ]
-	)
-opts.each do |opt, arg|
-	case opt
-		when "--signing-identity"
-			signer = arg
-	end
-end
 
-
-Find.find(ARGV.shift) do |path|
-	if not File.exists? path
-		puts "No such file #{path}"
-		problem = true
-		break
-	end
-	fileCheck = %x{/usr/bin/file -h "#{path}"}
-	if fileCheck.include? "Mach-O"         
-		#we found a binary now lets call codesign on it and see if it has the right signer
-		codesign_command = IO.popen("codesign -dvvv \"#{path}\" 2>&1")
-		candidate_lines = codesign_command.readlines
-		lines = candidate_lines.select {|v| v =~ /Authority=/}
-		signature = lines[0]
-		if !signature or !signature.include? "#{signer}"
-			actualsigner = signature.split("=")[1] if signature
-			puts "Invalid signature on #{path}\n \texpected: #{signer}\n \tfound: #{actualsigner}"
-			problem = true		
+def validate(path,signer)
+    problem = false
+    files = Array.new
+    puts signer+"\n\n"
+    Find.find(path) do |subpath|
+        if not File.exists? subpath
+            puts "No such file #{subpath}"
+            problem = true
+            break
+        end
+        fileCheck = %x{/usr/bin/file -h "#{subpath}"}
+        if fileCheck.include? "Mach-O"
+            files.push(subpath)
         end
         
-	end
+    end
+    
+    if files.size()
+        names = Array.new
+        files.each do |key|
+            names.push(File.basename(key))
+        end
+        longest_key = names.max { |a, b| a.length <=> b.length }
+        
+        files.each do |key|
+            codesign_command = IO.popen("codesign -dvvv \"#{key}\" 2>&1")
+            candidate_lines = codesign_command.readlines
+            lines = candidate_lines.select {|v| v =~ /Authority=/}
+            signature = lines[0]
+            signature = signature.gsub("Authority=", "")
+            signature = signature.strip
+            printf "%-#{longest_key.length}s: %s\n", File.basename(key), signature
+            if !signature or !signature.include? "#{signer}"
+                problem = true
+            end
+        end
+    end
+    
+    if not problem
+        puts "\nBinary valid"
+    else
+        puts "\nBinary invalid"
+    end
+    return problem
 end
 
-if problem
-	exit(1)
+def main
+    path = ARGV[0]
+    signer = "3rd Party Mac Developer Application: The Growl Project, LLC"
+    problem = false
+    opts = GetoptLong.new(
+		[ "--signing-identity", "-i", GetoptLong::REQUIRED_ARGUMENT ]
+	)
+    
+    opts.each do |opt, arg|
+        case opt
+            when "--signing-identity"
+                signer = arg
+            end
+        end
+    problem = validate(path, signer)
+    if problem
+        exit(1)
+    end
+end
+
+
+
+if __FILE__ == $0
+    main()
 end
