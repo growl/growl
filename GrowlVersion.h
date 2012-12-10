6b812b2b9f1da242c6349c4d46f846bb4642e223
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2012-12-10 17:39:04 -0600

    Bumping to 2.1a2

--- GrowlVersion.h
+++ GrowlVersion.h
@@ -1,8 +1,8 @@
 #define COMMENT_1 DO NOT EDIT THE VERSION IN THIS FILE!
 #define COMMENT_2 In case you're wondering why we're using #define for comments: This header is #imported into both Objective-C code and XML Info.plist files.
 
-#define GROWL_VERSION 2.1a1
-#define GROWL_VERSION_STRING "2.1a1"
+#define GROWL_VERSION 2.1a2
+#define GROWL_VERSION_STRING "2.1a2"
 
 #include "VCSData.h"
 
