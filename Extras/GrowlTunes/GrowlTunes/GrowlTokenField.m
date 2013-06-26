f07ed8f0c788aecf65b7243b088df2e6a675e47e
Elizabeth <3429935+JCFalkenberg@users.noreply.github.com>
2013-06-25 21:02:51 -0500

    GrowlTunes: Couple minor tweaks, removing some extraneous logging statements and commented out code.

--- Extras/GrowlTunes/GrowlTunes/GrowlTokenField.m
+++ Extras/GrowlTunes/GrowlTunes/GrowlTokenField.m
@@ -29,8 +29,6 @@
 - (NSSize)intrinsicContentSize {
 	CGSize size = [super intrinsicContentSize];
 	CGFloat width = [self frame].size.width;
-	/*if(self.tag > 0)
-		width -= 10.0f;*/
 	__block CGFloat currentHeight = 0.0f;
 	__block CGFloat currentWidth = 0.0f;
 	__block CGFloat height = 16.0f;
@@ -49,42 +47,34 @@
 		
 		if(cellWidth > width){
 			if(currentWidth > 0.0f){
-				//NSLog(@"Double new line");
 				height += currentHeight;
 				height += cellHeight;
 				currentHeight = 0.0f;
 				currentWidth = 0.0f;
 			}else{
-				//NSLog(@"new line");
 				height += currentHeight;
 			}
 		}else{
 			if(currentWidth > 0.0f){
 				if(currentWidth + cellWidth > width){
-					//NSLog(@"new line from width");
 					height += currentHeight;
 					currentWidth = cellWidth;
 					currentHeight = cellHeight;
 				}else if([[obj displayString] rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].location != NSNotFound){
-					//NSLog(@"new line from return");
 					height += currentHeight;
 					currentWidth = 0.0f;
 					currentHeight = cellHeight;
 				}else{
-					//NSLog(@"no new line from width");
 					currentWidth += cellWidth;
 				}
 			}else{
-				//NSLog(@"setting new width");
 				currentWidth = cellWidth;
 			}
 		}
-		
+		RELEASE(cell);
 	}];
 	if(self.tag > 0)
 		height += 8.0f;
-	/*if(size.height != height)
-		NSLog(@"old: %lf new: %lf", size.height, height);*/
 	size.height = height;
 	return size;
 }
