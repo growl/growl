70fa3c82b707cd836e38c667a999211618999503
Chris Forsythe <1464091+ChrisForsythe@users.noreply.github.com>
2013-08-14 15:17:44 -0500

    Bumping to 2.1.1

--- Core/Source/GrowlFirstLaunchWindowController.m
+++ Core/Source/GrowlFirstLaunchWindowController.m
@@ -33,7 +33,7 @@ typedef void(^GrowlFirstLaunchAction)(void);
 
 +(BOOL)previousVersionOlder
 {
-   NSString *current = @"2.1.1b3";//[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+   NSString *current = @"2.1.1";//[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
    NSString *previous = [[GrowlPreferencesController sharedController] objectForKey:LastKnownVersionKey];
    return (!previous || compareVersionStrings(previous, current) == kCFCompareLessThan);
 }
